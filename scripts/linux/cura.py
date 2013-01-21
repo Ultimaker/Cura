d6be1dfbd9124ed1c35c32a0819bbfa3d9e6759a
Miro Hrončok <miro@hroncok.cz>
2013-01-21 15:26:45 +0100

    Add py prefix to OpenGL and serial. Exit when error.

--- scripts/linux/cura.py
+++ scripts/linux/cura.py
@@ -10,12 +10,20 @@ try:
     import power
 except ImportError as e:
     module = e.message.lstrip('No module named ')
+    
+    if module == 'OpenGL':
+        module = 'PyOpenGL'
+    elif module == 'serial':
+        module = 'pyserial'
+    
     print 'Requires ' + module
 
     if module == 'power':
         print "Install from: https://github.com/GreatFruitOmsk/Power"
     else:
         print "Try sudo easy_install " + module
+    
+    exit(1)
 
 sys.path.insert(1, os.path.dirname(__file__))
 
