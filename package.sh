7cfdf68260b9d2df9b9597dd49bc9df0fac1ad99
daid303 <daid303@gmail.com>
2013-04-22 14:17:14 +0200

    Update build number.

--- package.sh
+++ package.sh
@@ -17,7 +17,7 @@ BUILD_TARGET=${1:-all}
 ##Do we need to create the final archive
 ARCHIVE_FOR_DISTRIBUTION=1
 ##Which version name are we appending to the final archive
-BUILD_NAME=13.03
+BUILD_NAME=13.04
 TARGET_DIR=Cura-${BUILD_NAME}-${BUILD_TARGET}
 
 ##Which versions of external programs to use
