0426a34a427f3c497408df7b67976ff06f16008b
daid <daid303@gmail.com>
2013-06-28 08:48:05 +0200

    Add paths for engine globally installed on linux.

--- Cura/util/sliceEngine.py
+++ Cura/util/sliceEngine.py
@@ -23,6 +23,10 @@ def getEngineFilename():
 		return os.path.abspath(os.path.join(os.path.dirname(__file__), '../..', 'CuraEngine.exe'))
 	if hasattr(sys, 'frozen'):
 		return os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../../..', 'CuraEngine'))
+	if os.path.isfile('/usr/bin/CuraEngine'):
+		return '/usr/bin/CuraEngine'
+	if os.path.isfile('/usr/local/bin/CuraEngine'):
+		return '/usr/local/bin/CuraEngine'
 	return os.path.abspath(os.path.join(os.path.dirname(__file__), '../..', 'CuraEngine'))
 
 def getTempFilename():
