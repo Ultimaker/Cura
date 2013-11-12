ee172c2f222b15ab226342351dcb1f8dcf2b85d7
daid <daid303@gmail.com>
2013-11-12 14:15:44 +0100

    Slight text change.

--- Cura/gui/simpleMode.py
+++ Cura/gui/simpleMode.py
@@ -35,7 +35,7 @@ class simpleModePanel(wx.Panel):
 		sizer = wx.GridBagSizer()
 		self.SetSizer(sizer)
 
-		sb = wx.StaticBox(printTypePanel, label=_("Select a print type:"))
+		sb = wx.StaticBox(printTypePanel, label=_("Select a print profile:"))
 		boxsizer = wx.StaticBoxSizer(sb, wx.VERTICAL)
 		boxsizer.Add(self.printTypeHigh)
 		boxsizer.Add(self.printTypeNormal)
