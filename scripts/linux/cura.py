3e9ca2d5251e74f786da62d507a77960f19d5c18
nallath <nallath@gmail.com>
2014-03-05 15:54:23 +0100

    Fix #763

--- scripts/linux/cura.py
+++ scripts/linux/cura.py
@@ -5,11 +5,11 @@ import os, sys
 sys.path.insert(1, os.path.dirname(__file__))
 
 try:
-    import OpenGL
-    import wx
-    import serial
-    import numpy
-    import power
+	import OpenGL
+	import wx
+	import serial
+	import numpy
+	import power
 except ImportError as e:
 	if e.message[0:16] == 'No module named ':
 		module = e.message[16:]
@@ -26,7 +26,7 @@ except ImportError as e:
 			print "Try sudo easy_install " + module
 		print e.message
     
-    exit(1)
+	exit(1)
 
 
 import Cura.cura as cura
