70c16228ca84ac002009842b6dcd6b9b2c9b69d6
Tomasz CEDRO <cederom@tlen.pl>
2014-06-03 17:41:24 +0200

    Added FreeBSD as the supported platform for package.sh.

--- package.sh
+++ package.sh
@@ -13,6 +13,7 @@ BUILD_TARGET=${1:-none}
 #BUILD_TARGET=darwin
 #BUILD_TARGET=debian_i386
 #BUILD_TARGET=debian_amd64
+#BUILD_TARGET=freebsd
 
 ##Do we need to create the final archive
 ARCHIVE_FOR_DISTRIBUTION=1
@@ -74,6 +75,7 @@ if [ "$BUILD_TARGET" = "none" ]; then
 	echo "$0 debian_i368"
 	echo "$0 debian_amd64"
 	echo "$0 darwin"
+	echo "$0 freebsd"
 	exit 0
 fi
 
