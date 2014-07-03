410e33c8ad07693648c57f281c9ed43369b6180a
daid <daid303@gmail.com>
2014-07-03 10:32:49 +0200

    Update to release 14.07

--- package.sh
+++ package.sh
@@ -18,7 +18,7 @@ BUILD_TARGET=${1:-none}
 ##Do we need to create the final archive
 ARCHIVE_FOR_DISTRIBUTION=1
 ##Which version name are we appending to the final archive
-export BUILD_NAME=14.06.1
+export BUILD_NAME=14.07
 TARGET_DIR=Cura-${BUILD_NAME}-${BUILD_TARGET}
 
 ##Which versions of external programs to use
