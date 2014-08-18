Ultimaker/Cura

commit e17446726f7fbac31f97a0e51536571822987215

Update the language files.

Cura/util/resources.py
@@ -50,14 +50,8 @@ def getDefaultMachineProfiles():
 	return glob.glob(path)
 
 def setupLocalization(selectedLanguage = None):
-	try:
-		if sys.platform.startswith('darwin'):
-			languages = NSLocale.preferredLanguages()
-		else:
-			#Using wx.Locale before you created wx.App seems to cause an nasty exception. So default to 'en' at the moment.
-			languages = [wx.Locale(wx.LANGUAGE_DEFAULT).GetCanonicalName()]
-	except Exception as e:
-		languages = ['en']
+	#Default to english
+	languages = ['en']
 
 	if selectedLanguage is not None:
 		for item in getLanguageOptions():
@@ -70,7 +64,6 @@ def setupLocalization(selectedLanguage = None):
 
 def getLanguageOptions():
 	return [
-		# [None, 'System default'],
 		['en', 'English'],
 		# ['de', 'Deutsch'],
 		# ['fr', 'French'],

Cura/util/profile.py
@@ -352,7 +352,7 @@ setting('filament_cost_kg', '0', float, 'preference', 'hidden').setLabel(_("Cost
 setting('filament_cost_meter', '0', float, 'preference', 'hidden').setLabel(_("Cost (price/m)"), _("Cost of your filament per meter, to estimate the cost of the final print."))
 setting('auto_detect_sd', 'True', bool, 'preference', 'hidden').setLabel(_("Auto detect SD card drive"), _("Auto detect the SD card. You can disable this because on some systems external hard-drives or USB sticks are detected as SD card."))
 setting('check_for_updates', 'True', bool, 'preference', 'hidden').setLabel(_("Check for updates"), _("Check for newer versions of Cura on startup"))
-setting('submit_slice_information', 'False', bool, 'preference', 'hidden').setLabel(_("Send usage statistics"), _("Submit anonymous usage information to improve next versions of Cura"))
+setting('submit_slice_information', 'False', bool, 'preference', 'hidden').setLabel(_("Send usage statistics"), _("Submit anonymous usage information to improve future versions of Cura"))
 setting('youmagine_token', '', str, 'preference', 'hidden')
 setting('filament_physical_density', '1240', float, 'preference', 'hidden').setRange(500.0, 3000.0).setLabel(_("Density (kg/m3)"), _("Weight of the filament per m3. Around 1240 for PLA. And around 1040 for ABS. This value is used to estimate the weight if the filament used for the print."))
 setting('language', 'English', str, 'preference', 'hidden').setLabel(_('Language'), _('Change the language in which Cura runs. Switching language requires a restart of Cura'))

commit e8c275d05020b066a6aca0b22dccf5b6eae98534

util: Remove uneccesary and unwanted import of wx
This dependency was actually removed in e17446726f7fbac31f97a0e51536571822987215
Caused problems for non-GUI uses of this modules, like in CuraServer

Cura/util/resources.py
@@ -8,8 +8,6 @@ import os
 import sys
 import glob
 
-#Cura/util classes should not depend on wx...
-import wx
 import gettext
 
 if sys.platform.startswith('darwin'):