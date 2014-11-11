8ad7b5546e0afd25b55411851feee61e5377a71d
Arjen Hiemstra <ahiemstra@heimr.nl>
2014-11-11 15:15:11 +0100

    Add a MainWindow class to Wx and use it in printer and scanner

--- PrinterApplication.py
+++ PrinterApplication.py
@@ -1,8 +1,11 @@
 from Cura.Wx.WxApplication import WxApplication
+from Cura.Wx.MainWindow import MainWindow
 
 class PrinterApplication(WxApplication):
     def __init__(self):
         super(PrinterApplication, self).__init__()
         
     def run(self):
+        window = MainWindow("Cura Printer")
+        window.Show()
         super(PrinterApplication, self).run()
