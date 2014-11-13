797a345aa1a163e5810f09fefb98cc3c79cb4373
Arjen Hiemstra <ahiemstra@heimr.nl>
2014-11-13 16:34:51 +0100

    Add Mesh reading and a basic mesh to render to PrinterApplication

--- PrinterApplication.py
+++ PrinterApplication.py
@@ -1,6 +1,10 @@
 from Cura.Wx.WxApplication import WxApplication
 from Cura.Wx.MainWindow import MainWindow
 
+from Cura.Scene.SceneObject import SceneObject
+
+import wx
+
 class PrinterApplication(WxApplication):
     def __init__(self):
         super(PrinterApplication, self).__init__()
@@ -8,9 +12,16 @@ class PrinterApplication(WxApplication):
     def run(self):
         self._plugin_registry.loadPlugins({ "type": "StorageDevice" })
         self._plugin_registry.loadPlugins({ "type": "View" })
+        self._plugin_registry.loadPlugins({ "type": "MeshHandler" })
         
         self.getController().setActiveView("MeshView")
-        
+
+        root = self.getController().getScene().getRoot()
+        mesh = SceneObject()
+        mesh.setMeshData(self.getMeshFileHandler().read("plugins/STLReader/simpleTestCube.stl",self.getStorageDevice('local')))
+        root.addChild(mesh)
+
         window = MainWindow("Cura Printer", self)
+        window.getCanvas().setBackgroundColor(wx.Colour(255, 0, 0, 255))
         window.Show()
         super(PrinterApplication, self).run()
