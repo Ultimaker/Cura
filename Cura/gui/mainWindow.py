3f6cef28ac12572e788fef45a1ff3d8d9502f634
Steven Abadie <steven@alephobjects.com>
2014-11-19 21:31:03 -0700

    Disabled the bed level wizard for LulzBot Mini and TAZ.

--- Cura/gui/mainWindow.py
+++ Cura/gui/mainWindow.py
@@ -329,8 +329,11 @@ class mainWindow(wx.Frame):
 			# Enabled sash
 			self.splitter.SetSashSize(4)
 		self.defaultFirmwareInstallMenuItem.Enable(firmwareInstall.getDefaultFirmware() is not None)
-		if profile.getMachineSetting('machine_type') == 'ultimaker2':
+		if profile.getMachineSetting('machine_type') == 'ultimaker2' or profile.getMachineSetting('machine_type') == 'lulzbot_mini' or profile.getMachineSetting('machine_type') == 'lulzbot_TAZ':
 			self.bedLevelWizardMenuItem.Enable(False)
+			self.headOffsetWizardMenuItem.Enable(False)
+		else:
+			self.bedLevelWizardMenuItem.Enable(True)
 			self.headOffsetWizardMenuItem.Enable(False)
 		if int(profile.getMachineSetting('extruder_amount')) < 2:
 			self.headOffsetWizardMenuItem.Enable(False)
