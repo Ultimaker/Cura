973a80909d1e17c4770334a9d1437d1b9e61d42e
daid <daid303@gmail.com>
2014-12-16 16:00:36 +0100

    RC3 for paul.

--- package.sh
+++ package.sh
@@ -19,7 +19,7 @@ BUILD_TARGET=${1:-none}
 ##Do we need to create the final archive
 ARCHIVE_FOR_DISTRIBUTION=1
 ##Which version name are we appending to the final archive
-export BUILD_NAME=15.01-RC2
+export BUILD_NAME=15.01-RC3
 TARGET_DIR=Cura-${BUILD_NAME}-${BUILD_TARGET}
 
 ##Which versions of external programs to use
