8c47319eb75d5f06efe834e3a854279e19b4d82c
daid <daid303@gmail.com>
2015-02-04 14:39:22 +0100

    Updated translations. Included russian translation from russian reseller.

--- Cura/util/resources.py
+++ Cura/util/resources.py
@@ -87,6 +87,7 @@ def getLanguageOptions():
 		['de', 'Deutsch'],
 		['fr', 'French'],
 		['tr', 'Turkish'],
+		['ru', 'Russian'],
 		# ['ko', 'Korean'],
 		# ['zh', 'Chinese'],
 		# ['nl', 'Nederlands'],
