608ef48b15b8159fd1e760137935cc6b7112085d
Youness Alaoui <kakaroto@kakaroto.homelinux.net>
2015-02-12 16:14:34 -0500

    Revision 1.18.8

--- package.sh
+++ package.sh
@@ -22,7 +22,7 @@ export BUILD_NAME=14.09
 TARGET_DIR=Cura-${BUILD_NAME}-${BUILD_TARGET}
 
 ##Revision
-export REVISION=1.18.7
+export REVISION=1.18.8
 
 ##Git commit
 GIT_HASH=$(git rev-parse --short=4 HEAD)
