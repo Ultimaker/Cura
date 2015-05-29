9aa6318a32de94f360d5dede1761f8957d8f4e69
Youness Alaoui <kakaroto@kakaroto.homelinux.net>
2015-05-29 11:08:07 -0400

    Set print all at once as the default setting
    Fixes issue #110

--- Cura/util/profile.py
+++ Cura/util/profile.py
@@ -484,7 +484,7 @@ setting('postSwitchExtruder.gcode', """;Switch between the current extruder and 
 setting('startMode', 'Simple', ['Simple', 'Normal'], 'preference', 'hidden')
 setting('simpleModeProfile', '2_normal', str, 'hidden', 'hidden')
 setting('simpleModeMaterial', '1_pla', str, 'hidden', 'hidden')
-setting('oneAtATime', 'True', bool, 'preference', 'hidden')
+setting('oneAtATime', 'False', bool, 'preference', 'hidden')
 setting('lastFile', os.path.normpath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'resources', 'example', 'Rocktopus.stl')), str, 'preference', 'hidden')
 setting('save_profile', 'False', bool, 'preference', 'hidden').setLabel(_("Save profile on slice"), _("When slicing save the profile as [stl_file]_profile.ini next to the model."))
 setting('filament_cost_kg', '0', float, 'preference', 'hidden').setLabel(_("Cost (price/kg)"), _("Cost of your filament per kg, to estimate the cost of the final print."))
