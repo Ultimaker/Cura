5019a157a1a31b2c999cfd75f510785b74e47dd0
Arjen Hiemstra <a.hiemstra@ultimaker.com>
2015-06-09 05:22:28 -0700

    Bump version to .95

--- cura/CuraApplication.py
+++ cura/CuraApplication.py
@@ -49,7 +49,7 @@ class CuraApplication(QtApplication):
         if not hasattr(sys, "frozen"):
             Resources.addResourcePath(os.path.join(os.path.abspath(os.path.dirname(__file__)), ".."))
 
-        super().__init__(name = "cura", version = "15.05.93")
+        super().__init__(name = "cura", version = "15.05.94")
 
         self.setRequiredPlugins([
             "CuraEngineBackend",
