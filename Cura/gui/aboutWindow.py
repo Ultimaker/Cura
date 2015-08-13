b3c438c1fe803b01a6e4cb37004d84b5fb7a711f
nickthetait <tait@alephobjects.com>
2015-08-13 16:22:15 -0600

    Include link to release notes from 'About Cura' window

--- Cura/gui/aboutWindow.py
+++ Cura/gui/aboutWindow.py
@@ -22,8 +22,14 @@ class aboutWindow(wx.Frame):
 		title.SetFont(wx.Font(18, wx.SWISS, wx.NORMAL, wx.BOLD))
 		s.Add(title, flag=wx.ALIGN_CENTRE|wx.EXPAND|wx.BOTTOM, border=5)
 
- 		version_num = version.getVersion()
- 		s.Add(wx.StaticText(p, -1, 'Version {}'.format(version_num)))
+		version_num = version.getVersion()
+		s.Add(wx.StaticText(p, -1, 'Version {}'.format(version_num)))
+		
+		s.Add(wx.StaticText(p, -1, 'Release notes:'))
+		url = "code.alephobjects.com/w/cura/release-notes/"
+		s.Add(wx.HyperlinkCtrl(p, -1, url))
+		
+		s.Add(wx.StaticText(p, -1, ''))
 		s.Add(wx.StaticText(p, -1, 'End solution for Open Source Fused Filament Fabrication 3D printing.'), flag=wx.TOP, border=5)
 		s.Add(wx.StaticText(p, -1, 'Cura is currently developed and maintained by David Braam and Ultimaker.'), flag=wx.TOP, border=5)
 		s.Add(wx.StaticText(p, -1, 'Cura LulzBot Edition has been modified and maintained by Aleph Objects, Inc.'))
