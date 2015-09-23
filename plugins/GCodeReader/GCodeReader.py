162e27691f2343d2ff56f6b4b36c289a0ef590a6
Jaime van Kessel <nallath@gmail.com>
2015-09-23 15:16:00 +0200

    Added file extension checking

--- plugins/GCodeReader/GCodeReader.py
+++ plugins/GCodeReader/GCodeReader.py
@@ -3,6 +3,19 @@
 
 from UM.Mesh.MeshReader import MeshReader
 
+from cura.LayerData import LayerData
+from cura.LayerDataDecorator import LayerDataDecorator
+
+import os
+
 class GCodeReader(MeshReader):
+    def __init__(self):
+        super().__init__()
+        self._supported_extension = ".gcode"
+
     def read(self, file_name):
-        pass
+        extension = os.path.splitext(file_name)[1]
+        if extension.lower() == self._supported_extension:
+            with open (file_name,"rt") as f:
+                for line in f:
+                    pass
