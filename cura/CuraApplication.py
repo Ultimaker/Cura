457f2945b033eaf7386e9387e9722a2baf518eab
Arjen Hiemstra <ahiemstra@heimr.nl>
2015-09-29 16:59:26 +0200

    Bump version

--- cura/CuraApplication.py
+++ cura/CuraApplication.py
@@ -61,7 +61,7 @@ class CuraApplication(QtApplication):
         if not hasattr(sys, "frozen"):
             Resources.addSearchPath(os.path.join(os.path.abspath(os.path.dirname(__file__)), ".."))
 
-        super().__init__(name = "cura", version = "15.09.83")
+        super().__init__(name = "cura", version = "15.09.85")
 
         self.setWindowIcon(QIcon(Resources.getPath(Resources.Images, "cura-icon.png")))
 
