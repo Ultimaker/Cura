3f5d609baa6be6fad1d4b92fa8e95b23883f6ecc
Arjen Hiemstra <ahiemstra@heimr.nl>
2015-10-08 12:13:35 +0200

    Set version to master

--- cura/CuraApplication.py
+++ cura/CuraApplication.py
@@ -68,7 +68,7 @@ class CuraApplication(QtApplication):
         if not hasattr(sys, "frozen"):
             Resources.addSearchPath(os.path.join(os.path.abspath(os.path.dirname(__file__)), ".."))
 
-        super().__init__(name = "cura", version = "15.09.90")
+        super().__init__(name = "cura", version = "master")
 
         self.setWindowIcon(QIcon(Resources.getPath(Resources.Images, "cura-icon.png")))
 
