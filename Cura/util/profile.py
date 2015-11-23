11a61272a3fa9888f198fc856b4ebf8a7d570c70
nickthetait <tait@alephobjects.com>
2015-11-23 10:39:08 -0700

    Initial fakeouts for Mangrove
    Plenty of things are misplaced, broken and poorly implemented yet

--- Cura/util/profile.py
+++ Cura/util/profile.py
@@ -1213,7 +1213,8 @@ def calculateObjectSizeOffsets():
 def getMachineCenterCoords():
 	if getMachineSetting('machine_center_is_zero') == 'True':
 		return [0, 0]
-	elif getMachineSetting('machine_type') == 'lulzbot_mini':
+	elif getMachineSetting('machine_type') == 'lulzbot_mini' or \
+		 getMachineSetting('machine_type') == 'lulzbot_TAZ_6':
 		return [(getMachineSettingFloat('machine_width') / 2) + 2.5,
 			    (getMachineSettingFloat('machine_width') / 2) + 0.5]
 	return [getMachineSettingFloat('machine_width') / 2, getMachineSettingFloat('machine_depth') / 2]
