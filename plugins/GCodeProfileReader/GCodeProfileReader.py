fedfffb98d713c04a40864dcecde48af6e63f6dd
Ghostkeeper <ghost_keeper+github@hotmail.com>
2015-12-15 13:00:11 +0100

    Update documentation
    Just a slight inaccuracy in the documentation of one of the imports.
    
    Contributes to issue CURA-34.

--- plugins/GCodeProfileReader/GCodeProfileReader.py
+++ plugins/GCodeProfileReader/GCodeProfileReader.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2015 Ultimaker B.V.
 # Cura is released under the terms of the AGPLv3 or higher.
 
-from UM.Application import Application #To get the current profile that should be updated with the settings from the g-code.
+from UM.Application import Application #To get the machine manager to create the new profile in.
 from UM.Settings.Profile import Profile
 from UM.Settings.ProfileReader import ProfileReader
 import re #Regular expressions for parsing escape characters in the settings.
