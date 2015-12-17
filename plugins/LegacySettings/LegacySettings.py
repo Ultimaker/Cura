77453cf80f93d5033a306b0afd41ed098fc163d4
Ghostkeeper <ghost_keeper+github@hotmail.com>
2015-12-17 14:01:42 +0100

    Remove old version of LegacyProfileReader
    This shouldn't have been committed. I suspect this came in via checking out a branch where this was still on the working tree and then going to a branch where it wasn't, and it kept the files and I thought LegacySettings was the plugin I had to commit.
    
    Contributes to issue CURA-37.

--- plugins/LegacySettings/LegacySettings.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2015 Ultimaker B.V.
-# Cura is released under the terms of the AGPLv3 or higher.
-
-from UM.Extension import Extension
-
-class LegacySettings(Extension):
-    def __init__(self):
-        super().__init__()
