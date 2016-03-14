eed5f88b1316a267e5c4d2ea0a4953cce636925d
Thomas Karl Pietrowski <thopiekar@googlemail.com>
2016-03-14 13:56:27 +0100

    USBPrinting: Adding a short instruction
    Just added a short instruction where to find the id of the machine, just to make it easier for the vendor to fill in the correct values.

--- plugins/USBPrinting/USBPrinterManager.py
+++ plugins/USBPrinting/USBPrinterManager.py
@@ -140,6 +140,8 @@ class USBPrinterManager(QObject, SignalEmitter, OutputDevicePlugin, Extension):
         else:
             baudrate = 250000
 
+        # Note the keyword here is the id of the machine. You can find the id of your machine in the *.json file, eg.
+        # https://github.com/Ultimaker/Cura/blob/master/resources/machines/ultimaker_original.json#L2
         machine_without_heated_bed = {"ultimaker_original"       : "MarlinUltimaker-{baudrate}.hex",
                                       "ultimaker_original_plus"  : "MarlinUltimaker-UMOP-{baudrate}.hex",
                                       "bq_witbox"                : "MarlinWitbox.hex",
