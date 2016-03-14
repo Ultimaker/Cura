3893cd06bd9b5277a6d771bf99cc611f78e42903
Thomas-Karl Pietrowski <thopiekar@googlemail.com>
2016-03-14 15:49:47 +0100

    platform gets imported twice

--- cura/CuraApplication.py
+++ cura/CuraApplication.py
@@ -1,8 +1,6 @@
 # Copyright (c) 2015 Ultimaker B.V.
 # Cura is released under the terms of the AGPLv3 or higher.
 
-import platform
-
 from UM.Qt.QtApplication import QtApplication
 from UM.Scene.SceneNode import SceneNode
 from UM.Scene.Camera import Camera
