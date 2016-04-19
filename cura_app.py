e040029a410624a4e239ead892304c92a82b8bd4
Thomas Karl Pietrowski <thopiekar@googlemail.com>
2016-04-19 19:18:05 +0200

    Just a little typo

--- cura_app.py
+++ cura_app.py
@@ -15,7 +15,7 @@ import sys
 # incompatibility issues with libArcus
 if "PYTHONPATH" in os.environ.keys():                # If PYTHONPATH is used
     if sys.path[-1] == os.environ["PYTHONPATH"]:     # .. check whether PYTHONPATH is placed incorrectly at the end of sys.path.
-        sys.path.pop(-1)                             # If so remove that element..
+        sys.path.pop(-1)                             # If so, remove that element..
         sys.path.insert(1, os.environ['PYTHONPATH']) # and add it at the correct place again.
 
 
