41ee575cc1bfdc769bde94d9d81d8146498e46ce
Ghostkeeper <rubend@tutanota.com>
2016-04-28 12:03:12 +0200

    Correct copyright year
    These files were made in 2016.
    
    Contributes to issue CURA-844.

--- plugins/VersionUpgrade/VersionUpgrade21to22/MachineInstance.py
+++ plugins/VersionUpgrade/VersionUpgrade21to22/MachineInstance.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2015 Ultimaker B.V.
+# Copyright (c) 2016 Ultimaker B.V.
 # Cura is released under the terms of the AGPLv3 or higher.
 
 import UM.Settings.SettingsError #To indicate that a file is of incorrect format.
