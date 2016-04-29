b899c65027793e691c2cac4354d47841ce97a280
Jaime van Kessel <nallath@gmail.com>
2016-04-29 12:01:24 +0200

    Added getters for MachineAction
    CURA-1385

--- cura/MachineAction.py
+++ cura/MachineAction.py
@@ -7,5 +7,12 @@ from UM.PluginObject import PluginObject
 
 class MachineAction(QObject, PluginObject):
     def __init__(self, key, label = ""):
+        super().__init__()
         self._key = key
         self._label = label
+
+    def getKey(self):
+        return self._key
+
+    def getLabel(self):
+        return self._label
