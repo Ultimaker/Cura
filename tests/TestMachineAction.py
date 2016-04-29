30182f295f753bb03d73db1eac9f82eaaa06f716
Jaime van Kessel <nallath@gmail.com>
2016-04-29 13:19:07 +0200

    Added tests for firstStart
    CURA-1385

--- tests/TestMachineAction.py
+++ tests/TestMachineAction.py
@@ -12,7 +12,6 @@ class Machine:
     def getKey(self):
         return self._key
 
-
 def test_addMachineAction():
 
     machine_manager = MachineActionManager()
@@ -59,4 +58,20 @@ def test_addMachineAction():
     # Check if adding multiple required actions works.
     machine_manager.addRequiredAction(test_machine, "test_action_2")
     assert machine_manager.getRequiredActions(test_machine) == {test_action, test_action_2}
+
+    # Ensure that firstStart actions are empty by default.
+    assert machine_manager.getFirstStartActions(test_machine) == []
+
+    # Check if adding multiple (the same) actions to first start actions work.
+    machine_manager.addFirstStartAction(test_machine, "test_action")
+    machine_manager.addFirstStartAction(test_machine, "test_action")
+    assert machine_manager.getFirstStartActions(test_machine) == [test_action, test_action]
+
+    # Check if inserting an action works
+    machine_manager.addFirstStartAction(test_machine, "test_action_2", index = 1)
+    assert machine_manager.getFirstStartActions(test_machine) == [test_action, test_action_2, test_action]
+
+    # Check that adding a unknown action doesn't change anything.
+    machine_manager.addFirstStartAction(test_machine, "key_that_doesnt_exist", index = 1)
+    assert machine_manager.getFirstStartActions(test_machine) == [test_action, test_action_2, test_action]
 
