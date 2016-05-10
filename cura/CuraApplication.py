170df747b3710d9491547e874253cc9316d3d1e8
Jaime van Kessel <nallath@gmail.com>
2016-05-10 11:40:36 +0200

    Use globalContainer stack instead of activeInstance
    CURA-1278

--- cura/CuraApplication.py
+++ cura/CuraApplication.py
@@ -101,8 +101,6 @@ class CuraApplication(QtApplication):
         self._camera_animation = None
         self._cura_actions = None
 
-        #self.getMachineManager().activeMachineInstanceChanged.connect(self._onGlobalContainerStackChanged)
-        #self.getMachineManager().addMachineRequested.connect(self._onAddMachineRequested)
         self.getController().getScene().sceneChanged.connect(self.updatePlatformActivity)
         self.getController().toolOperationStopped.connect(self._onToolOperationStopped)
 
