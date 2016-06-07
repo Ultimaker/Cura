4695862b491dc0edd2560b89f689b170f385e391
Jaime van Kessel <nallath@gmail.com>
2016-06-07 13:33:19 +0200

    Added deepcopy function to settingOverrideDecorator
    CURA-1636

--- cura/SettingOverrideDecorator.py
+++ cura/SettingOverrideDecorator.py
@@ -7,7 +7,7 @@ from UM.Settings.InstanceContainer import InstanceContainer
 from UM.Settings.ContainerRegistry import ContainerRegistry
 
 from UM.Application import Application
-
+import copy
 ##  A decorator that adds a container stack to a Node. This stack should be queried for all settings regarding
 #   the linked node. The Stack in question will refer to the global stack (so that settings that are not defined by
 #   this stack still resolve.
@@ -24,6 +24,15 @@ class SettingOverrideDecorator(SceneNodeDecorator):
 
         Application.getInstance().globalContainerStackChanged.connect(self._onGlobalContainerStackChanged)
         self._onGlobalContainerStackChanged()
+
+    def __deepcopy__(self, memo):
+        ## Create a fresh decorator object
+        deep_copy = SettingOverrideDecorator()
+        ## Copy the stack
+        deep_copy._stack = copy.deepcopy(self._stack, memo)
+        ## Ensure that the id is unique.
+        deep_copy._stack._id = id(deep_copy)
+        return deep_copy
 
     def _onSettingChanged(self, instance, property):
         if property == "value":  # Only reslice if the value has changed.
