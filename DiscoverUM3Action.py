bca613b47e7b44e04d61eacca2d0e45a09636f11
Jaime van Kessel <nallath@gmail.com>
2016-06-20 13:43:43 +0200

    Added label
    CURA-1385

--- DiscoverUM3Action.py
+++ DiscoverUM3Action.py
@@ -2,4 +2,7 @@ from cura.MachineAction import MachineAction
 
 class DiscoverUM3Action(MachineAction):
     def __init__(self):
-        super().__init__("DiscoverUM3Action")
+        super().__init__("DiscoverUM3Action", "Discover printers")
+
+    def _execute(self):
+        pass
