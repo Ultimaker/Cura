7f5f22d472bc6b261010707ffe0d0d994c62bafa
Thomas Karl Pietrowski <thopiekar@googlemail.com>
2016-07-01 19:37:24 +0200

    Moving Nvidia workaround to cura_app.py * also added "debian" into the list of distributions

--- cura_app.py
+++ cura_app.py
@@ -5,6 +5,19 @@
 
 import os
 import sys
+import platform
+
+from UM.Platform import Platform
+
+#WORKAROUND: GITHUB-88 GITHUB-385 GITHUB-612
+if Platform.isLinux(): # Needed for platform.linux_distribution, which is not available on Windows and OSX
+    # For Ubuntu: https://bugs.launchpad.net/ubuntu/+source/python-qt4/+bug/941826
+    print(platform.linux_distribution())
+    if platform.linux_distribution()[0] in ("debian", "Ubuntu", ): # TODO: Needs a "if X11_GFX == 'nvidia'" here. The workaround is only needed on Ubuntu+NVidia drivers. Other drivers are not affected, but fine with this fix.
+        import ctypes
+        from ctypes.util import find_library
+        libGL = find_library("GL")
+        ctypes.CDLL(libGL, ctypes.RTLD_GLOBAL)
 
 #WORKAROUND: GITHUB-704 GITHUB-708
 # It looks like setuptools creates a .pth file in
