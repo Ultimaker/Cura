0c23c26ac91b7bcdccfa7525686e8317bcaa14f9
fieldOfView <aldo@fieldofview.com>
2016-07-03 16:01:57 +0200

    Code style CURA-1676

--- cura/CuraSplashScreen.py
+++ cura/CuraSplashScreen.py
@@ -25,14 +25,13 @@ class CuraSplashScreen(QSplashScreen):
         if buildtype:
             version[0] += " (%s)" %(buildtype)
 
-        used_font = QFont() # Using system-default font here
-        used_font.setPointSize(20)
-        painter.setFont(used_font)
+        font = QFont() # Using system-default font here
+        font.setPointSize(20)
+        painter.setFont(font)
         painter.drawText(0, 0, 330 * self._scale, 230 * self._scale, Qt.AlignHCenter | Qt.AlignBottom, version[0])
         if len(version) > 1:
-            used_font = QFont() # Using system-default font here
-            used_font.setPointSize(12)
-            painter.setFont(used_font)
+            font.setPointSize(12)
+            painter.setFont(font)
             painter.drawText(0, 0, 330 * self._scale, 255 * self._scale, Qt.AlignHCenter | Qt.AlignBottom, version[1])
 
         painter.restore()
