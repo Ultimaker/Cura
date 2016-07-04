0e28b331fb3f0949f01834c924bc9d439f96aa57
Arjen Hiemstra <ahiemstra@heimr.nl>
2016-07-04 12:07:29 +0200

    Move Cura setting related classes to cura/Settings
    Contributes to CURA-342

--- cura/Settings/__init__.py
+++ cura/Settings/__init__.py
@@ -1,2 +1,11 @@
+# Copyright (c) 2016 Ultimaker B.V.
+# Cura is released under the terms of the AGPLv3 or higher.
 
 from .MaterialSettingsVisibilityHandler import MaterialSettingsVisibilityHandler
+from .ContainerManager import ContainerManager
+from .ContainerSettingsModel import ContainerSettingsModel
+from .CuraContainerRegistry import CuraContainerRegistry
+from .ExtruderManager import ExtruderManager
+from .ExtrudersModel import ExtrudersModel
+from .MaterialSettingsVisibilityHandler import MaterialSettingsVisibilityHandler
+from .SettingOverrideDecorator import SettingOverrideDecorator
