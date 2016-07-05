fc99cac05eeded3a9cf9fad0fa865595cd817541
Jaime van Kessel <nallath@gmail.com>
2016-07-05 09:39:18 +0200

    Removed update firmware from extensions; This is now handled by machine actions

--- plugins/USBPrinting/USBPrinterOutputDeviceManager.py
+++ plugins/USBPrinting/USBPrinterOutputDeviceManager.py
@@ -41,10 +41,6 @@ class USBPrinterOutputDeviceManager(QObject, OutputDevicePlugin, Extension):
         self._check_updates = True
         self._firmware_view = None
 
-        ## Add menu item to top menu of the application.
-        self.setMenuName(i18n_catalog.i18nc("@title:menu","Firmware"))
-        self.addMenuItem(i18n_catalog.i18nc("@item:inmenu", "Update Firmware"), self.updateAllFirmware)
-
         Application.getInstance().applicationShuttingDown.connect(self.stop)
         self.addUSBOutputDeviceSignal.connect(self.addOutputDevice) #Because the model needs to be created in the same thread as the QMLEngine, we use a signal.
 
