aba0392728d38accda9cb60b367401d6682973d6
Jaime van Kessel <nallath@gmail.com>
2016-07-05 10:46:38 +0200

    Saving g-code no longer crashes

--- plugins/SliceInfoPlugin/SliceInfo.py
+++ plugins/SliceInfoPlugin/SliceInfo.py
@@ -54,7 +54,9 @@ class SliceInfo(Extension):
         # Get total material used (in mm^3)
         print_information = Application.getInstance().getPrintInformation()
         material_radius = 0.5 * global_container_stack.getProperty("material_diameter", "value")
-        material_used = math.pi * material_radius * material_radius * print_information.materialAmounts #Volume of material used
+
+        # TODO; Send material per extruder instead of mashing it on a pile
+        material_used = math.pi * material_radius * material_radius * sum(print_information.materialAmounts) #Volume of all materials used
 
         # Get model information (bounding boxes, hashes and transformation matrix)
         models_info = []
