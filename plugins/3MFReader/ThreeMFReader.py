25d13969ee8642b0791554b49a41c36d1d4ad464
Jaime van Kessel <nallath@gmail.com>
2016-07-08 15:58:33 +0200

    Filename is now correctly set for 3mf
    CURA-1622

--- plugins/3MFReader/ThreeMFReader.py
+++ plugins/3MFReader/ThreeMFReader.py
@@ -70,7 +70,7 @@ class ThreeMFReader(MeshReader):
 
                 # TODO: We currently do not check for normals and simply recalculate them.
                 mesh_builder.calculateNormals()
-                
+                mesh_builder.setFileName(file_name)
                 node.setMeshData(mesh_builder.build().getTransformed(rotation))
                 node.setSelectable(True)
 
