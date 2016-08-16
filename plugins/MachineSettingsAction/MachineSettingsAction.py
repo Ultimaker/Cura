9b048efe22c10cdad579bba7ba535206e56f4253
fieldOfView <aldo@fieldofview.com>
2016-08-16 18:19:25 +0200

    Fixed copyrights

--- plugins/MachineSettingsAction/MachineSettingsAction.py
+++ plugins/MachineSettingsAction/MachineSettingsAction.py
@@ -1,3 +1,6 @@
+# Copyright (c) 2016 Ultimaker B.V.
+# Cura is released under the terms of the AGPLv3 or higher.
+
 from PyQt5.QtCore import pyqtSlot
 
 from cura.MachineAction import MachineAction
--- plugins/MachineSettingsAction/__init__.py
+++ plugins/MachineSettingsAction/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2016 Aldo Hoeben / fieldOfView
+# Copyright (c) 2016 Ultimaker B.V.
 # Cura is released under the terms of the AGPLv3 or higher.
 
 from . import MachineSettingsAction
