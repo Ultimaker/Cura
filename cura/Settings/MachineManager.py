4fad2a790bc466215dd459fd581e9253e3336ebc
Youness Alaoui <kakaroto@kakaroto.homelinux.net>
2016-09-14 14:57:43 -0400

    Fix MachineManager connection to OutputDeviceManager signals late.
    If a PrinterOutputDevice is able to connect quickly to a machine, then
    by the time the MachineManager is created and connects to the signal,
    it will be too late, and it might miss that there is already connected
    devices.
    
    # Conflicts:
    #	plugins/USBPrinting/USBPrinterOutputDeviceManager.py

--- cura/Settings/MachineManager.py
+++ cura/Settings/MachineManager.py
@@ -69,6 +69,8 @@ class MachineManager(QObject):
 
         self._printer_output_devices = []
         Application.getInstance().getOutputDeviceManager().outputDevicesChanged.connect(self._onOutputDevicesChanged)
+        # There might already be some output devices by the time the signal is connected
+        self._onOutputDevicesChanged()
 
         if active_machine_id != "":
             # An active machine was saved, so restore it.
