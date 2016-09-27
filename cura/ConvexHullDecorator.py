6783b4b3ef444654ab8aa5aada0c31cac3d44ccb
Ghostkeeper <rubend@tutanota.com>
2016-09-27 16:56:50 +0200

    Fix function call to _getSettingProperty
    Don't know who ever tested this...
    
    Contributes to issue CURA-2407.

--- cura/ConvexHullDecorator.py
+++ cura/ConvexHullDecorator.py
@@ -240,7 +240,7 @@ class ConvexHullDecorator(SceneNodeDecorator):
         elif adhesion_type == "skirt":
             extra_margin = max(
                 0, self._getSettingProperty("skirt_gap", "value") +
-                   self._getSettingPropertyy("skirt_line_count", "value") * self._getSettingProperty("skirt_brim_line_width", "value"))
+                   self._getSettingProperty("skirt_line_count", "value") * self._getSettingProperty("skirt_brim_line_width", "value"))
         else:
             raise Exception("Unknown bed adhesion type. Did you forget to update the convex hull calculations for your new bed adhesion type?")
 
