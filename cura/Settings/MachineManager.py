f60146ad26102cf98c60b34f9bd65a684c3c1c28
Jaime van Kessel <nallath@gmail.com>
2016-10-12 11:59:54 +0200

    Active quality ID is selected from active stack instead of global
    CURA-2624

--- cura/Settings/MachineManager.py
+++ cura/Settings/MachineManager.py
@@ -475,11 +475,11 @@ class MachineManager(QObject):
 
     @pyqtProperty(str, notify=activeQualityChanged)
     def activeQualityId(self):
-        if self._global_container_stack:
-            quality = self._global_container_stack.findContainer({"type": "quality_changes"})
+        if self._active_container_stack:
+            quality = self._active_container_stack.findContainer({"type": "quality_changes"})
             if quality and quality != self._empty_quality_changes_container:
                 return quality.getId()
-            quality = self._global_container_stack.findContainer({"type": "quality"})
+            quality = self._active_container_stack.findContainer({"type": "quality"})
             if quality:
                 return quality.getId()
         return ""
