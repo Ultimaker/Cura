3f6877d2ec8f2df42ccac8f6488a5cb2a385e53b
Jack Ha <j.ha@ultimaker.com>
2016-10-12 13:58:09 +0200

    Keep per object extruder when switching machines. CURA-2533

--- cura/Settings/SettingOverrideDecorator.py
+++ cura/Settings/SettingOverrideDecorator.py
@@ -61,6 +61,15 @@ class SettingOverrideDecorator(SceneNodeDecorator):
     def getActiveExtruder(self):
         return self._extruder_stack
 
+    ##  Gets the currently active extruders position
+    #
+    #   \return An extruder's position, or None if no position info is available.
+    def getActiveExtruderPosition(self):
+        containers = ContainerRegistry.getInstance().findContainers(id = self.getActiveExtruder())
+        if containers:
+            container_stack = containers[0]
+            return container_stack.getMetaDataEntry("position", default=None)
+
     def _onSettingChanged(self, instance, property_name): # Reminder: 'property' is a built-in function
         if property_name == "value":  # Only reslice if the value has changed.
             Application.getInstance().getBackend().forceSlice()
