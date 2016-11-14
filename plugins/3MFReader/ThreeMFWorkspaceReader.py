bade9e1bff66983b7c50bc038bc3216aff67e7cf
Jaime van Kessel <nallath@gmail.com>
2016-11-14 17:50:54 +0100

    Resolve strategy is now always set
    CURA-1263

--- plugins/3MFReader/ThreeMFWorkspaceReader.py
+++ plugins/3MFReader/ThreeMFWorkspaceReader.py
@@ -55,7 +55,7 @@ class ThreeMFWorkspaceReader(WorkspaceReader):
         archive = zipfile.ZipFile(file_name, "r")
         cura_file_names = [name for name in archive.namelist() if name.startswith("Cura/")]
         container_stack_files = [name for name in cura_file_names if name.endswith(self._container_stack_suffix)]
-
+        self._resolve_strategies = {"machine": None, "quality_changes": None}
         machine_conflict = False
         quality_changes_conflict = False
         for container_stack_file in container_stack_files:
@@ -89,7 +89,6 @@ class ThreeMFWorkspaceReader(WorkspaceReader):
                 return WorkspaceReader.PreReadResult.cancelled
             result = self._dialog.getResult()
 
-            self._resolve_strategies = {"machine": None, "quality_changes": None}
             if machine_conflict:
                 self._resolve_strategies["machine"] = result
             if quality_changes_conflict:
