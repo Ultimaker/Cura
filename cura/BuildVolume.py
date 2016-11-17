41825231ad8a78f7a850c056d9b3d63b5f54d5d9
Ghostkeeper <rubend@tutanota.com>
2016-11-17 09:54:00 +0100

    Fix check for if nozzle offset exists
    Because an offset of 0 results in False as well.
    
    Contributes to issue CURA-2625.

--- cura/BuildVolume.py
+++ cura/BuildVolume.py
@@ -559,10 +559,10 @@ class BuildVolume(SceneNode):
         for extruder in used_extruders:
             extruder_id = extruder.getId()
             offset_x = extruder.getProperty("machine_nozzle_offset_x", "value")
-            if not offset_x:
+            if offset_x is None:
                 offset_x = 0
             offset_y = extruder.getProperty("machine_nozzle_offset_y", "value")
-            if not offset_y:
+            if offset_y is None:
                 offset_y = 0
             result[extruder_id] = []
 
