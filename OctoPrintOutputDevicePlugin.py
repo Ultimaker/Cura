a09d9a7affdaec0d63a7ef5874ffe80ddef77dee
Ryan Smith <ryansmith125@gmail.com>
2016-12-06 07:04:10 -0600

    Fixed errant bytearrays
    These caused the system to always use the default path ("/"), occasionally leaving it in an inconsistent state.
    
    The GUI would be able to set the path to the correct value, but the correct value would not be reflected in the GUI, even though the preferences file was occasionally correct.
    
    Since the system used the same function to read from a preference file as it did to create the GUI object, the GUI and internal data structures would show the default path, and the preference file would show the correct value.

--- OctoPrintOutputDevicePlugin.py
+++ OctoPrintOutputDevicePlugin.py
@@ -60,18 +60,17 @@ class OctoPrintOutputDevicePlugin(OutputDevicePlugin):
 
         # Add manual instances from preference
         for name, properties in self._manual_instances.items():
-            additional_properties = {b"path": properties["path"].encode("utf-8"), b"manual": b"true"}
+            additional_properties = {"path": properties["path"], "manual": True}
             self.addInstance(name, properties["address"], properties["port"], additional_properties)
 
     def addManualInstance(self, name, address, port, path):
         self._manual_instances[name] = {"address": address, "port": port, "path": path}
         self._preferences.setValue("octoprint/manual_instances", json.dumps(self._manual_instances))
 
-        properties = { b"path": path.encode("utf-8"), b"manual": b"true" }
+        properties = { "path": path, "manual": True }
 
         if name in self._instances:
             self.removeInstance(name)
-
         self.addInstance(name, address, port, properties)
         self.instanceListChanged.emit()
 
