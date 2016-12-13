b967f045fe2e0342fd4629066f1764d5482454c5
Victor Larchenko <victor99992@gmail.com>
2016-12-13 13:41:00 +0600

    D6: Removed unused file

--- cura/NonSliceableObjectDecorator.py
+++
@@ -1,9 +0,0 @@
-from UM.Scene.SceneNodeDecorator import SceneNodeDecorator
-
-
-class NonSliceableObjectDecorator(SceneNodeDecorator):
-    def __init__(self):
-        super().__init__()
-        
-    def isNonSliceable(self):
-        return True
