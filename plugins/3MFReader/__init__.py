8676f3db404b7c815fae79bdb96b27497ff90af8
Jaime van Kessel <nallath@gmail.com>
2016-12-16 09:36:40 +0100

    Added workaround for curaproject.3mf extensions not being handled on osx
    CURA-3130

--- plugins/3MFReader/__init__.py
+++ plugins/3MFReader/__init__.py
@@ -4,10 +4,16 @@
 from . import ThreeMFReader
 from . import ThreeMFWorkspaceReader
 from UM.i18n import i18nCatalog
+import UM.Platform
 catalog = i18nCatalog("cura")
 
 
 def getMetaData():
+    # Workarround for osx not supporting double file extensions correclty.
+    if UM.Platform.isOSX():
+        workspace_extension = "3mf"
+    else:
+        workspace_extension = "curaproject.3mf"
     return {
         "plugin": {
             "name": catalog.i18nc("@label", "3MF Reader"),
@@ -25,7 +31,7 @@ def getMetaData():
         "workspace_reader":
         [
             {
-                "extension": "curaproject.3mf",
+                "extension": workspace_extension,
                 "description": catalog.i18nc("@item:inlistbox", "3MF File")
             }
         ]
