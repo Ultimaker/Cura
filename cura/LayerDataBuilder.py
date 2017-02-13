106cb6ded9cd4e4fff9c2c19283105ef8cc1ff59
Jack Ha <j.ha@ultimaker.com>
2017-02-13 12:47:04 +0100

    Fixed compatibility mode. CURA-3273

--- cura/LayerDataBuilder.py
+++ cura/LayerDataBuilder.py
@@ -63,8 +63,8 @@ class LayerDataBuilder(MeshBuilder):
         line_dimensions = numpy.empty((vertex_count, 2), numpy.float32)
         colors = numpy.empty((vertex_count, 4), numpy.float32)
         indices = numpy.empty((index_count, 2), numpy.int32)
-        extruders = numpy.empty((vertex_count), numpy.int32)
-        line_types = numpy.empty((vertex_count), numpy.int32)
+        extruders = numpy.empty((vertex_count), numpy.float32)
+        line_types = numpy.empty((vertex_count), numpy.float32)
 
         vertex_offset = 0
         index_offset = 0
