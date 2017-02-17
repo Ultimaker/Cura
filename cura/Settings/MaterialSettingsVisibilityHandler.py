cec3eebace1ad5f236761bdd98bef0d5ac52d3ba
Ghostkeeper <rubend@tutanota.com>
2017-02-17 10:16:11 +0100

    Replace list-to-set cast with normal set literal
    Don't know who did this but he did wrong, yo.

--- cura/Settings/MaterialSettingsVisibilityHandler.py
+++ cura/Settings/MaterialSettingsVisibilityHandler.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2016 Ultimaker B.V.
+# Copyright (c) 2017 Ultimaker B.V.
 # Uranium is released under the terms of the AGPLv3 or higher.
 
 from UM.Settings.Models.SettingVisibilityHandler import SettingVisibilityHandler
@@ -7,13 +7,13 @@ class MaterialSettingsVisibilityHandler(SettingVisibilityHandler):
     def __init__(self, parent = None, *args, **kwargs):
         super().__init__(parent = parent, *args, **kwargs)
 
-        material_settings = set([
+        material_settings = {
             "default_material_print_temperature",
             "material_bed_temperature",
             "material_standby_temperature",
             "cool_fan_speed",
             "retraction_amount",
             "retraction_speed",
-        ])
+        }
 
         self.setVisible(material_settings)
