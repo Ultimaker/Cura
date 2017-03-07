85d979cf5007be4ac77dab667cddf04027c12e8f
Jaime van Kessel <nallath@gmail.com>
2017-03-07 11:41:18 +0100

    Removed unneeded check from network printing.
    We used to not have a printer_state so this check was needed.

--- plugins/UM3NetworkPrinting/NetworkPrinterOutputDevice.py
+++ plugins/UM3NetworkPrinting/NetworkPrinterOutputDevice.py
@@ -600,10 +600,6 @@ class NetworkPrinterOutputDevice(PrinterOutputDevice):
     #   \param filter_by_machine Whether to filter MIME types by machine. This
     #   is ignored.
     def requestWrite(self, nodes, file_name = None, filter_by_machine = False, file_handler = None):
-        if self._progress != 0:
-            self._error_message = Message(i18n_catalog.i18nc("@info:status", "Unable to start a new print job because the printer is busy. Please check the printer."))
-            self._error_message.show()
-            return
         if self._printer_state != "idle":
             self._error_message = Message(
                 i18n_catalog.i18nc("@info:status", "Unable to start a new print job, printer is busy. Current printer status is %s.") % self._printer_state)
