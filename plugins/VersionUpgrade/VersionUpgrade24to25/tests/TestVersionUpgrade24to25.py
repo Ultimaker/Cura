ce50a6ebf1b568d8aef70edb5c51c72f2914a5ee
Ghostkeeper <rubend@tutanota.com>
2017-03-13 12:44:18 +0100

    Add tests for cfg_version
    These test if the output of cfg_version is as expected.
    
    Contributes to issue CURA-3479.

---
+++ plugins/VersionUpgrade/VersionUpgrade24to25/tests/TestVersionUpgrade24to25.py
@@ -0,0 +1,95 @@
+# Copyright (c) 2017 Ultimaker B.V.
+# Cura is released under the terms of the AGPLv3 or higher.
+
+import configparser #To check whether the appropriate exceptions are raised.
+import pytest #To register tests with.
+
+import VersionUpgrade24to25 #The module we're testing.
+
+##  Creates an instance of the upgrader to test with.
+@pytest.fixture
+def upgrader():
+    return VersionUpgrade24to25.VersionUpgrade24to25()
+
+test_cfg_version_good_data = [
+    {
+        "test_name": "Simple",
+        "file_data": """[general]
+version = 1
+""",
+        "version": 1
+    },
+    {
+        "test_name": "Other Data Around",
+        "file_data": """[nonsense]
+life = good
+
+[general]
+version = 3
+
+[values]
+layer_height = 0.12
+infill_sparse_density = 42
+""",
+        "version": 3
+    },
+    {
+        "test_name": "Negative Version", #Why not?
+        "file_data": """[general]
+version = -20
+""",
+        "version": -20
+    }
+]
+
+##  Tests the technique that gets the version number from CFG files.
+#
+#   \param data The parametrised data to test with. It contains a test name
+#   to debug with, the serialised contents of a CFG file and the correct
+#   version number in that CFG file.
+#   \param upgrader The instance of the upgrade class to test.
+@pytest.mark.parametrize("data", test_cfg_version_good_data)
+def test_cfg_version_good(data, upgrader):
+    version = upgrader.getCfgVersion(data["file_data"])
+    assert version == data["version"]
+
+test_cfg_version_bad_data = [
+    {
+        "test_name": "Empty",
+        "file_data": "",
+        "exception": configparser.Error #Explicitly not specified further which specific error we're getting, because that depends on the implementation of configparser.
+    },
+    {
+        "test_name": "No General",
+        "file_data": """[values]
+layer_height = 0.1337
+""",
+        "exception": configparser.Error
+    },
+    {
+        "test_name": "No Version",
+        "file_data": """[general]
+true = false
+""",
+        "exception": configparser.Error
+    },
+    {
+        "test_name": "Not a Number",
+        "file_data": """[general]
+version = not-a-text-version-number
+""",
+        "exception": ValueError
+    }
+]
+
+##  Tests whether getting a version number from bad CFG files gives an
+#   exception.
+#
+#   \param data The parametrised data to test with. It contains a test name
+#   to debug with, the serialised contents of a CFG file and the class of
+#   exception it needs to throw.
+#   \param upgrader The instance of the upgrader to test.
+@pytest.mark.parametrize("data", test_cfg_version_bad_data)
+def test_cfg_version_bad(data, upgrader):
+    with pytest.raises(data["exception"]):
+        upgrader.getCfgVersion(data["file_data"])
