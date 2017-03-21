3ee3e0aee31b490fce3caedf6b58bf8c3d074c70
Arjen Hiemstra <ahiemstra@heimr.nl>
2017-03-21 17:39:06 +0100

    Add a private class to GlobalStack that defines the expected indices of each container type

--- cura/Settings/GlobalStack.py
+++ cura/Settings/GlobalStack.py
@@ -31,3 +31,23 @@ global_stack_mime = MimeType(
 
 MimeTypeDatabase.addMimeType(global_stack_mime)
 ContainerRegistry.addContainerTypeByName(GlobalStack, "global_stack", global_stack_mime.name)
+
+class _ContainerIndexes:
+    UserChanges = 0
+    QualityChanges = 1
+    Quality = 2
+    Material = 3
+    Variant = 4
+    DefinitionChanges = 5
+    Definition = 6
+
+    # Simple hash map to map from index to "type" metadata entry
+    IndexTypeMap = {
+        UserChanges: "user",
+        QualityChanges: "quality_changes",
+        Quality: "quality",
+        Material: "material",
+        Variant: "variant",
+        DefinitionChanges: "definition_changes",
+        Definition: "definition",
+    }
