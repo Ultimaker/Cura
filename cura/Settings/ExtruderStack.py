dd8ea6175c409c8f27310305cfd7425b567ec1a3
Arjen Hiemstra <ahiemstra@heimr.nl>
2017-03-23 00:03:58 +0100

    Add properties for user changes etc. to ExtruderStack

--- cura/Settings/ExtruderStack.py
+++ cura/Settings/ExtruderStack.py
@@ -1,6 +1,8 @@
 # Copyright (c) 2017 Ultimaker B.V.
 # Cura is released under the terms of the AGPLv3 or higher.
 
+from PyQt5.QtCore import pyqtProperty, pyqtSignal, pyqtSlot
+
 from UM.MimeTypeDatabase import MimeType, MimeTypeDatabase
 from UM.Settings.ContainerStack import ContainerStack
 from UM.Settings.ContainerRegistry import ContainerRegistry
@@ -8,6 +10,30 @@ from UM.Settings.ContainerRegistry import ContainerRegistry
 class ExtruderStack(ContainerStack):
     def __init__(self, container_id, *args, **kwargs):
         super().__init__(container_id, *args, **kwargs)
+
+    @pyqtProperty(InstanceContainer)
+    def userChanges(self) -> InstanceContainer:
+        return self._containers[_ContainerIndexes.UserChanges]
+
+    @pyqtProperty(InstanceContainer)
+    def qualityChanges(self) -> InstanceContainer:
+        return self._containers[_ContainerIndexes.QualityChanges]
+
+    @pyqtProperty(InstanceContainer)
+    def quality(self) -> InstanceContainer:
+        return self._containers[_ContainerIndexes.Quality]
+
+    @pyqtProperty(InstanceContainer)
+    def material(self) -> InstanceContainer:
+        return self._containers[_ContainerIndexes.Material]
+
+    @pyqtProperty(InstanceContainer)
+    def variant(self) -> InstanceContainer:
+        return self._containers[_ContainerIndexes.Variant]
+
+    @pyqtProperty(DefinitionContainer)
+    def definition(self) -> DefinitionContainer:
+        return self._containers[_ContainerIndexes.Definition]
 
 extruder_stack_mime = MimeType(
     name = "application/x-cura-extruderstack",
@@ -17,3 +43,12 @@ extruder_stack_mime = MimeType(
 
 MimeTypeDatabase.addMimeType(extruder_stack_mime)
 ContainerRegistry.addContainerTypeByName(ExtruderStack, "extruder_stack", extruder_stack_mime.name)
+
+class _ContainerIndexes:
+    UserChanges = 0
+    QualityChanges = 1
+    Quality = 2
+    Material = 3
+    Variant = 4
+    Definition = 5
+
