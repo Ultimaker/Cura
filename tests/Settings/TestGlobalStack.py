72bbb8ec0c6437cfb1470779af20af72b144db4a
Ghostkeeper <rubend@tutanota.com>
2017-03-24 15:46:29 +0100

    Also test whether adding an extruder had any effect on the extruder list
    Contributes to issue CURA-3497.

--- tests/Settings/TestGlobalStack.py
+++ tests/Settings/TestGlobalStack.py
@@ -71,10 +71,18 @@ def test_addExtruder(global_stack):
 
     global_stack.definition = mock_definition
 
-    global_stack.addExtruder(unittest.mock.MagicMock())
-    global_stack.addExtruder(unittest.mock.MagicMock())
+    assert len(global_stack.extruders) == 0
+    first_extruder = unittest.mock.MagicMock()
+    global_stack.addExtruder(first_extruder)
+    assert len(global_stack.extruders) == 1
+    assert global_stack.extruders[0] == first_extruder
+    second_extruder = unittest.mock.MagicMock()
+    global_stack.addExtruder(second_extruder)
+    assert len(global_stack.extruders) == 2
+    assert global_stack.extruders[1] == second_extruder
     with pytest.raises(TooManyExtrudersError): #Should be limited to 2 extruders because of machine_extruder_count.
         global_stack.addExtruder(unittest.mock.MagicMock())
+    assert len(global_stack.extruders) == 2 #Didn't add the faulty extruder.
 
 ##  Tests whether the container types are properly enforced on the stack.
 #
