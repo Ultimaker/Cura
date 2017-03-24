9cb7c30fcb096edfc23bd31ae8351081c8e3d9ee
Ghostkeeper <rubend@tutanota.com>
2017-03-24 17:13:32 +0100

    Add tests for deserialising extruders with quality
    Only the greatest extruders with actual quality are deserialised...
    
    Contributes to issue CURA-3497.

--- tests/Settings/TestExtruderStack.py
+++ tests/Settings/TestExtruderStack.py
@@ -55,6 +55,26 @@ def test_addContainer(extruder_stack):
     with pytest.raises(InvalidOperationError):
         extruder_stack.addContainer(unittest.mock.MagicMock())
 
+@pytest.mark.parametrize("filename,                  quality_id", [
+                        ("Left.extruder.cfg",        "empty"),
+                        ("ExtruderLegacy.stack.cfg", "empty"),
+                        ("OnlyQuality.extruder.cfg", "some_instance"),
+                        ("Complete.extruder.cfg",    "some_quality")
+])
+def test_deserializeQuality(filename, quality_id, container_registry, extruder_stack):
+    serialized = readStack(filename)
+
+    #Mock the loading of the instance containers.
+    extruder_stack.findContainer = findSomeContainers
+    original_container_registry = UM.Settings.ContainerStack._containerRegistry
+    UM.Settings.ContainerStack._containerRegistry = container_registry #Always has all profiles you ask of.
+
+    extruder_stack.deserialize(serialized)
+    assert extruder_stack.quality.getId() == quality_id
+
+    #Restore.
+    UM.Settings.ContainerStack._containerRegistry = original_container_registry
+
 @pytest.mark.parametrize("filename,                         quality_changes_id", [
                         ("Left.extruder.cfg",               "empty"),
                         ("ExtruderLegacy.stack.cfg",        "empty"),
