73bd47a0a1841196e8e2a9126a0ddd8f7b2ea056
Ghostkeeper <rubend@tutanota.com>
2017-04-11 16:46:53 +0200

    Actually assert that the ID is correct
    Oops.
    
    Contributes to issue CURA-3497.

--- tests/Settings/TestGlobalStack.py
+++ tests/Settings/TestGlobalStack.py
@@ -575,7 +575,7 @@ def test_setPropertyOtherContainers(target_container, writable_global_stack):
 def test_setQualityByIdExists(global_stack, container_registry):
     container_registry.return_value = getInstanceContainer(container_type = "quality")
     global_stack.setQualityById("InstanceContainer")
-    global_stack.quality.getId() == "InstanceContainer"
+    assert global_stack.quality.getId() == "InstanceContainer"
 
 ##  Tests setting qualities by specifying an ID of a quality that doesn't exist.
 def test_setQualityByIdDoesntExist(global_stack):
