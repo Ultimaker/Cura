3b3d9c4dae1d03ccd5d812df835e6e9e17005785
Lipu Fei <lipu.fei815@gmail.com>
2017-05-17 15:53:24 +0200

    Minor code refactoring in ExtruderManager
    CURA-3756

--- cura/Settings/ExtruderManager.py
+++ cura/Settings/ExtruderManager.py
@@ -77,8 +77,9 @@ class ExtruderManager(QObject):
     @pyqtProperty("QVariantMap", notify=extrudersChanged)
     def extruderIds(self):
         map = {}
-        for position in self._extruder_trains[Application.getInstance().getGlobalContainerStack().getId()]:
-            map[position] = self._extruder_trains[Application.getInstance().getGlobalContainerStack().getId()][position].getId()
+        global_stack_id = Application.getInstance().getGlobalContainerStack().getId()
+        for position in self._extruder_trains[global_stack_id]:
+            map[position] = self._extruder_trains[global_stack_id][position].getId()
         return map
 
     @pyqtSlot(str, result = str)
