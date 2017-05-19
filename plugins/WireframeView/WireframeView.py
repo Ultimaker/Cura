aa40fd112a6543ff36733cd9936d34b498ed456f
Tim Kuipers <t.kuipers@ultimaker.com>
2017-05-19 15:53:32 +0200

    feat: color wireframe by edge overhang

--- plugins/WireframeView/WireframeView.py
+++ plugins/WireframeView/WireframeView.py
@@ -6,6 +6,7 @@ import os.path
 from UM.Application import Application
 from UM.Math.Color import Color
 from UM.PluginRegistry import PluginRegistry
+from UM.Preferences import Preferences
 from UM.Event import Event
 from UM.View.View import View
 from UM.View.RenderBatch import RenderBatch
@@ -13,6 +14,8 @@ from UM.Scene.Iterator.BreadthFirstIterator import BreadthFirstIterator
 
 from UM.View.RenderBatch import RenderBatch
 from UM.View.GL.OpenGL import OpenGL
+
+import math
 
 
 ## View used to display the edges of objects.
@@ -29,6 +32,30 @@ class WireframeView(View):
         if not self._wireframe_shader:
             self._wireframe_shader = OpenGL.getInstance().createShaderProgram(os.path.join(PluginRegistry.getInstance().getPluginPath("WireframeView"), "wireframe.shader"))
             self._wireframe_shader.setUniformValue("u_color", Color(*Application.getInstance().getTheme().getColor("xray").getRgb()))
+            self._wireframe_shader.setUniformValue("u_color_error", Color(*Application.getInstance().getTheme().getColor("xray_error").getRgb()))
+
+            global_container_stack = Application.getInstance().getGlobalContainerStack()
+            if global_container_stack:
+                multi_extrusion = global_container_stack.getProperty("machine_extruder_count", "value") > 1
+
+                if multi_extrusion:
+                    support_extruder_nr = global_container_stack.getProperty("support_extruder_nr", "value")
+                    support_angle_stack = ExtruderManager.getInstance().getExtruderStack(support_extruder_nr)
+                    if not support_angle_stack:
+                        support_angle_stack = global_container_stack
+                else:
+                    support_angle_stack = global_container_stack
+                if Preferences.getInstance().getValue("view/show_overhang"):
+                    angle = support_angle_stack.getProperty("support_angle", "value")
+                    # Make sure the overhang angle is valid before passing it to the shader
+                    # Note: if the overhang angle is set to its default value, it does not need to get validated (validationState = None)
+                    if angle is not None and global_container_stack.getProperty("support_angle", "validationState") in [None, ValidatorState.Valid]:
+                        self._enabled_shader.setUniformValue("u_overhangAngle", math.cos(math.radians(90 - angle)))
+                    else:
+                        self._enabled_shader.setUniformValue("u_overhangAngle", math.cos(math.radians(0))) #Overhang angle of 0 causes no area at all to be marked as overhang.
+                else:
+                    self._enabled_shader.setUniformValue("u_overhangAngle", math.cos(math.radians(0)))
+                    
 
         for node in BreadthFirstIterator(scene.getRoot()):
             if not node.render(renderer):
@@ -39,7 +66,8 @@ class WireframeView(View):
                                        blend_mode = RenderBatch.BlendMode.Additive,
                                        sort = -10,
                                        state_setup_callback = lambda gl: gl.glDepthFunc(gl.GL_ALWAYS),
-                                       state_teardown_callback = lambda gl: gl.glDepthFunc(gl.GL_LESS)
+                                       state_teardown_callback = lambda gl: gl.glDepthFunc(gl.GL_LESS),
+                                       mode = RenderBatch.RenderMode.TrianglesAdjacency
                     )
 
     def endRendering(self):
--- plugins/WireframeView/wireframe.shader
+++ plugins/WireframeView/wireframe.shader
@@ -20,12 +20,17 @@ fragment =
 vertex41core =
     #version 410
     uniform highp mat4 u_modelViewProjectionMatrix;
+    uniform highp mat4 u_normalMatrix;
 
     in highp vec4 a_vertex;
+    in highp vec4 a_normal;
+
+    out highp vec3 f_normal;
 
     void main()
     {
         gl_Position = u_modelViewProjectionMatrix * a_vertex;
+        f_normal = (u_normalMatrix * normalize(a_normal)).xyz;
     }
 
 
@@ -33,39 +38,54 @@ geometry41core =
     #version 410
 
     uniform highp mat4 u_modelViewProjectionMatrix;
+    uniform lowp float u_overhangAngle;
+    uniform lowp vec4 u_color;
+    uniform lowp vec4 u_color_error;
 
-    layout(triangles) in;
-    layout(line_strip, max_vertices = 4) out;
+    layout(triangles_adjacency) in;
+    layout(line_strip, max_vertices = 12) out;
 
+    in highp vec3 f_normal[];
+
+    out lowp vec4 e_color;
+
+    void makeEdge(vec4 from, vec4 to, vec3 n1, vec3 n2)
+    {
+        gl_Position = from;
+        EmitVertex();
+        gl_Position = to;
+        EmitVertex();
+        vec3 edge_normal = normalize(n1 + n2);
+        e_color = (-edge_normal.y > u_overhangAngle)? u_color_error : u_color;
+        EndPrimitive();
+    }
+    
     void main()
     {
-        gl_Position = gl_in[0].gl_Position;
-        EmitVertex();
-        gl_Position = gl_in[1].gl_Position;
-        EmitVertex();
-        gl_Position = gl_in[2].gl_Position;
-        EmitVertex();
-        gl_Position = gl_in[0].gl_Position;
-        EmitVertex();
-        EndPrimitive();
+        makeEdge(gl_in[0].gl_Position, gl_in[2].gl_Position, f_normal[0], f_normal[1]);
+        makeEdge(gl_in[2].gl_Position, gl_in[4].gl_Position, f_normal[0], f_normal[2]);
+        makeEdge(gl_in[4].gl_Position, gl_in[0].gl_Position, f_normal[0], f_normal[3]);
     }
 
 fragment41core =
     #version 410
-    uniform lowp vec4 u_color;
 
+    in lowp vec4 e_color;
     out vec4 frag_color;
 
     void main()
     {
-        frag_color = u_color;
+        frag_color = e_color;
     }
 
 [defaults]
 u_color = [0.02, 0.02, 0.02, 1.0]
+u_color_error = [1.0, 0.0, 0.0, 1.0]
 
 [bindings]
 u_modelViewProjectionMatrix = model_view_projection_matrix
+u_normalMatrix = normal_matrix
 
 [attributes]
 a_vertex = vertex
+a_normal = normal
