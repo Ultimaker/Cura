3f2a5b693a8224fbf0470c1ac6eb290b2c1f439d
Tim Kuipers <t.kuipers@ultimaker.com>
2017-05-20 10:46:19 +0200

    fix: retrieve overhang_angle correctly

--- plugins/WireframeView/WireframeView.py
+++ plugins/WireframeView/WireframeView.py
@@ -15,6 +15,10 @@ from UM.Scene.Iterator.BreadthFirstIterator import BreadthFirstIterator
 from UM.View.RenderBatch import RenderBatch
 from UM.View.GL.OpenGL import OpenGL
 
+from UM.Settings.Validator import ValidatorState
+from cura.Settings.ExtruderManager import ExtruderManager
+from cura.Settings.ExtrudersModel import ExtrudersModel
+
 import math
 
 
@@ -34,27 +38,27 @@ class WireframeView(View):
             self._wireframe_shader.setUniformValue("u_color", Color(*Application.getInstance().getTheme().getColor("xray").getRgb()))
             self._wireframe_shader.setUniformValue("u_color_error", Color(*Application.getInstance().getTheme().getColor("xray_error").getRgb()))
 
-            global_container_stack = Application.getInstance().getGlobalContainerStack()
-            if global_container_stack:
-                multi_extrusion = global_container_stack.getProperty("machine_extruder_count", "value") > 1
+        global_container_stack = Application.getInstance().getGlobalContainerStack()
+        if global_container_stack:
+            multi_extrusion = global_container_stack.getProperty("machine_extruder_count", "value") > 1
 
-                if multi_extrusion:
-                    support_extruder_nr = global_container_stack.getProperty("support_extruder_nr", "value")
-                    support_angle_stack = ExtruderManager.getInstance().getExtruderStack(support_extruder_nr)
-                    if not support_angle_stack:
-                        support_angle_stack = global_container_stack
-                else:
+            if multi_extrusion:
+                support_extruder_nr = global_container_stack.getProperty("support_extruder_nr", "value")
+                support_angle_stack = ExtruderManager.getInstance().getExtruderStack(support_extruder_nr)
+                if not support_angle_stack:
                     support_angle_stack = global_container_stack
-                if Preferences.getInstance().getValue("view/show_overhang"):
-                    angle = support_angle_stack.getProperty("support_angle", "value")
-                    # Make sure the overhang angle is valid before passing it to the shader
-                    # Note: if the overhang angle is set to its default value, it does not need to get validated (validationState = None)
-                    if angle is not None and global_container_stack.getProperty("support_angle", "validationState") in [None, ValidatorState.Valid]:
-                        self._enabled_shader.setUniformValue("u_overhangAngle", math.cos(math.radians(90 - angle)))
-                    else:
-                        self._enabled_shader.setUniformValue("u_overhangAngle", math.cos(math.radians(0))) #Overhang angle of 0 causes no area at all to be marked as overhang.
+            else:
+                support_angle_stack = global_container_stack
+            if Preferences.getInstance().getValue("view/show_overhang"):
+                angle = support_angle_stack.getProperty("support_angle", "value")
+                # Make sure the overhang angle is valid before passing it to the shader
+                # Note: if the overhang angle is set to its default value, it does not need to get validated (validationState = None)
+                if angle is not None and global_container_stack.getProperty("support_angle", "validationState") in [None, ValidatorState.Valid]:
+                    self._wireframe_shader.setUniformValue("u_overhangAngle", math.cos(math.radians(90 - angle)))
                 else:
-                    self._enabled_shader.setUniformValue("u_overhangAngle", math.cos(math.radians(0)))
+                    self._wireframe_shader.setUniformValue("u_overhangAngle", math.cos(math.radians(0))) #Overhang angle of 0 causes no area at all to be marked as overhang.
+            else:
+                self._wireframe_shader.setUniformValue("u_overhangAngle", math.cos(math.radians(0)))
                     
 
         for node in BreadthFirstIterator(scene.getRoot()):
