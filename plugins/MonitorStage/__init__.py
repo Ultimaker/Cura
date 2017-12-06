a5b99bd8624c8a2db866ab37c1f31563178993c8
ChrisTerBeke <c.terbeke@ultimaker.com>
2017-12-06 09:36:09 +0100

    Scaffold monitor stage

---
+++ plugins/MonitorStage/__init__.py
@@ -0,0 +1,19 @@
+# Copyright (c) 2017 Ultimaker B.V.
+# Cura is released under the terms of the LGPLv3 or higher.
+
+from . import MonitorStage
+
+from UM.i18n import i18nCatalog
+i18n_catalog = i18nCatalog("cura")
+
+def getMetaData():
+    return {
+        "stage": {
+            "name": i18n_catalog.i18nc("@item:inmenu", "Monitor"),
+            "weight": 0,
+            "icon": ""
+        }
+    }
+
+def register(app):
+    return { "stage": MonitorStage.MonitorStage() }
