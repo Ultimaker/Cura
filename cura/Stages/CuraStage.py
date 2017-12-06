ab5449b01e75bfa2e2834b2efcd7875707d10dcd
ChrisTerBeke <c.terbeke@ultimaker.com>
2017-12-06 09:10:19 +0100

    Add CuraStage

---
+++ cura/Stages/CuraStage.py
@@ -0,0 +1,12 @@
+from UM.Stage import Stage
+
+class CuraStage(Stage):
+
+    def __init__(self):
+        super().__init__()
+
+    def getMainView(self):
+        return None
+
+    def getSidebarView(self):
+        return None
