8bc9663294c7253f0bea512e16011b573dc47688
Jaime van Kessel <nallath@gmail.com>
2017-12-12 10:28:55 +0100

    Removed old & unused code
    CL-541

--- plugins/UM3NetworkPrinting/__init__.py
+++ plugins/UM3NetworkPrinting/__init__.py
@@ -1,6 +1,6 @@
-# Copyright (c) 2015 Ultimaker B.V.
+# Copyright (c) 2017 Ultimaker B.V.
 # Cura is released under the terms of the LGPLv3 or higher.
-from . import NetworkPrinterOutputDevicePlugin
+
 from . import DiscoverUM3Action
 from UM.i18n import i18nCatalog
 catalog = i18nCatalog("cura")
