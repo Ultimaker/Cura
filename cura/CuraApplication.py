b29047abd32195666b989adc50ce49545f073a44
ChrisTerBeke <c.terbeke@ultimaker.com>
2017-12-22 11:38:56 +0100

    Small fixes for disable user data triggering preferences window

--- cura/CuraApplication.py
+++ cura/CuraApplication.py
@@ -409,6 +409,14 @@ class CuraApplication(QtApplication):
         else:
             self.exit(0)
 
+    ##  Signal to connect preferences action in QML
+    showPreferencesWindow = pyqtSignal()
+
+    ##  Show the preferences window
+    @pyqtSlot()
+    def showPreferences(self):
+        self.showPreferencesWindow.emit()
+
     ## A reusable dialogbox
     #
     showMessageBox = pyqtSignal(str, str, str, str, int, int, arguments = ["title", "text", "informativeText", "detailedText", "buttons", "icon"])
@@ -1426,11 +1434,3 @@ class CuraApplication(QtApplication):
                     node = node.getParent()
 
                 Selection.add(node)
-
-
-    triggerPreferenceWindow = pyqtSignal()
-
-    # This event has a simple logic, display pereference window if user decided to disable "collect information"
-    @pyqtProperty(bool, notify = triggerPreferenceWindow)
-    def showMyTest(self):
-        return True
