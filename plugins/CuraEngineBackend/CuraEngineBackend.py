3f4a2c565cd76663089dc6422d2124847c210c28
Aleksei S <a.sasin@ultimaker.com>
2018-01-11 13:32:05 +0100

    Fix: after extruders switch do not trigger slicing, provided a new signal which is triggered after adding extruder and it subscribers for property changes

--- plugins/CuraEngineBackend/CuraEngineBackend.py
+++ plugins/CuraEngineBackend/CuraEngineBackend.py
@@ -88,7 +88,7 @@ class CuraEngineBackend(QObject, Backend):
         #
         self._global_container_stack = None
         Application.getInstance().globalContainerStackChanged.connect(self._onGlobalStackChanged)
-        Application.getInstance().getExtruderManager().activeExtruderChanged.connect(self._onGlobalStackChanged)
+        Application.getInstance().getExtruderManager().extrudersAdded.connect(self._onGlobalStackChanged)
         self._onGlobalStackChanged()
 
         Application.getInstance().stacksValidationFinished.connect(self._onStackErrorCheckFinished)
