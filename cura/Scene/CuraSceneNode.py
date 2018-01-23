cf556ccf8ff7116af0014ffc91c019b26cf4f364
Ghostkeeper <rubend@tutanota.com>
2018-01-23 11:20:06 +0100

    Remove unused import
    Contributes to issue CURA-4797.

--- cura/Scene/CuraSceneNode.py
+++ cura/Scene/CuraSceneNode.py
@@ -1,5 +1,7 @@
+# Copyright (c) 2018 Ultimaker B.V.
+# Cura is released under the terms of the LGPLv3 or higher.
+
 from UM.Application import Application
-from UM.Logger import Logger
 from UM.Scene.SceneNode import SceneNode
 from copy import deepcopy
 
