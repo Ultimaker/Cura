2d48fbb6088e9c44f7db7108ff3a1c1ddf41ca4c
Ghostkeeper <rubend@tutanota.com>
2018-01-29 12:51:23 +0100

    Rename file_type parameter to relation_type
    It was renamed in libCharon for better semantic meaning.
    
    Contributes to issue CURA-4872.

--- plugins/UFPWriter/UFPWriter.py
+++ plugins/UFPWriter/UFPWriter.py
@@ -21,7 +21,7 @@ class UFPWriter(MeshWriter):
         gcode = archive.getStream("/3D/model.gcode")
         gcode.write(gcode_textio.getvalue().encode("UTF-8"))
         gcode.close()
-        archive.addRelation(virtual_path = "/3D/model.gcode", file_type = "http://schemas.ultimaker.org/package/2018/relationships/gcode")
+        archive.addRelation(virtual_path = "/3D/model.gcode", relation_type = "http://schemas.ultimaker.org/package/2018/relationships/gcode")
 
         #Store the thumbnail.
         #TODO: Generate the thumbnail image. Below is just a placeholder.
@@ -29,6 +29,6 @@ class UFPWriter(MeshWriter):
         thumbnail = archive.getStream("/Metadata/thumbnail.png")
         thumbnail.write(open(os.path.join(os.path.dirname(__file__), "kitten.png"), "rb").read())
         thumbnail.close()
-        archive.addRelation(virtual_path = "/Metadata/thumbnail.png", file_type = "http://schemas.openxmlformats.org/package/2006/relationships/metadata/thumbnail")
+        archive.addRelation(virtual_path = "/Metadata/thumbnail.png", relation_type = "http://schemas.openxmlformats.org/package/2006/relationships/metadata/thumbnail")
 
         archive.close()
