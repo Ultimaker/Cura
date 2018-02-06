b4a1b97eda62dcbcf6da02ed44ac07e4973daa84
Jaime van Kessel <nallath@gmail.com>
2018-02-06 14:37:39 +0100

    Removed warning that caused massive spam

--- plugins/XmlMaterialProfile/XmlMaterialProfile.py
+++ plugins/XmlMaterialProfile/XmlMaterialProfile.py
@@ -544,7 +544,6 @@ class XmlMaterialProfile(InstanceContainer):
                 for machine_id in machine_id_list:
                     definitions = ContainerRegistry.getInstance().findDefinitionContainersMetadata(id = machine_id)
                     if not definitions:
-                        Logger.log("w", "No definition found for machine ID %s", machine_id)
                         continue
 
                     definition = definitions[0]
@@ -777,7 +776,6 @@ class XmlMaterialProfile(InstanceContainer):
                 for machine_id in machine_id_list:
                     definition_metadata = ContainerRegistry.getInstance().findDefinitionContainersMetadata(id = machine_id)
                     if not definition_metadata:
-                        Logger.log("w", "No definition found for machine ID %s", machine_id)
                         continue
 
                     definition_metadata = definition_metadata[0]
