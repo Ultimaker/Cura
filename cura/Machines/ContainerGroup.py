d99bacb2d2ebe586ed1e7a2f6c68d22b4b5656cd
Lipu Fei <lipu.fei815@gmail.com>
2018-02-13 21:13:44 +0100

    WIP: Create ContainerNode and ContainerGroup

--- cura/Machines/ContainerGroup.py
+++ cura/Machines/ContainerGroup.py
@@ -1,10 +1,13 @@
+from typing import Optional
+
 from PyQt5.Qt import QObject
 
 from cura.Machines.ContainerNode import ContainerNode
 
 
 class ContainerGroup(QObject):
+
     def __init__(self, parent = None):
         super().__init__(parent)
-        self.node_for_global = None  # type: ContainerNode
+        self.node_for_global = None  # type: Optional[ContainerNode]
         self.nodes_for_extruders = dict()
