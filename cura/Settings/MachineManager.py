ab4cfa5fbbfa75075045ac021cb23a76cf4db9e2
Lipu Fei <lipu.fei815@gmail.com>
2018-02-28 12:49:37 +0100

    Use userChanges instead of getContainer(0)
    CURA-4916

--- cura/Settings/MachineManager.py
+++ cura/Settings/MachineManager.py
@@ -1494,5 +1494,5 @@ class MachineManager(QObject):
     @pyqtSlot(str, str, str)
     def setSettingForAllExtruders(self, setting_name: str, property_name: str, property_value: str):
         for key, extruder in self._global_container_stack.extruders.items():
-            container = extruder.getContainer(0)
+            container = extruder.userChanges
             container.setProperty(setting_name, property_name, property_value)
