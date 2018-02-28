025239c26ea99d7c9ea982221b5b1e9daed1bd85
Lipu Fei <lipu.fei815@gmail.com>
2018-02-28 11:24:31 +0100

    Add documentation for ContainerGroup

--- cura/Machines/ContainerGroup.py
+++ cura/Machines/ContainerGroup.py
@@ -8,6 +8,19 @@ from PyQt5.Qt import QObject, pyqtSlot
 from cura.Machines.ContainerNode import ContainerNode
 
 
+#
+# A ContainerGroup represents a group of containers that must be applied to each ContainerStack when it's used.
+# Some concrete examples are Quality and QualityChanges: when we select quality type "normal", this quality type
+# must be applied to all stacks in a machine, although each stack can have different containers. Use an Ultimaker 3
+# as an example, suppose we choose quality type "normal", the actual InstanceConstainers on each stack may look
+# as below:
+#                       GlobalStack         ExtruderStack 1         ExtruderStack 2
+# quality container:    um3_global_normal   um3_aa04_pla_normal     um3_aa04_abs_normal
+#
+# This ContainerGroup is mainly used in quality and quality_changes to group the containers that can be applied to
+# a machine, so when a quality/custom quality is selected, the container can be directly applied to each stack instead
+# of looking them up again.
+#
 class ContainerGroup(QObject):
 
     def __init__(self, name: str, parent = None):
