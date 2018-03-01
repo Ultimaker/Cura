31d1a3b5dbec39ea93cf78e0117dddfcce944d9b
Lipu Fei <lipu.fei815@gmail.com>
2018-03-01 16:31:00 +0100

    Code style fix

--- cura/Machines/MaterialGroup.py
+++ cura/Machines/MaterialGroup.py
@@ -1,6 +1,7 @@
 # Copyright (c) 2018 Ultimaker B.V.
 # Cura is released under the terms of the LGPLv3 or higher.
 
+
 #
 # A MaterialGroup represents a group of material InstanceContainers that are derived from a single material profile.
 # The main InstanceContainer which has the ID of the material profile file name is called the "root_material". For
