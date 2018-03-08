676f9b84747337eaf80262c5d2e632c60d91d272
Ghostkeeper <rubend@tutanota.com>
2018-03-08 15:18:32 +0100

    Switch if-statement around for fail checking
    This reduces indent and makes the control flow a bit easier to read in my opinion.
    
    Contributes to issue CURA-5061.

--- plugins/UltimakerMachineActions/BedLevelMachineAction.py
+++ plugins/UltimakerMachineActions/BedLevelMachineAction.py
@@ -10,6 +10,7 @@ from UM.FlameProfiler import pyqtSlot
 
 from UM.Application import Application
 from UM.i18n import i18nCatalog
+from UM.Logger import Logger
 catalog = i18nCatalog("cura")
 
 
@@ -32,10 +33,13 @@ class BedLevelMachineAction(MachineAction):
     def startBedLeveling(self):
         self._bed_level_position = 0
         printer_output_devices = self._getPrinterOutputDevices()
-        if printer_output_devices:
-            printer_output_devices[0].homeBed()
-            printer_output_devices[0].moveHead(0, 0, 3)
-            printer_output_devices[0].homeHead()
+        if not printer_output_devices:
+            Logger.log("e", "Can't start bed levelling. The printer connection seems to have been lost.")
+            return
+
+        printer_output_devices[0].homeBed()
+        printer_output_devices[0].moveHead(0, 0, 3)
+        printer_output_devices[0].homeHead()
 
     def _getPrinterOutputDevices(self) -> List[PrinterOutputDevice]:
         return [printer_output_device for printer_output_device in Application.getInstance().getOutputDeviceManager().getOutputDevices() if isinstance(printer_output_device, PrinterOutputDevice)]
@@ -43,26 +47,29 @@ class BedLevelMachineAction(MachineAction):
     @pyqtSlot()
     def moveToNextLevelPosition(self):
         output_devices = self._getPrinterOutputDevices()
-        if output_devices:  # We found at least one output device
-            output_device = output_devices[0]
+        if not output_devices: #No output devices. Can't move.
+            Logger.log("e", "Can't move to the next position. The printer connection seems to have been lost.")
+            return
 
-            if self._bed_level_position == 0:
-                output_device.moveHead(0, 0, 3)
-                output_device.homeHead()
-                output_device.moveHead(0, 0, 3)
-                output_device.moveHead(Application.getInstance().getGlobalContainerStack().getProperty("machine_width", "value") - 10, 0, 0)
-                output_device.moveHead(0, 0, -3)
-                self._bed_level_position += 1
-            elif self._bed_level_position == 1:
-                output_device.moveHead(0, 0, 3)
-                output_device.moveHead(-Application.getInstance().getGlobalContainerStack().getProperty("machine_width", "value" ) / 2, Application.getInstance().getGlobalContainerStack().getProperty("machine_depth", "value") - 10, 0)
-                output_device.moveHead(0, 0, -3)
-                self._bed_level_position += 1
-            elif self._bed_level_position == 2:
-                output_device.moveHead(0, 0, 3)
-                output_device.moveHead(-Application.getInstance().getGlobalContainerStack().getProperty("machine_width", "value") / 2 + 10, -(Application.getInstance().getGlobalContainerStack().getProperty("machine_depth", "value") + 10), 0)
-                output_device.moveHead(0, 0, -3)
-                self._bed_level_position += 1
-            elif self._bed_level_position >= 3:
-                output_device.sendCommand("M18") # Turn off all motors so the user can move the axes
-                self.setFinished()
+        output_device = output_devices[0]
+
+        if self._bed_level_position == 0:
+            output_device.moveHead(0, 0, 3)
+            output_device.homeHead()
+            output_device.moveHead(0, 0, 3)
+            output_device.moveHead(Application.getInstance().getGlobalContainerStack().getProperty("machine_width", "value") - 10, 0, 0)
+            output_device.moveHead(0, 0, -3)
+            self._bed_level_position += 1
+        elif self._bed_level_position == 1:
+            output_device.moveHead(0, 0, 3)
+            output_device.moveHead(-Application.getInstance().getGlobalContainerStack().getProperty("machine_width", "value" ) / 2, Application.getInstance().getGlobalContainerStack().getProperty("machine_depth", "value") - 10, 0)
+            output_device.moveHead(0, 0, -3)
+            self._bed_level_position += 1
+        elif self._bed_level_position == 2:
+            output_device.moveHead(0, 0, 3)
+            output_device.moveHead(-Application.getInstance().getGlobalContainerStack().getProperty("machine_width", "value") / 2 + 10, -(Application.getInstance().getGlobalContainerStack().getProperty("machine_depth", "value") + 10), 0)
+            output_device.moveHead(0, 0, -3)
+            self._bed_level_position += 1
+        elif self._bed_level_position >= 3:
+            output_device.sendCommand("M18") # Turn off all motors so the user can move the axes
+            self.setFinished()
