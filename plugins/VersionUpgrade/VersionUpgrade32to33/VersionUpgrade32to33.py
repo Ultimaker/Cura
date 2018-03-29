50b4bac672988b48bea2d8608ea668fb0e6e9950
Ghostkeeper <rubend@tutanota.com>
2018-03-29 11:56:47 +0200

    Upgrade quality profile names
    These files have been renamed.
    
    Contributes to issue CURA-5177.

--- plugins/VersionUpgrade/VersionUpgrade32to33/VersionUpgrade32to33.py
+++ plugins/VersionUpgrade/VersionUpgrade32to33/VersionUpgrade32to33.py
@@ -53,6 +53,11 @@ _EXTRUDER_TO_POSITION = {
     "vertex_k8400_dual_2nd": 1
 }
 
+_RENAMED_QUALITY_PROFILES = {
+    "low": "fast",
+    "um2_low": "um2_fast"
+}
+
 ##  Upgrades configurations from the state they were in at version 3.2 to the
 #   state they should be in at version 3.3.
 class VersionUpgrade32to33(VersionUpgrade):
@@ -93,6 +98,10 @@ class VersionUpgrade32to33(VersionUpgrade):
 
         #Update version number.
         parser["general"]["version"] = "4"
+
+        #Update the name of the quality profile.
+        if parser["containers"]["2"] in _RENAMED_QUALITY_PROFILES:
+            parser["containers"]["2"] = _RENAMED_QUALITY_PROFILES[parser["containers"]["2"]]
 
         result = io.StringIO()
         parser.write(result)
