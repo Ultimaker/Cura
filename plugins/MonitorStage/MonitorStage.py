a11bf31ce6cb549dc987955628c63df9c8425beb
fieldOfView <aldo@fieldofview.com>
2018-03-29 13:47:46 +0200

    Use separate QML files for prepare and monitor stage

--- plugins/MonitorStage/MonitorStage.py
+++ plugins/MonitorStage/MonitorStage.py
@@ -73,6 +73,5 @@ class MonitorStage(CuraStage):
         self.addDisplayComponent("main", main_component_path)
 
     def _updateSidebar(self):
-        # TODO: currently the sidebar component for prepare and monitor stages is the same, this will change with the printer output device refactor!
-        sidebar_component_path = os.path.join(Resources.getPath(Application.getInstance().ResourceTypes.QmlFiles), "Sidebar.qml")
+        sidebar_component_path = os.path.join(Resources.getPath(Application.getInstance().ResourceTypes.QmlFiles), "MonitorSidebar.qml")
         self.addDisplayComponent("sidebar", sidebar_component_path)
