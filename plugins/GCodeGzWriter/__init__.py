9e7c417801509788d8fe47e0347dd7f8d7ac8b94
Lipu Fei <lipu.fei815@gmail.com>
2018-04-18 15:59:28 +0200

    Revert "Fix gcode.gz double extension problem on Mac"
    This reverts commit fa90fcf18e26871df030ddb8371cdc05f4d4b6cc.
    
    CURA-5228

--- plugins/GCodeGzWriter/__init__.py
+++ plugins/GCodeGzWriter/__init__.py
@@ -1,19 +1,16 @@
 # Copyright (c) 2018 Ultimaker B.V.
 # Cura is released under the terms of the LGPLv3 or higher.
 
-from UM.i18n import i18nCatalog
-from UM.Platform import Platform
-
 from . import GCodeGzWriter
 
+from UM.i18n import i18nCatalog
 catalog = i18nCatalog("cura")
 
 def getMetaData():
-    file_extension = "gz" if Platform.isOSX() else "gcode.gz"
     return {
         "mesh_writer": {
             "output": [{
-                "extension": file_extension,
+                "extension": "gcode.gz",
                 "description": catalog.i18nc("@item:inlistbox", "Compressed G-code File"),
                 "mime_type": "application/gzip",
                 "mode": GCodeGzWriter.GCodeGzWriter.OutputMode.BinaryMode
