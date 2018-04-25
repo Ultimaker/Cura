cbc45bffd0eb30b57d4d9c853f2ba70e4a34e53e
Lipu Fei <lipu.fei815@gmail.com>
2018-04-25 16:43:59 +0200

    Should use .gcode.gz

--- plugins/GCodeGzReader/GCodeGzReader.py
+++ plugins/GCodeGzReader/GCodeGzReader.py
@@ -15,7 +15,7 @@ class GCodeGzReader(MeshReader):
 
     def __init__(self):
         super().__init__()
-        self._supported_extensions = [".gz"]
+        self._supported_extensions = [".gcode.gz"]
 
     def read(self, file_name):
         with open(file_name, "rb") as file:
