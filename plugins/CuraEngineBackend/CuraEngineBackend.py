439ea79c6029454a2f988e29b459e65754a166fa
Lipu Fei <lipu.fei815@gmail.com>
2018-06-13 11:09:18 +0200

    Add message for unable to slice due to disabled extruders
    CURA-5456

--- plugins/CuraEngineBackend/CuraEngineBackend.py
+++ plugins/CuraEngineBackend/CuraEngineBackend.py
@@ -379,6 +379,14 @@ class CuraEngineBackend(QObject, Backend):
             else:
                 self.backendStateChange.emit(BackendState.NotStarted)
 
+        if job.getResult() == StartSliceJob.StartJobResult.ObjectsWithDisabledExtruder:
+            self._error_message = Message(catalog.i18nc("@info:status", "Unable to slice because there are objects associated with disabled Extruder %s." % job.getMessage()),
+                                          title = catalog.i18nc("@info:title", "Unable to slice"))
+            self._error_message.show()
+            self.backendStateChange.emit(BackendState.Error)
+            self.backendError.emit(job)
+            return
+
         if job.getResult() == StartSliceJob.StartJobResult.NothingToSlice:
             if Application.getInstance().platformActivity:
                 self._error_message = Message(catalog.i18nc("@info:status", "Nothing to slice because none of the models fit the build volume. Please scale or rotate models to fit."),
