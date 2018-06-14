a76b0db7cae04da34f0e6fbc5fd34b3d17627a32
Ghostkeeper <rubend@tutanota.com>
2018-06-14 20:01:25 +0200

    Add empty tool plug-in for custom support
    With a very nice brush icon.
    
    Contributes to issue CURA-5482.

---
+++ plugins/CustomSupport/CustomSupport.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2018 Ultimaker B.V.
+# Cura is released under the terms of the LGPLv3 or higher.
+
+from UM.Tool import Tool #The interface we're implementing.
+
+class CustomSupport(Tool):
+    pass
---
+++ plugins/CustomSupport/__init__.py
@@ -0,0 +1,22 @@
+# Copyright (c) 2018 Ultimaker B.V.
+# Cura is released under the terms of the LGPLv3 or higher.
+
+from .CustomSupport import CustomSupport
+
+from UM.i18n import i18nCatalog
+i18n_catalog = i18nCatalog("cura")
+
+def getMetaData():
+    return {
+        "tool": {
+            "name": i18n_catalog.i18nc("@label", "Custom Support"),
+            "description": i18n_catalog.i18nc("@info:tooltip", "Draw and erase areas where support needs to be generated."),
+            "icon": "support_brush.svg",
+            "weight": 5
+        }
+    }
+
+def register(app):
+    return {
+        "tool": CustomSupport()
+    }
