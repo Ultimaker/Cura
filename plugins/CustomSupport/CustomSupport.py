2be98ea0c10430cd120add231d627f790cb0d7ce
Ghostkeeper <rubend@tutanota.com>
2018-06-14 20:04:30 +0200

    Add shortcut key
    Press S for Support.
    
    Contributes to issue CURA-5482.

--- plugins/CustomSupport/CustomSupport.py
+++ plugins/CustomSupport/CustomSupport.py
@@ -1,7 +1,11 @@
 # Copyright (c) 2018 Ultimaker B.V.
 # Cura is released under the terms of the LGPLv3 or higher.
 
+from PyQt5.QtCore import Qt #For shortcut keys.
+
 from UM.Tool import Tool #The interface we're implementing.
 
 class CustomSupport(Tool):
-    pass
+    def __init__(self):
+        super().__init__()
+        self._shortcut_key = Qt.Key_S
