b83e18307a69630d4158abb245e3ed0c3db72b02
Ghostkeeper <rubend@tutanota.com>
2018-06-15 09:44:36 +0200

    Switch to Solid View when using Custom Support tool
    This is the one you need to be able to see what you're drawing.
    
    Contributes to issue CURA-5482.

--- plugins/CustomSupport/CustomSupport.py
+++ plugins/CustomSupport/CustomSupport.py
@@ -3,9 +3,23 @@
 
 from PyQt5.QtCore import Qt #For shortcut keys.
 
+from UM.Application import Application #To change the active view.
+from UM.Event import Event #To register mouse movements.
 from UM.Tool import Tool #The interface we're implementing.
 
 class CustomSupport(Tool):
     def __init__(self):
         super().__init__()
         self._shortcut_key = Qt.Key_S
+        self._previous_view = None
+
+    def event(self, event: Event):
+        if event.type == Event.ToolActivateEvent:
+            active_view = Application.getInstance().getController().getActiveView()
+            if active_view is not None:
+                self._previous_view = active_view.getPluginId()
+            Application.getInstance().getController().setActiveView("SolidView")
+        elif event.type == Event.ToolDeactivateEvent:
+            if self._previous_view is not None:
+                Application.getInstance().getController().setActiveView(self._previous_view)
+                self._previous_view = None
