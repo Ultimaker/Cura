8d9c65aad5a71821bf1820e8fbdf924f382c911b
Ghostkeeper <rubend@tutanota.com>
2018-06-18 15:09:14 +0200

    Start drawing support when user clicks mouse
    This draws something on an invisible buffer now. It's not actually visible that something happens.
    
    Contributes to issue CURA-5482.

--- plugins/CustomSupport/CustomSupport.py
+++ plugins/CustomSupport/CustomSupport.py
@@ -1,26 +1,50 @@
 # Copyright (c) 2018 Ultimaker B.V.
 # Cura is released under the terms of the LGPLv3 or higher.
 
-from PyQt5.QtCore import Qt #For shortcut keys.
+from PyQt5.QtCore import Qt #For shortcut keys and colours.
+from PyQt5.QtGui import QBrush, QImage, QPainter, QPen #Drawing on a temporary buffer until we're ready to process the area of custom support.
 from typing import Optional
 
-from UM.Application import Application #To change the active view.
-from UM.Event import Event #To register mouse movements.
+from UM.Qt.QtApplication import QtApplication #To change the active view.
+from UM.Event import Event, MouseEvent #To register mouse movements.
 from UM.Tool import Tool #The interface we're implementing.
 
 class CustomSupport(Tool):
+    brush_size = 10 #Diameter of the brush.
+
     def __init__(self):
         super().__init__()
         self._shortcut_key = Qt.Key_S
         self._previous_view = None #type: Optional[str] #This tool forces SolidView. When the tool is disabled, it goes back to the original view.
 
+        self._draw_buffer = None #type: Optional[QImage] #An image to temporarily draw support on until we've processed the draw command completely.
+        self._painter = None #type: Optional[QPainter] #A pen tool that paints on the draw buffer.
+        self._last_x = 0 #type: int #The last position that was drawn in the previous frame, if we are currently drawing.
+        self._last_y = 0 #type: int #The last position that was drawn in the previous frame, if we are currently drawing.
+        self._endcap_pen = QPen(Qt.white) #type: QPen #Pen to use for the end caps of the drawn line. This draws a circle when pressing and releasing the mouse.
+        self._line_pen = QPen(Qt.white) #type: QPen #Pen to use for drawing connecting lines while dragging the mouse.
+        self._line_pen.setWidth(self.brush_size)
+        self._line_pen.setCapStyle(Qt.RoundCap)
+
     def event(self, event: Event):
         if event.type == Event.ToolActivateEvent:
-            active_view = Application.getInstance().getController().getActiveView()
+            active_view = QtApplication.getInstance().getController().getActiveView()
             if active_view is not None:
                 self._previous_view = active_view.getPluginId()
-            Application.getInstance().getController().setActiveView("SolidView")
+            QtApplication.getInstance().getController().setActiveView("SolidView")
         elif event.type == Event.ToolDeactivateEvent:
             if self._previous_view is not None:
-                Application.getInstance().getController().setActiveView(self._previous_view)
+                QtApplication.getInstance().getController().setActiveView(self._previous_view)
                 self._previous_view = None
+
+        elif event.type == Event.MousePressEvent and MouseEvent.LeftButton in event.buttons:
+            #Reset the draw buffer and start painting.
+            self._draw_buffer = QImage(QtApplication.getInstance().getMainWindow().width(), QtApplication.getInstance().getMainWindow().height(), QImage.Format_Grayscale8)
+            self._draw_buffer.fill(Qt.black)
+            self._painter = QPainter(self._draw_buffer)
+            self._painter.setBrush(QBrush(Qt.white))
+            self._painter.setPen(self._endcap_pen)
+            self._last_x = QtApplication.getInstance().getMainWindow().mouseX
+            self._last_y = QtApplication.getInstance().getMainWindow().mouseY
+            self._painter.drawEllipse(self._last_x - self.brush_size / 2, self._last_y - self.brush_size / 2, self.brush_size, self.brush_size) #Paint an initial ellipse at the spot you're clicking.
+            self._draw_buffer.save("/tmp/test.png", "PNG") #For debugging.
