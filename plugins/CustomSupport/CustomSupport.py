06a52384d8db37485d4ea1c8ab208f33e04aa0c7
Ghostkeeper <rubend@tutanota.com>
2018-06-18 15:57:07 +0200

    Move getting cursor coordinates to separate function
    Removes some repeated code.
    
    Contributes to issue CURA-5482.

--- plugins/CustomSupport/CustomSupport.py
+++ plugins/CustomSupport/CustomSupport.py
@@ -3,7 +3,7 @@
 
 from PyQt5.QtCore import Qt #For shortcut keys and colours.
 from PyQt5.QtGui import QBrush, QImage, QPainter, QPen #Drawing on a temporary buffer until we're ready to process the area of custom support.
-from typing import Optional
+from typing import Optional, Tuple
 
 from UM.Qt.QtApplication import QtApplication #To change the active view.
 from UM.Event import Event, MouseEvent #To register mouse movements.
@@ -20,7 +20,7 @@ class CustomSupport(Tool):
         self._draw_buffer = None #type: Optional[QImage] #An image to temporarily draw support on until we've processed the draw command completely.
         self._painter = None #type: Optional[QPainter] #A pen tool that paints on the draw buffer.
         self._last_x = 0 #type: int #The last position that was drawn in the previous frame, if we are currently drawing.
-        self._last_y = 0 #type: int #The last position that was drawn in the previous frame, if we are currently drawing.
+        self._last_y = 0 #type: int
         self._endcap_pen = QPen(Qt.white) #type: QPen #Pen to use for the end caps of the drawn line. This draws a circle when pressing and releasing the mouse.
         self._line_pen = QPen(Qt.white) #type: QPen #Pen to use for drawing connecting lines while dragging the mouse.
         self._line_pen.setWidth(self.brush_size)
@@ -46,22 +46,25 @@ class CustomSupport(Tool):
             self._painter = QPainter(self._draw_buffer)
             self._painter.setBrush(QBrush(Qt.white))
             self._painter.setPen(self._endcap_pen)
-            self._last_x = QtApplication.getInstance().getMainWindow().mouseX
-            self._last_y = QtApplication.getInstance().getMainWindow().mouseY
+            self._last_x, self._last_y = self._cursorCoordinates()
             self._painter.drawEllipse(self._last_x - self.brush_size / 2, self._last_y - self.brush_size / 2, self.brush_size, self.brush_size) #Paint an initial ellipse at the spot you're clicking.
         elif event.type == Event.MouseReleaseEvent and MouseEvent.LeftButton in event.buttons:
             #Complete drawing.
             #TODO: Use the current buffer to place actual support.
-            self._last_x = QtApplication.getInstance().getMainWindow().mouseX
-            self._last_y = QtApplication.getInstance().getMainWindow().mouseY
+            self._last_x, self._last_y = self._cursorCoordinates()
             self._painter.setPen(self._endcap_pen)
             self._painter.drawEllipse(self._last_x - self.brush_size / 2, self._last_y - self.brush_size / 2, self.brush_size, self.brush_size) #Paint another ellipse when you're releasing as endcap.
             self._painter = None
             self._draw_buffer.save("/tmp/test.png", "PNG") #For debugging.
         elif event.type == Event.MouseMoveEvent and self._painter is not None: #While dragging.
             self._painter.setPen(self._line_pen)
-            new_x = QtApplication.getInstance().getMainWindow().mouseX
-            new_y = QtApplication.getInstance().getMainWindow().mouseY
+            new_x, new_y = self._cursorCoordinates()
             self._painter.drawLine(self._last_x, self._last_y, new_x, new_y)
             self._last_x = new_x
             self._last_y = new_y
+
+    ##  Get the current mouse coordinates.
+    #   \return A tuple containing first the X coordinate and then the Y
+    #   coordinate.
+    def _cursorCoordinates(self) -> Tuple[int, int]:
+        return QtApplication.getInstance().getMainWindow().mouseX, QtApplication.getInstance().getMainWindow().mouseY
