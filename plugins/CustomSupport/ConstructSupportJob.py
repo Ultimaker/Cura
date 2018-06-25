de4cc5d49fbd44bda94838ddc511342f9b25c68c
Ghostkeeper <rubend@tutanota.com>
2018-06-25 16:29:24 +0200

    Correct positions to create vertices
    It doesn't seem to be all correct yet, but it's much better.
    
    Contributes to issue CURA-5482.

--- plugins/CustomSupport/ConstructSupportJob.py
+++ plugins/CustomSupport/ConstructSupportJob.py
@@ -30,6 +30,8 @@ class ConstructSupportJob(Job):
         self._camera_projection = camera.getProjectionMatrix()
         self._camera_transformation = camera.getWorldTransformation()
         self._camera_position = camera.getPosition()
+        self._camera_viewport = numpy.array([camera.getViewportHeight(), camera.getViewportWidth()])
+        self._window_size = numpy.array(camera.getWindowSize())
 
     def run(self):
         Logger.log("d", "Constructing/removing support.")
@@ -38,32 +40,37 @@ class ConstructSupportJob(Job):
         depth = qimage2ndarray.recarray_view(self._depth_image)
         depth.a = 0 #Discard alpha channel.
         depth = depth.view(dtype = numpy.int32).astype(numpy.float32) / 1000 #Conflate the R, G and B channels to one 24-bit (cast to 32) float. Divide by 1000 to get mm.
-        support_positions_2d = numpy.array(numpy.where(numpy.bitwise_and(to_support == 255, depth < 16777))) #All the 2D coordinates on the screen where we want support.
+        support_positions_2d = numpy.array(numpy.where(numpy.bitwise_and(to_support == 255, depth < 16777))) #All the 2D coordinates on the screen where we want support. The 16777 is for points that don't land on a model.
         support_depths = numpy.take(depth, support_positions_2d[0, :] * depth.shape[1] + support_positions_2d[1, :]) #The depth at those pixels.
+        support_positions_2d = support_positions_2d.transpose() #We want rows with pixels, not columns with pixels.
+        support_positions_2d[:, [0, 1]] = support_positions_2d[:, [1, 0]] #Swap columns to get OpenGL's coordinate system.
+        support_positions_2d = support_positions_2d * 2.0 / self._window_size - 1.0 #Scale to view coordinates (range -1 to 1).
         inverted_projection = numpy.linalg.inv(self._camera_projection.getData().copy())
         transformation = self._camera_transformation.getData()
 
         #For each pixel, get the near and far plane.
-        near = numpy.ndarray((4, support_positions_2d.shape[1]))
+        near = numpy.ndarray((support_positions_2d.shape[0], 4))
         near.fill(1)
         near[0: support_positions_2d.shape[0], 0: support_positions_2d.shape[1]] = support_positions_2d
-        near[2,:].fill(-1)
-        near = numpy.dot(inverted_projection, near)
+        near[:, 2].fill(-1)
+        near = numpy.dot(inverted_projection, near.transpose())
         near = numpy.dot(transformation, near)
         near = near[0:3] / near[3]
-        far = numpy.ndarray((4, support_positions_2d.shape[1]))
+        far = numpy.ndarray((support_positions_2d.shape[0], 4))
         far.fill(1)
         far[0: support_positions_2d.shape[0], 0: support_positions_2d.shape[1]] = support_positions_2d
-        far = numpy.dot(inverted_projection, far)
+        far = numpy.dot(inverted_projection, far.transpose())
         far = numpy.dot(transformation, far)
         far = far[0:3] / far[3]
 
         #Direction is from near plane pixel to far plane pixel, normalised.
-        direction = far - near
+        direction = near - far
         direction /= numpy.linalg.norm(direction, axis = 0)
 
         #Final position is in the direction of the pixel, moving with <depth> mm away from the camera position.
-        support_positions_3d = (direction * support_depths).transpose() + self._camera_position.getData()
+        support_positions_3d = support_depths * direction
+        support_positions_3d = support_positions_3d.transpose()
+        support_positions_3d = support_positions_3d + self._camera_position.getData()
 
         #Create the 3D mesh.
         builder = MeshBuilder()
@@ -71,11 +78,10 @@ class ConstructSupportJob(Job):
             builder.addDiamond(1, 2, 1, center = Vector(x = position[0], y = position[1], z = position[2]))
 
         #Create the scene node and add it to the scene.
-        mesh_data = builder.build()
         scene = CuraApplication.getInstance().getController().getScene()
         new_node = CuraSceneNode(parent = scene.getRoot(), name = "CustomSupport")
         new_node.setSelectable(True)
-        new_node.setMeshData(mesh_data)
+        new_node.setMeshData(builder.build())
         new_node.addDecorator(BuildPlateDecorator(CuraApplication.getInstance().getMultiBuildPlateModel().activeBuildPlate))
         new_node.addDecorator(SliceableObjectDecorator())
         operation = AddSceneNodeOperation(new_node, scene.getRoot())
