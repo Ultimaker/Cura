7cbadf011dcca8580c47a425fb3f4207780bd949
fieldOfView <aldo@fieldofview.com>
2018-07-22 18:14:22 +0200

    Add CuraVersion to force version for bb cura

---
+++ cura/CuraVersion.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2015 Ultimaker B.V.
+# Cura is released under the terms of the LGPLv3 or higher.
+
+CuraVersion = "3.4.2"
+CuraBuildType = "BlackBelt"
+CuraDebugMode = False
+CuraSDKVersion = "4"
+CuraCloudAPIRoot = "https://api.ultimaker.com"
+CuraCloudAPIVersion = "1"
