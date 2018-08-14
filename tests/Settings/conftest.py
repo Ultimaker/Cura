1364370ede4ecbe71c077b56371a55971639a9fa
Diego Prado Gesto <d.pradogesto@ultimaker.com>
2018-08-14 13:49:37 +0200

    Fix global stack tests.
    Contributes to CURA-5628.

--- tests/Settings/conftest.py
+++ tests/Settings/conftest.py
@@ -4,16 +4,35 @@
 # The purpose of this class is to create fixtures or methods that can be shared among all settings tests.
 
 import pytest
-import copy
 
 from UM.Settings.DefinitionContainer import DefinitionContainer #To provide definition containers in the registry fixtures.
+from UM.Settings.InstanceContainer import InstanceContainer
+from cura.Settings.CuraContainerRegistry import CuraContainerRegistry
+from cura.Settings.ExtruderStack import ExtruderStack
+from cura.Settings.GlobalStack import GlobalStack
+import cura.Settings.CuraContainerStack
 
 # Returns the CuraContainerRegistry instance with some empty containers.
 @pytest.fixture()
-def container_registry(application):
+def container_registry(application) -> CuraContainerRegistry:
     return application.getContainerRegistry()
 
 # Gives an arbitrary definition container.
 @pytest.fixture()
-def definition_container():
+def definition_container() -> DefinitionContainer:
     return DefinitionContainer(container_id = "Test Definition")
+
+#An empty global stack to test with.
+@pytest.fixture()
+def global_stack() -> GlobalStack:
+    global_stack = GlobalStack("TestGlobalStack")
+    # There is a restriction here that the definition changes cannot be an empty container. Added in CURA-5281
+    definition_changes_container = InstanceContainer(container_id = "InstanceContainer")
+    definition_changes_container.setMetaDataEntry("type", "definition_changes")
+    global_stack._containers[cura.Settings.CuraContainerStack._ContainerIndexes.DefinitionChanges] = definition_changes_container
+    return global_stack
+
+##  An empty extruder stack to test with.
+@pytest.fixture()
+def extruder_stack() -> ExtruderStack:
+    return ExtruderStack("TestExtruderStack")
--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,7 @@
 # The purpose of this class is to create fixtures or methods that can be shared among all tests.
 
 import pytest
-from UM.Qt.QtApplication import QtApplication #QTApplication import is required, even though it isn't used.
+from UM.Qt.QtApplication import QtApplication #QtApplication import is required, even though it isn't used.
 from cura.CuraApplication import CuraApplication
 from cura.MachineActionManager import MachineActionManager
 
@@ -15,24 +15,9 @@ def application() -> CuraApplication:
     application = CuraApplication.getInstance()
     if application is None:
         application = CuraApplication()
-        application.initialize()
     return application
 
 # Returns a MachineActionManager instance.
 @pytest.fixture()
 def machine_action_manager(application) -> MachineActionManager:
     return application.getMachineActionManager()
-
-# @pytest.fixture()
-# def plugin_registry(application):
-#     PluginRegistry._PluginRegistry__instance = None
-#     plugin_registry = PluginRegistry(application)
-#     plugin_registry._plugin_locations = [] # Clear pre-defined plugin locations
-#     return plugin_registry
-#
-# @pytest.fixture()
-# def upgrade_manager(application):
-#     VersionUpgradeManager._VersionUpgradeManager__instance = None
-#     upgrade_manager = VersionUpgradeManager(application)
-#     return upgrade_manager
-
