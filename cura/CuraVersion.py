1b01e0a7a58bf5a703ec43b76eec861a8362f50b
fieldOfView <aldo@fieldofview.com>
2018-09-12 22:56:34 +0200

    Update Cura version and SDK version

--- cura/CuraVersion.py
+++ cura/CuraVersion.py
@@ -1,9 +1,9 @@
 # Copyright (c) 2015 Ultimaker B.V.
 # Cura is released under the terms of the LGPLv3 or higher.
 
-CuraVersion = "3.4.2"
+CuraVersion = "3.5"
 CuraBuildType = "BlackBelt"
 CuraDebugMode = False
-CuraSDKVersion = "4"
+CuraSDKVersion = "5"
 CuraCloudAPIRoot = "https://api.ultimaker.com"
 CuraCloudAPIVersion = "1"
