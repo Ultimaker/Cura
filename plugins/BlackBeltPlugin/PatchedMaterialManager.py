fb826101c9bbbfe2d854db5babbd66c9f6f6732f
fieldOfView <aldo@fieldofview.com>
2018-09-12 22:54:54 +0200

    Update MaterialManager patch

--- plugins/BlackBeltPlugin/PatchedMaterialManager.py
+++ plugins/BlackBeltPlugin/PatchedMaterialManager.py
@@ -4,7 +4,11 @@ import uuid
 
 from PyQt5.Qt import pyqtSlot
 
-from typing import Optional, TYPE_CHECKING
+from cura.Machines.MaterialNode import MaterialNode
+from cura.Machines.MaterialGroup import MaterialGroup
+from cura.Machines.VariantType import VariantType
+
+from typing import Dict, Optional, TYPE_CHECKING
 if TYPE_CHECKING:
     from UM.Settings.DefinitionContainer import DefinitionContainer
 
@@ -14,56 +18,63 @@ class PatchedMaterialManager(MaterialManager):
     #
     # Copied verbatim from MaterialManager.getAvailableMaterials, with a minor patch to limit shown materials
     # if they are so specified in the machine definition
-    def getAvailableMaterials(self, machine_definition: "DefinitionContainer", extruder_variant_name: Optional[str],
-                              diameter: float) -> dict:
+    def getAvailableMaterials(self, machine_definition: "DefinitionContainer", nozzle_name: Optional[str],
+                              buildplate_name: Optional[str], diameter: float) -> Dict[str, MaterialNode]:
         # round the diameter to get the approximate diameter
         rounded_diameter = str(round(diameter))
-        if rounded_diameter not in self._diameter_machine_variant_material_map:
+        if rounded_diameter not in self._diameter_machine_nozzle_buildplate_material_map:
             Logger.log("i", "Cannot find materials with diameter [%s] (rounded to [%s])", diameter, rounded_diameter)
             return dict()
 
         machine_definition_id = machine_definition.getId()
 
-        # If there are variant materials, get the variant material
-        machine_variant_material_map = self._diameter_machine_variant_material_map[rounded_diameter]
-        machine_node = machine_variant_material_map.get(machine_definition_id)
-        default_machine_node = machine_variant_material_map.get(self._default_machine_definition_id)
-        variant_node = None
-        if extruder_variant_name is not None and machine_node is not None:
-            variant_node = machine_node.getChildNode(extruder_variant_name)
+        # If there are nozzle-and-or-buildplate materials, get the nozzle-and-or-buildplate material
+        machine_nozzle_buildplate_material_map = self._diameter_machine_nozzle_buildplate_material_map[rounded_diameter]
+        machine_node = machine_nozzle_buildplate_material_map.get(machine_definition_id)
+        default_machine_node = machine_nozzle_buildplate_material_map.get(self._default_machine_definition_id)
+        nozzle_node = None
+        buildplate_node = None
+        if nozzle_name is not None and machine_node is not None:
+            nozzle_node = machine_node.getChildNode(nozzle_name)
+            # Get buildplate node if possible
+            if nozzle_node is not None and buildplate_name is not None:
+                buildplate_node = nozzle_node.getChildNode(buildplate_name)
 
-        nodes_to_check = [variant_node, machine_node, default_machine_node]
+        nodes_to_check = [buildplate_node, nozzle_node, machine_node, default_machine_node]
 
         # Fallback mechanism of finding materials:
-        #  1. variant-specific material
-        #  2. machine-specific material
-        #  3. generic material (for fdmprinter)
+        #  1. buildplate-specific material
+        #  2. nozzle-specific material
+        #  3. machine-specific material
+        #  4. generic material (for fdmprinter)
         machine_exclude_materials = machine_definition.getMetaDataEntry("exclude_materials", [])
 
         ### START PATCH
         machine_limit_materials = machine_definition.getMetaDataEntry("limit_materials", False)
         ### END PATCH
 
-        material_id_metadata_dict = dict()
-        for node in nodes_to_check:
-            if node is not None:
-                # Only exclude the materials that are explicitly specified in the "exclude_materials" field.
-                # Do not exclude other materials that are of the same type.
-                for material_id, node in node.material_map.items():
-                    ### START PATCH
-                    if machine_limit_materials and node.getContainer().getId() == material_id:
-                        # For the materials we want Cura creates a variant-specific InstanceContainer
-                        # If the InstanceContainer is not variant-specific then we are not interested
-                        continue
-                    ### END PATCH
+        material_id_metadata_dict = dict()  # type: Dict[str, MaterialNode]
+        for current_node in nodes_to_check:
+            if current_node is None:
+                continue
 
-                    if material_id in machine_exclude_materials:
-                        Logger.log("d", "Exclude material [%s] for machine [%s]",
-                                   material_id, machine_definition.getId())
-                        continue
+            # Only exclude the materials that are explicitly specified in the "exclude_materials" field.
+            # Do not exclude other materials that are of the same type.
+            for material_id, node in current_node.material_map.items():
+                ### START PATCH
+                if machine_limit_materials and node.getContainer().getId() == material_id:
+                    # For the materials we want Cura creates a variant-specific InstanceContainer
+                    # If the InstanceContainer is not variant-specific then we are not interested
+                    continue
+                ### END PATCH
 
-                    if material_id not in material_id_metadata_dict:
-                        material_id_metadata_dict[material_id] = node
+                if material_id in machine_exclude_materials:
+                    Logger.log("d", "Exclude material [%s] for machine [%s]",
+                               material_id, machine_definition.getId())
+                    continue
+
+                if material_id not in material_id_metadata_dict:
+                    material_id_metadata_dict[material_id] = node
 
         return material_id_metadata_dict
 
