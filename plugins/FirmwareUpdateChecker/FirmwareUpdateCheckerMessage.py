56a383814b78bc6dc9d7349381a4a4b23d286edc
Ghostkeeper <rubend@tutanota.com>
2018-10-15 14:48:18 +0200

    Code style: Spaces around binary operators
    Contributes to issue CURA-5483.

--- plugins/FirmwareUpdateChecker/FirmwareUpdateCheckerMessage.py
+++ plugins/FirmwareUpdateChecker/FirmwareUpdateCheckerMessage.py
@@ -1,3 +1,5 @@
+# Copyright (c) 2018 Ultimaker B.V.
+# Cura is released under the terms of the LGPLv3 or higher.
 
 from UM.i18n import i18nCatalog
 from UM.Message import Message
@@ -13,8 +15,8 @@ class FirmwareUpdateCheckerMessage(Message):
         super().__init__(i18n_catalog.i18nc(
             "@info Don't translate {machine_name}, since it gets replaced by a printer name!",
             "New features are available for your {machine_name}! It is recommended to update the firmware on your printer.").format(
-            machine_name=machine_name),
-            title=i18n_catalog.i18nc(
+            machine_name = machine_name),
+            title = i18n_catalog.i18nc(
                 "@info:title The %s gets replaced with the printer name.",
                 "New %s firmware available") % machine_name)
 
@@ -25,8 +27,8 @@ class FirmwareUpdateCheckerMessage(Message):
                           i18n_catalog.i18nc("@action:button", "How to update"),
                           "[no_icon]",
                           "[no_description]",
-                          button_style=Message.ActionButtonStyle.LINK,
-                          button_align=Message.ActionButtonStyle.BUTTON_ALIGN_LEFT)
+                          button_style = Message.ActionButtonStyle.LINK,
+                          button_align = Message.ActionButtonStyle.BUTTON_ALIGN_LEFT)
 
     def getMachineId(self) -> int:
         return self._machine_id
