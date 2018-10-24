eea340d57a3856682a9132b53005856af54c0971
Ghostkeeper <rubend@tutanota.com>
2018-10-24 15:54:14 +0200

    Add typing

--- cura/Scene/BlockSlicingDecorator.py
+++ cura/Scene/BlockSlicingDecorator.py
@@ -1,9 +1,12 @@
+# Copyright (c) 2018 Ultimaker B.V.
+# Cura is released under the terms of the LGPLv3 or higher.
+
 from UM.Scene.SceneNodeDecorator import SceneNodeDecorator
 
 
 class BlockSlicingDecorator(SceneNodeDecorator):
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
 
-    def isBlockSlicing(self):
+    def isBlockSlicing(self) -> bool:
         return True
