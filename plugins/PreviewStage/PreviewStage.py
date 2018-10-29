cfa962311bb45476fcc9e65dd24933bb9f2aa397
Jaime van Kessel <nallath@gmail.com>
2018-10-29 13:54:20 +0100

    Fixed typing
    CURA-5829

--- plugins/PreviewStage/PreviewStage.py
+++ plugins/PreviewStage/PreviewStage.py
@@ -2,16 +2,17 @@
 # Cura is released under the terms of the LGPLv3 or higher.
 import os.path
 
-from UM.Application import Application
+from UM.Qt.QtApplication import QtApplication
 from cura.Stages.CuraStage import CuraStage
 
 from typing import TYPE_CHECKING, Optional
 
 if TYPE_CHECKING:
-    from UM.View import View
+    from UM.View.View import View
+
 
 class PreviewStage(CuraStage):
-    def __init__(self, application: Application, parent = None) -> None:
+    def __init__(self, application: QtApplication, parent = None) -> None:
         super().__init__(parent)
         self._application = application
         self._application.engineCreatedSignal.connect(self._engineCreated)
@@ -26,6 +27,7 @@ class PreviewStage(CuraStage):
         self._previously_active_view = None
 
     def _engineCreated(self) -> None:
-        menu_component_path = os.path.join(self._application.getPluginRegistry().getPluginPath(self.getPluginId()),
-                                           "PreviewMenu.qml")
-        self.addDisplayComponent("menu", menu_component_path)
+        plugin_path = self._application.getPluginRegistry().getPluginPath(self.getPluginId())
+        if plugin_path is not None:
+            menu_component_path = os.path.join(plugin_path, "PreviewMenu.qml")
+            self.addDisplayComponent("menu", menu_component_path)
