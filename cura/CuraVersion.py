39d9526fc33061b35a2c3345db861e2225a1be91
fieldOfView <aldo@fieldofview.com>
2018-11-13 15:50:49 +0100

    Update version

--- cura/CuraVersion.py
+++ cura/CuraVersion.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2015 Ultimaker B.V.
 # Cura is released under the terms of the LGPLv3 or higher.
 
-CuraVersion = "3.5.1-BETA"
+CuraVersion = "3.6.0-BETA"
 CuraBuildType = "BlackBelt"
 CuraDebugMode = False
 CuraSDKVersion = "5"
