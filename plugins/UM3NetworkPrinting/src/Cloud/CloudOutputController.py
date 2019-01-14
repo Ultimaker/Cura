7bf319dfd12ce82385ff2c290dfdceb64921b778
ChrisTerBeke <c.terbeke@ultimaker.com>
2019-01-14 14:17:36 +0100

    Better fix for circular dependency

--- plugins/UM3NetworkPrinting/src/Cloud/CloudOutputController.py
+++ plugins/UM3NetworkPrinting/src/Cloud/CloudOutputController.py
@@ -1,11 +1,14 @@
 # Copyright (c) 2018 Ultimaker B.V.
 # Cura is released under the terms of the LGPLv3 or higher.
-from UM.OutputDevice.OutputDevice import OutputDevice
 from cura.PrinterOutput.PrinterOutputController import PrinterOutputController
+
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from .CloudOutputDevice import CloudOutputDevice
 
 
 class CloudOutputController(PrinterOutputController):
-    def __init__(self, output_device: "OutputDevice") -> None:
+    def __init__(self, output_device: "CloudOutputDevice") -> None:
         super().__init__(output_device)
         
         # The cloud connection only supports fetching the printer and queue status and adding a job to the queue.
