54ce6a3676a22afc6068ba4f127ade921f9a8fca
Lipu Fei <lipu.fei815@gmail.com>
2019-01-28 13:40:42 +0100

    Fix typing

--- cura/LayerDataDecorator.py
+++ cura/LayerDataDecorator.py
@@ -1,20 +1,21 @@
-from typing import Optional
+from typing import TYPE_CHECKING, Optional
 
 from UM.Scene.SceneNodeDecorator import SceneNodeDecorator
 
+if TYPE_CHECKING:
+    from cura.LayerData import LayerData
+
+
 ## Simple decorator to indicate a scene node holds layer data.
-from cura.LayerData import LayerData
-
-
 class LayerDataDecorator(SceneNodeDecorator):
     def __init__(self):
         super().__init__()
         self._layer_data = None  # type: Optional[LayerData]
-        
-    def getLayerData(self) -> Optional[LayerData]:
+
+    def getLayerData(self) -> Optional["LayerData"]:
         return self._layer_data
-    
-    def setLayerData(self, layer_data: LayerData) -> None:
+
+    def setLayerData(self, layer_data: Optional["LayerData"]) -> None:
         self._layer_data = layer_data
 
     def __deepcopy__(self, memo) -> "LayerDataDecorator":
