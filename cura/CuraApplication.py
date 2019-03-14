e98cf83cb3db15d481063d675b894f98dd951f5e
Lipu Fei <lipu.fei815@gmail.com>
2019-03-14 15:38:06 +0100

    Add HttpNetworkRequestManager to CuraApplication
    CURA-6387

--- cura/CuraApplication.py
+++ cura/CuraApplication.py
@@ -130,6 +130,9 @@ from . import CameraAnimation
 from . import CuraActions
 from . import PrintJobPreviewImageProvider
 
+from cura.TaskManagement.HttpNetworkRequestManager import HttpNetworkRequestManager
+from cura.TaskManagement.OnExitCallbackManager import OnExitCallbackManager
+
 from cura import ApplicationMetadata, UltimakerCloudAuthentication
 from cura.Settings.GlobalStack import GlobalStack
 
@@ -200,6 +203,8 @@ class CuraApplication(QtApplication):
         self.empty_material_container = None  # type: EmptyInstanceContainer
         self.empty_quality_container = None  # type: EmptyInstanceContainer
         self.empty_quality_changes_container = None  # type: EmptyInstanceContainer
+
+        self._http_network_request_manager = HttpNetworkRequestManager(parent = self)
 
         self._material_manager = None
         self._machine_manager = None
@@ -886,6 +891,9 @@ class CuraApplication(QtApplication):
 
         # Hide the splash screen
         self.closeSplash()
+
+    def getHttpNetworkRequestManager(self) -> "HttpNetworkRequestManager":
+        return self._http_network_request_manager
 
     @pyqtSlot(result = QObject)
     def getDiscoveredPrintersModel(self, *args) -> "DiscoveredPrintersModel":
