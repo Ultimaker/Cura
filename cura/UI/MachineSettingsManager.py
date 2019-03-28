82471a0e398387980b4b02e5f5697bb295e9ea43
Lipu Fei <lipu.fei815@gmail.com>
2019-03-28 14:31:33 +0100

    Add doc for MachineSettingsManager

--- cura/UI/MachineSettingsManager.py
+++ cura/UI/MachineSettingsManager.py
@@ -1,11 +1,16 @@
+# Copyright (c) 2019 Ultimaker B.V.
+# Cura is released under the terms of the LGPLv3 or higher.
 
-from typing import Any, Dict, Optional, TYPE_CHECKING
+from typing import Optional
 
-from PyQt5.QtCore import Qt, QObject, pyqtSlot
+from PyQt5.QtCore import QObject, pyqtSlot
 
 from UM.i18n import i18nCatalog
 
 
+#
+# This manager provides (convenience) functions to the Machine Settings Dialog QML to update certain machine settings.
+#
 class MachineSettingsManager(QObject):
 
     def __init__(self, parent: Optional["QObject"] = None) -> None:
