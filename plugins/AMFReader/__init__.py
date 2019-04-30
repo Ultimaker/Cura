0a136b58f0cc15901a7479f9627190947be89a37
Jaime van Kessel <nallath@gmail.com>
2019-04-30 11:38:55 +0200

    Also add the right metadata to the AMF plugin.
    This ensures that the filetype shows up in the supported types of the open window

--- plugins/AMFReader/__init__.py
+++ plugins/AMFReader/__init__.py
@@ -3,8 +3,19 @@
 
 from . import AMFReader
 
+from UM.i18n import i18nCatalog
+i18n_catalog = i18nCatalog("uranium")
+
+
 def getMetaData():
-    return {}
+    return {
+        "mesh_reader": [
+            {
+                "extension": "amf",
+                "description": i18n_catalog.i18nc("@item:inlistbox", "AMF File")
+            }
+        ]
+    }
 
 def register(app):
     return {"mesh_reader": AMFReader.AMFReader()}
