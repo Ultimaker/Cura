c4044e731afc36a3e89b2f6e1a3730bda4363628
Diego Prado Gesto <d.pradogesto@ultimaker.com>
2019-05-16 12:04:26 +0200

    Fix typo in a log warning message

--- cura/PrinterOutputDevice.py
+++ cura/PrinterOutputDevice.py
@@ -1,4 +1,4 @@
 import warnings
-warnings.warn("Importing cura.PrinterOutputDevice has been deprecated since 4.1, use cura.PrinterOutput.PrinterOutputDevice inststad", DeprecationWarning, stacklevel=2)
+warnings.warn("Importing cura.PrinterOutputDevice has been deprecated since 4.1, use cura.PrinterOutput.PrinterOutputDevice instead", DeprecationWarning, stacklevel=2)
 # We moved the PrinterOutput device to it's own submodule.
 from cura.PrinterOutput.PrinterOutputDevice import PrinterOutputDevice, ConnectionState
