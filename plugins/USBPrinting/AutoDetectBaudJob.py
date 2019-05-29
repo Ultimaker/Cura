db36179f2d3fbedb66ed1ed1f159ec7fcda368bc
Karsten Jeppesen <karsten@jeppesens.com>
2019-05-29 20:42:22 +0200

    Buffer flush; lots of zombie code, BL wait
    ASCII decode could fail (not seen but possible)

--- plugins/USBPrinting/AutoDetectBaudJob.py
+++ plugins/USBPrinting/AutoDetectBaudJob.py
@@ -22,8 +22,8 @@ class AutoDetectBaudJob(Job):
 
     def run(self) -> None:
         Logger.log("d", "Auto detect baud rate started.")
-        wait_response_timeouts = [4, 15, 30]
-        wait_bootloader_times = [1.5, 5, 15]
+        read_timeouts = [2, 5, 15, 30]
+        wait_bootloader_times = [5, 10, 15]
         write_timeout = 3
         read_timeout = 3
         tries = 2
@@ -38,16 +38,16 @@ class AutoDetectBaudJob(Job):
 
         for retry in range(tries):
             for baud_rate in self._all_baud_rates:
-                if retry < len(wait_response_timeouts):
-                    wait_response_timeout = wait_response_timeouts[retry]
+                if retry < len(read_timeouts):
+                    read_timeout = read_timeouts[retry]
                 else:
-                    wait_response_timeout = wait_response_timeouts[-1]
+                    read_timeout = read_timeouts[-1]
                 if retry < len(wait_bootloader_times):
                     wait_bootloader = wait_bootloader_times[retry]
                 else:
                     wait_bootloader = wait_bootloader_times[-1]
                 Logger.log("d", "Checking {serial} if baud rate {baud_rate} works. Retry nr: {retry}. Wait timeout: {timeout}".format(
-                    serial = self._serial_port, baud_rate = baud_rate, retry = retry, timeout = wait_response_timeout))
+                    serial = self._serial_port, baud_rate = baud_rate, retry = retry, timeout = read_timeout))
 
                 if serial is None:
                     try:
@@ -62,50 +62,26 @@ class AutoDetectBaudJob(Job):
                     # We already have a serial connection, just change the baud rate.
                     try:
                         serial.baudrate = baud_rate
+                        serial.timeout = read_timeout
+                        serial.write_timeout = write_timeout
                     except:
                         Logger.logException("w", "Setting baud rate: Exception")
                         continue
-                sleep(wait_bootloader)  # Ensure that we are not talking to the boot loader. 1.5 seconds seems to be the magic number
+                sleep(wait_bootloader)  # Ensure that we are not talking to the boot loader. 5 seconds seems to be the magic number
                 successful_responses = 0
 
-                try:
-                    serial.write(b"\n")  # Ensure we clear out previous responses
-                    serial.write(b"M105\n")
-                except:
-                    Logger.logException("w", "serial.write: Exception")
-                    continue
-
+                readBack = serial.read(10000).decode("ASCII")
+                # Include the startup log from printer in log
+                Logger.log("d", "Printer Init Info\n{ts}".format( ts = readBack ))
                 start_timeout_time = time()
-                timeout_time = time() + wait_response_timeout
-                mytime = time()
-
-# Using serial.readline will not work here due to all the noise received during baudrate mismatch
-# Replaced with state machinery which will work
-
-                while timeout_time > time():
-                    if mytime != time():
-                        serial.write(b"M105\n")
-                        mytime = time()
-                    mychar = serial.read()
-                    if mychar == b"\n":
-                        mychar = serial.read()
-                        if mychar == b"o":
-                            mychar = serial.read()
-                            if mychar == b"k":
-                                mychar = serial.read()
-                                if mychar == b" ":
-                                    mychar = serial.read()
-                                    if mychar == b"T":
-                                        mychar = serial.read()
-                                        if mychar == b":":
-                                            successful_responses += 1
-                                            if successful_responses >= 3:
-                                                self.setResult(baud_rate)
-                                                Logger.log("d", "Detected baud rate {baud_rate} on serial {serial} on retry {retry} with after {time_elapsed:0.2f} seconds.".format(
-                                                    serial = self._serial_port, baud_rate = baud_rate, retry = retry, time_elapsed = time() - start_timeout_time))
-                                                serial.close() # close serial port so it can be opened by the USBPrinterOutputDevice
-                                                return
-                                            serial.write(b"M105\n")
-                                            mytime = time()
+                serial.write( b"\nM105\n" )
+                readBack = serial.read(10000).decode("ASCII")
+                # Clearing the inputbuffer before the command should leave us with the correct reply in place
+                if readBack.startswith("ok T:"):
+                    self.setResult(baud_rate)
+                    Logger.log("d", "Detected baud rate {baud_rate} on serial {serial} on retry {retry} with after {time_elapsed:0.2f} seconds.".format(
+                        serial = self._serial_port, baud_rate = baud_rate, retry = retry, time_elapsed = time() - start_timeout_time))
+                    serial.close() # close serial port so it can be opened by the USBPrinterOutputDevice
+                    return
             sleep(15)  # Give the printer some time to init and try again.
         self.setResult(None)  # Unable to detect the correct baudrate.
