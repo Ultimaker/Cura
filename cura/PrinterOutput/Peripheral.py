f402235406823771ab627debe8b79bceca380990
Jaime van Kessel <nallath@gmail.com>
2019-07-16 10:27:50 +0200

    Fix shadowed use of builtin type
    CURA-6651

--- cura/PrinterOutput/Peripheral.py
+++ cura/PrinterOutput/Peripheral.py
@@ -1,6 +1,7 @@
 # Copyright (c) 2019 Ultimaker B.V.
 # Cura is released under the terms of the LGPLv3 or higher.
 
+
 ##  Data class that represents a peripheral for a printer.
 #
 #   Output device plug-ins may specify that the printer has a certain set of
@@ -8,10 +9,8 @@
 #   stage.
 class Peripheral:
     ##  Constructs the peripheral.
-    #   \param id A unique ID for the peripheral object, like a MAC address or
-    #   some hardware ID.
     #   \param type A unique ID for the type of peripheral.
     #   \param name A human-readable name for the peripheral.
-    def __init__(self, type: str, name: str):
-        self.type = type
+    def __init__(self, peripheral_type: str, name: str):
+        self.type = peripheral_type
         self.name = name
