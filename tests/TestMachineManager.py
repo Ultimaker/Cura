9ad8d91ff996a0ec5be438ba21da8580d2d73d10
Jaime van Kessel <nallath@gmail.com>
2019-07-19 13:55:16 +0200

    Add tests for ExtruderManager

--- tests/TestMachineManager.py
+++ tests/TestMachineManager.py
@@ -2,8 +2,6 @@ from unittest.mock import MagicMock, patch
 
 import pytest
 
-from UM.Settings.ContainerRegistry import ContainerRegistry
-from cura.Settings.ExtruderManager import ExtruderManager
 from cura.Settings.MachineManager import MachineManager
 
 
@@ -12,22 +10,6 @@ def global_stack():
     stack = MagicMock(name="Global Stack")
     stack.getId = MagicMock(return_value ="GlobalStack")
     return stack
-
-@pytest.fixture()
-def container_registry() -> ContainerRegistry:
-    return MagicMock(name = "ContainerRegistry")
-
-
-@pytest.fixture()
-def extruder_manager(application, container_registry) -> ExtruderManager:
-    if ExtruderManager.getInstance() is not None:
-        # Reset the data
-        ExtruderManager._ExtruderManager__instance = None
-
-    with patch("cura.CuraApplication.CuraApplication.getInstance", MagicMock(return_value=application)):
-        with patch("UM.Settings.ContainerRegistry.ContainerRegistry.getInstance", MagicMock(return_value=container_registry)):
-            manager = ExtruderManager()
-    return manager
 
 
 @pytest.fixture()
