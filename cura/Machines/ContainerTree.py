e84a75094a88be194592a3a7c187e5d5765ed555
Ghostkeeper <rubend@tutanota.com>
2019-08-05 17:49:21 +0200

    Add a MachineNode once a definition for it gets loaded
    This means that we've added this machine. We need to pre-load all of the containers for that printer then.
    
    Contributes to issue CURA-6600.

--- cura/Machines/ContainerTree.py
+++ cura/Machines/ContainerTree.py
@@ -1,6 +1,9 @@
 # Copyright (c) 2019 Ultimaker B.V.
 # Cura is released under the terms of the LGPLv3 or higher.
 
+from UM.Settings.ContainerRegistry import ContainerRegistry  # To listen to containers being added.
+from UM.Settings.DefinitionContainer import DefinitionContainer
+from UM.Settings.Interfaces import ContainerInterface
 from cura.Machines.MachineNode import MachineNode
 
 from typing import Dict
@@ -13,3 +16,14 @@ from typing import Dict
 class ContainerTree:
     def __init__(self) -> None:
         self.machines = {}  # type: Dict[str, MachineNode] # Mapping from definition ID to machine nodes.
+        ContainerRegistry.getInstance().containerAdded.connect(self.machineAdded)
+
+    ##  When a printer gets added, we need to build up the tree for that container.
+    def machineAdded(self, definition_container: ContainerInterface):
+        if not isinstance(definition_container, DefinitionContainer):
+            return  # Not our concern.
+        definition_id = definition_container.getId()
+        if definition_id in self.machines:
+            return  # Already have this definition ID.
+
+        self.machines[definition_id] = MachineNode(definition_id)
