b1fb843f094ca2a1722809b302895aae6e43f78a
Ghostkeeper <rubend@tutanota.com>
2019-08-12 16:15:03 +0200

    Implement matching qualities by material_id with same material type OR GUID
    This fallback with the GUID makes this part a lot more complex, but in theory it should work. I hope that we can get some tests to debug this because it's 90% made from the top of my head now.
    
    Contributes to issue CURA-6600.

--- cura/Machines/MaterialNode.py
+++ cura/Machines/MaterialNode.py
@@ -32,7 +32,16 @@ class MaterialNode(ContainerNode):
         if not self.variant.machine.has_machine_quality:  # Need to find the global qualities.
             qualities = container_registry.findInstanceContainersMetadata(type = "quality", definition = "fdmprinter")
         else:
-            qualities = container_registry.findInstanceContainersMetadata(type = "quality", definition = self.variant.machine.quality_definition, variant = self.variant.variant_name, material = self.base_file)
+            # Need to find the qualities that specify a material profile with the same material type.
+            my_metadata = container_registry.findInstanceContainersMetadata(id = self.container_id)[0]
+            my_material_type = my_metadata.get("material")
+            qualities = []
+            for material_metadata in container_registry.findInstanceContainersMetadata(type = "material", material = my_material_type):
+                qualities.extend(container_registry.findInstanceContainersMetadata(type = "quality", definition = self.variant.machine.quality_definition, variant = self.variant.variant_name, material = material_metadata["id"]))
+            if not qualities:  # No quality profiles found. Go by GUID then.
+                my_guid = my_metadata.get("material")
+                for material_metadata in container_registry.findInstanceContainersMetadata(type = "material", guid = my_guid):
+                    qualities.extend(container_registry.findInstanceContainersMetadata(type = "quality", definition = self.variant.machine.quality_definition, variant = self.variant.variant_name, material = material_metadata["id"]))
 
         for quality in qualities:
             quality_id = quality["id"]
@@ -46,8 +55,29 @@ class MaterialNode(ContainerNode):
             if container.getMetaDataEntry("definition") != "fdmprinter":
                 return  # Only want global qualities.
         else:
-            if container.getMetaDataEntry("definition") != self.variant.machine.quality_definition or container.getMetaDataEntry("variant") != self.variant.variant_name or container.getMetaDataEntry("material") != self.base_file:
-                return  # Doesn't match our configuration.
+            if container.getMetaDataEntry("definition") != self.variant.machine.quality_definition:
+                return  # Doesn't match the machine.
+            if container.getMetaDataEntry("variant") != self.variant.variant_name:
+                return  # Doesn't match the variant.
+            # Detect if we're falling back to matching via GUID.
+            # If so, we might need to erase the current list and put just this one in (i.e. no longer use the fallback).
+            container_registry = ContainerRegistry.getInstance()
+            my_metadata = container_registry.findInstanceContainersMetadata(id = self.container_id)[0]
+            my_material_type = my_metadata.get("material")
+            allowed_material_ids = {metadata["id"] for metadata in container_registry.findInstanceContainersMetadata(type = "material", material = my_material_type)}
+            is_fallback_guid = len(self.qualities) == 0 or next(iter(self.qualities.values())).getMetaDataEntry("material") not in allowed_material_ids  # Select any quality profile; if the material is not matching by material type, we've been falling back to GUID.
+
+            if is_fallback_guid and container.getMetaDataEntry("material") in allowed_material_ids:  # So far we needed the fallback, but no longer!
+                self.qualities.clear()
+            else:
+                if not is_fallback_guid:
+                    if container.getMetaDataEntry("material") not in allowed_material_ids:
+                        return  # Doesn't match the material type.
+                else:
+                    my_material_guid = my_metadata["guid"]
+                    allowed_material_ids = {metadata["id"] for metadata in container_registry.findInstanceContainersMetadata(type = "material", guid = my_material_guid)}
+                    if container.getMetaDataEntry("material") not in allowed_material_ids:
+                        return  # Doesn't match the material GUID.
 
         quality_id = container.getId()
         self.qualities[quality_id] = QualityNode(quality_id, parent = self)
