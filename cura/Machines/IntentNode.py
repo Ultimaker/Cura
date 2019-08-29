a8b818fbdc14d8205e8d91f6236f1361746f37ee
Jaime van Kessel <nallath@gmail.com>
2019-08-29 13:52:01 +0200

    Ensure that right intents are added to the tree
    CURA-6598

--- cura/Machines/IntentNode.py
+++ cura/Machines/IntentNode.py
@@ -3,6 +3,7 @@
 
 from typing import TYPE_CHECKING
 
+from UM.Settings.ContainerRegistry import ContainerRegistry
 from cura.Machines.ContainerNode import ContainerNode
 
 if TYPE_CHECKING:
@@ -15,3 +16,5 @@ class IntentNode(ContainerNode):
     def __init__(self, container_id: str, quality: "QualityNode") -> None:
         super().__init__(container_id)
         self.quality = quality
+        my_metadata = ContainerRegistry.getInstance().findContainersMetadata(id=container_id)[0]
+        self.intent_category = my_metadata.get("intent_category", "default")
