86d87acd5b9e0799f438a0f87f102355a806770b
ChrisTerBeke <ctbeke@gmail.com>
2019-09-03 21:21:06 +0200

    Filter out configurations for empty materials

--- plugins/UM3NetworkPrinting/src/Models/Http/ClusterPrinterMaterialStationSlot.py
+++ plugins/UM3NetworkPrinting/src/Models/Http/ClusterPrinterMaterialStationSlot.py
@@ -1,5 +1,7 @@
 # Copyright (c) 2019 Ultimaker B.V.
 # Cura is released under the terms of the LGPLv3 or higher.
+from typing import Optional
+
 from .ClusterPrintCoreConfiguration import ClusterPrintCoreConfiguration
 
 
@@ -10,8 +12,11 @@ class ClusterPrinterMaterialStationSlot(ClusterPrintCoreConfiguration):
     #  \param slot_index: The index of the slot in the material station (ranging 0 to 5).
     #  \param compatible: Whether the configuration is compatible with the print core.
     #  \param material_remaining: How much material is remaining on the spool (between 0 and 1, or -1 for missing data).
-    def __init__(self, slot_index: int, compatible: bool, material_remaining: float, **kwargs):
+    #  \param material_empty: Whether the material spool is too empty to be used.
+    def __init__(self, slot_index: int, compatible: bool, material_remaining: float,
+                 material_empty: Optional[bool] = False, **kwargs):
         self.slot_index = slot_index
         self.compatible = compatible
         self.material_remaining = material_remaining
+        self.material_empty = material_empty
         super().__init__(**kwargs)
