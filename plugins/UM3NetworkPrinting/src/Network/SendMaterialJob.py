05de920c21cc3cf262ecb376de86e6944c3852bb
Ghostkeeper <rubend@tutanota.com>
2019-09-18 14:28:22 +0200

    Once more with feeling
    Sorry, I didn't see the other review comment until after I pushed the previous commit.
    
    Contributes to issue CURA-6600.

--- plugins/UM3NetworkPrinting/src/Network/SendMaterialJob.py
+++ plugins/UM3NetworkPrinting/src/Network/SendMaterialJob.py
@@ -129,10 +129,10 @@ class SendMaterialJob(Job):
     def _getLocalMaterials() -> Dict[str, LocalMaterial]:
         result = {}  # type: Dict[str, LocalMaterial]
         all_materials = CuraApplication.getInstance().getContainerRegistry().findInstanceContainersMetadata(type = "material")
-        all_root_materials = [material for material in all_materials if material["id"] == material.get("base_file")]  # Don't send materials without base_file: The empty material doesn't need to be sent.
+        all_base_files = [material for material in all_materials if material["id"] == material.get("base_file")]  # Don't send materials without base_file: The empty material doesn't need to be sent.
 
         # Find the latest version of all material containers in the registry.
-        for material_metadata in all_root_materials:
+        for material_metadata in all_base_files:
             try:
                 # material version must be an int
                 material_metadata["version"] = int(material_metadata["version"])
