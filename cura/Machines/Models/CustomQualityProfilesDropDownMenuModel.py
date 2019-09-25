d1330e5ffa88d11d30e44bd64f4ed044eace6591
Lipu Fei <lipu.fei815@gmail.com>
2019-09-25 08:52:53 +0200

    Fix updating custom quality menu model
    CURA-6599

--- cura/Machines/Models/CustomQualityProfilesDropDownMenuModel.py
+++ cura/Machines/Models/CustomQualityProfilesDropDownMenuModel.py
@@ -1,16 +1,34 @@
 # Copyright (c) 2019 Ultimaker B.V.
 # Cura is released under the terms of the LGPLv3 or higher.
 
+from typing import Optional, TYPE_CHECKING
+
 from UM.Logger import Logger
 
 import cura.CuraApplication  # Imported this way to prevent circular references.
 from cura.Machines.ContainerTree import ContainerTree
 from cura.Machines.Models.QualityProfilesDropDownMenuModel import QualityProfilesDropDownMenuModel
 
+if TYPE_CHECKING:
+    from PyQt5.QtCore import QObject
+    from UM.Settings.Interfaces import ContainerInterface
+
 
 ##  This model is used for the custom profile items in the profile drop down
 #   menu.
 class CustomQualityProfilesDropDownMenuModel(QualityProfilesDropDownMenuModel):
+
+    def __init__(self, parent: Optional["QObject"] = None) -> None:
+        super().__init__(parent)
+
+        container_registry = cura.CuraApplication.CuraApplication.getInstance().getContainerRegistry()
+        container_registry.containerAdded.connect(self._qualityChangesListChanged)
+        container_registry.containerRemoved.connect(self._qualityChangesListChanged)
+        container_registry.containerMetaDataChanged.connect(self._qualityChangesListChanged)
+
+    def _qualityChangesListChanged(self, container: "ContainerInterface") -> None:
+        if container.getMetaDataEntry("type") == "quality_changes":
+            self._update()
 
     def _update(self) -> None:
         Logger.log("d", "Updating {model_class_name}.".format(model_class_name = self.__class__.__name__))
