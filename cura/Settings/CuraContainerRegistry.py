1def289bb9555a52c1c2fe5da5d20c635b8b46de
Jaime van Kessel <nallath@gmail.com>
2019-10-03 09:51:45 +0200

    Fix crash when importing profile that has different quality_definition
    CURA-6847

--- cura/Settings/CuraContainerRegistry.py
+++ cura/Settings/CuraContainerRegistry.py
@@ -238,7 +238,7 @@ class CuraContainerRegistry(ContainerRegistry):
 
                 # Get the expected machine definition.
                 # i.e.: We expect gcode for a UM2 Extended to be defined as normal UM2 gcode...
-                profile_definition = container_tree.machines[machine_definition.getId()].quality_definition
+                profile_definition = machine_definition.getMetaDataEntry("quality_definition", "")
                 expected_machine_definition = container_tree.machines[global_stack.definition.getId()].quality_definition
 
                 # And check if the profile_definition matches either one (showing error if not):
