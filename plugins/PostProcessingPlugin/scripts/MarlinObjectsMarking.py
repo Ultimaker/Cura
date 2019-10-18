c4b8207b620dc353b56b43af1002ebc14dcb52b2
Kirill Shashlov <Kirill.Shashlov@kaspersky.com>
2019-10-18 13:40:22 +0300

    Simple object marking script for Marlin (to support https://github.com/MarlinFirmware/Marlin/pull/15590)

---
+++ plugins/PostProcessingPlugin/scripts/MarlinObjectsMarking.py
@@ -0,0 +1,46 @@
+# Cura PostProcessingPlugin
+# Author:   Kirill Shashlov
+# Date:     October, 2019
+
+# Licence: AGPLv3 or higher
+
+from ..Script import Script
+from UM.Application import Application
+from UM.Logger import Logger
+
+class MarlinObjectsMarking(Script):
+    def __init__(self):
+        super().__init__()
+
+    def getSettingDataString(self):
+        return """{
+            "name": "Mark different objects (meshes) with Marlin firmware rules",
+            "key": "MarlinObjectsMarking",
+            "metadata": {},
+            "version": 2,
+            "settings": {}
+        }"""
+
+    def execute(self, data):
+        obj_dict = {}
+        for layer in data:
+            lay_idx = data.index(layer)
+            layerCommandLines = layer.split("\n")
+            for commandLine in layerCommandLines:
+                if not commandLine.startswith(";"):
+                    continue
+
+                command_idx = layerCommandLines.index(commandLine)
+
+                if commandLine.startswith(";TYPE:SKIRT") or \
+                   commandLine.startswith(";MESH:NONMESH"):
+                    layerCommandLines.insert(command_idx + 1, self.putValue("", M=86, S=-1))
+                    continue
+
+                if commandLine.startswith(";MESH:"):
+                    obj_name = commandLine.split(":")[1]
+                    obj_index = obj_dict.setdefault(obj_name, len(obj_dict.keys()))
+                    layerCommandLines.insert(command_idx + 1, self.putValue("", M=86, S=obj_index))
+
+            data[lay_idx] = "\n".join(layerCommandLines)
+        return data
