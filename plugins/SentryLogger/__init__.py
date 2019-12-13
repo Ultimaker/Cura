93ee1115668c3c60aa23f9e492859ceeb0183408
Jaime van Kessel <nallath@gmail.com>
2019-12-13 15:20:43 +0100

    Add missing typing

--- plugins/SentryLogger/__init__.py
+++ plugins/SentryLogger/__init__.py
@@ -1,12 +1,16 @@
 # Copyright (c) 2019 Ultimaker B.V.
 # Cura is released under the terms of the LGPLv3 or higher.
+from typing import TYPE_CHECKING, Dict, Any
 
 from . import SentryLogger
 
+if TYPE_CHECKING:
+    from UM.Application import Application
 
-def getMetaData():
+
+def getMetaData() -> Dict[str, Any]:
     return {}
 
 
-def register(app):
-    return { "logger": SentryLogger.SentryLogger() }
+def register(app: "Application") -> Dict[str, Any]:
+    return {"logger": SentryLogger.SentryLogger()}
