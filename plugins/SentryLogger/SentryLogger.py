6a36a71c2ccc1479d103c03a25ff8380a45a6eab
Jaime van Kessel <nallath@gmail.com>
2019-12-13 16:25:58 +0100

    Prune user paths from the sentry logs

--- plugins/SentryLogger/SentryLogger.py
+++ plugins/SentryLogger/SentryLogger.py
@@ -5,7 +5,9 @@ from UM.Logger import LogOutput
 from typing import Set
 from sentry_sdk import add_breadcrumb
 from typing import Optional
+import os
 
+home_dir = os.path.expanduser("~")
 
 class SentryLogger(LogOutput):
     def __init__(self) -> None:
@@ -17,6 +19,7 @@ class SentryLogger(LogOutput):
     #   \param message String containing message to be logged
     def log(self, log_type: str, message: str) -> None:
         level = self._translateLogType(log_type)
+        message = self._pruneSensitiveData(message)
         if level is None:
             if message not in self._show_once:
                 level = self._translateLogType(log_type[0])
@@ -25,6 +28,12 @@ class SentryLogger(LogOutput):
                     add_breadcrumb(level=level, message=message)
         else:
             add_breadcrumb(level=level, message=message)
+
+    @staticmethod
+    def _pruneSensitiveData(message):
+        if home_dir in message:
+            message = message.replace(home_dir, "<censored_path>")
+        return message
 
     @staticmethod
     def _translateLogType(log_type: str) -> Optional[str]:
