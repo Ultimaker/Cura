6e87944872b22a067c52e18deb0522090fbc93b2
Nino van Hooff <ninovanhooff@gmail.com>
2020-01-31 13:57:32 +0100

    WIP: Delete leftover files from hidden machines
    CURA-7024

--- plugins/VersionUpgrade/VersionUpgrade44to45/VersionUpgrade44to45.py
+++ plugins/VersionUpgrade/VersionUpgrade44to45/VersionUpgrade44to45.py
@@ -1,7 +1,17 @@
 import configparser
-from typing import Tuple, List
+import os
+from collections import namedtuple
+from pathlib import Path
+from typing import Tuple, List, Dict
 import io
+
+from UM.Logger import Logger
+from UM.Resources import Resources
 from UM.VersionUpgrade import VersionUpgrade
+from UM.VersionUpgradeManager import VersionUpgradeManager, CallableTask
+
+ExtruderMachineMapping = namedtuple("ExtruderMachineMapping", ["extruder_id", "extruder_filename", "machine_id"])
+HiddenMachine = namedtuple("HiddenMachine", ["machine_id", "filename"])
 
 # Settings that were merged into one. Each one is a pair of settings. If both
 # are overwritten, the key wins. If only the key or the value is overwritten,
@@ -15,7 +25,16 @@ _removed_settings = {
     "support_tree_wall_thickness"
 }
 
+
 class VersionUpgrade44to45(VersionUpgrade):
+
+    def __init__(self):
+        self._hidden_machines = {}  # type: Dict[str, str]
+        """machine_id -> stack filename prefix"""
+
+        self._extruder_machine_mapping = []  # type: List[ExtruderMachineMapping]
+
+
     def getCfgVersion(self, serialised: str) -> int:
         parser = configparser.ConfigParser(interpolation = None)
         parser.read_string(serialised)
@@ -75,8 +94,42 @@ class VersionUpgrade44to45(VersionUpgrade):
         # Update version number.
         if "metadata" not in parser:
             parser["metadata"] = {}
+
+        # Check for leftover hidden machines. They should be deleted along with their associated files
+        stack_id = parser["general"]["id"]
+        if parser["metadata"]["setting_version"] == "10" and stack_id is not None:
+            stack_type = parser["metadata"]["type"]
+            if stack_type == "machine" and parser["metadata"]["hidden"]:
+                if not self._hidden_machines:
+                    # schedule a final upgrade step to delete all hidden machines
+                    VersionUpgradeManager.getInstance().upgradeExtraTask(CallableTask(self.deleteHiddenMachines))
+                self._hidden_machines[stack_id] = filename
+            elif stack_type == "extruder_train":
+                machine = parser["metadata"]["machine"]
+                self._extruder_machine_mapping.append(ExtruderMachineMapping(stack_id, filename, machine))
+
         parser["metadata"]["setting_version"] = "11"
 
         result = io.StringIO()
         parser.write(result)
         return [filename], [result.getvalue()]
+
+    def deleteHiddenMachines(self):
+
+        # delete extruders associated to hidden machines
+        for item in self._extruder_machine_mapping:
+            if item.machine_id in self._hidden_machines:
+                Logger.debug("Deleting extruder files for {}".format(item))
+                self._deleteConfigFiles(item.extruder_filename)
+
+        # delete machine files
+        for machine_id, filename in self._hidden_machines.items():
+            Logger.debug("Deleting files for hidden machine {}".format(machine_id))
+            self._deleteConfigFiles(filename)
+
+    def _deleteConfigFiles(self, filename: str):
+        path_prefix = Resources.getConfigStoragePath()
+        # FIXME this rglob pattern does not work
+        for file_path in Path(path_prefix).rglob(filename + "*.cfg"):
+            Logger.debug("Deleting {}".format(file_path))
+            os.remove(str(file_path))
