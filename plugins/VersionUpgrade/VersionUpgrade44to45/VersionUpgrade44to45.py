bb738318891863ecd399f7a41702541a34ee35df
Ghostkeeper <rubend@tutanota.com>
2020-02-04 14:11:41 +0100

    Cura directory may be empty
    Contributes to issue CURA-7024.

--- plugins/VersionUpgrade/VersionUpgrade44to45/VersionUpgrade44to45.py
+++ plugins/VersionUpgrade/VersionUpgrade44to45/VersionUpgrade44to45.py
@@ -30,12 +30,18 @@ class VersionUpgrade44to45(VersionUpgrade):
         In this case the plug-in will also check for stacks that need to be
         deleted.
         """
+
+        # Only delete hidden stacks when upgrading from version 4.4. Not 4.3 or 4.5, just when you're starting out from 4.4.
+        # If you're starting from an earlier version, you can't have had the bug that produces too many hidden stacks (https://github.com/Ultimaker/Cura/issues/6731).
+        # If you're starting from a later version, the bug was already fixed.
         data_storage_root = os.path.dirname(Resources.getDataStoragePath())
         folders = os.listdir(data_storage_root)  # All version folders.
-        folders = filter(lambda p: re.fullmatch(r"\d+\.\d+", p), folders)  # Only folders with a correct version number as name.
-        latest_version = max(list(folders), key = Version)  # Sort them by semantic version numbering.
-        if latest_version == "4.4":
-            self.removeHiddenStacks()
+        folders = set(filter(lambda p: re.fullmatch(r"\d+\.\d+", p), folders))  # Only folders with a correct version number as name.
+        folders.difference_update({os.path.basename(Resources.getDataStoragePath())})  # Remove current version from candidates (since the folder was just copied).
+        if folders:
+            latest_version = max(folders, key = Version)  # Sort them by semantic version numbering.
+            if latest_version == "4.4":
+                self.removeHiddenStacks()
 
     def removeHiddenStacks(self) -> None:
         """
