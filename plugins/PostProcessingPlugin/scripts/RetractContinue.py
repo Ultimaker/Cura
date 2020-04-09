d9649dc3ddd7e055bc936030ae5f9ddc4b20bc7b
Ghostkeeper <rubend@tutanota.com>
2020-04-09 11:03:10 +0200

    Reduce indentation and complexity by using a pre-check
    Contributes to issue CURA-7351.

--- plugins/PostProcessingPlugin/scripts/RetractContinue.py
+++ plugins/PostProcessingPlugin/scripts/RetractContinue.py
@@ -38,36 +38,37 @@ class RetractContinue(Script):
                     current_x = self.getValue(line, "X", current_x)
                     current_y = self.getValue(line, "Y", current_y)
                 if self.getValue(line, "G") == 1:
-                    if self.getValue(line, "E"):
-                        new_e = self.getValue(line, "E")
-                        if new_e >= current_e:  # Not a retraction.
-                            continue
-                        # A retracted travel move may consist of multiple commands, due to combing.
-                        # This continues retracting over all of these moves and only unretracts at the end.
-                        delta_line = 1
-                        dx = current_x  # Track the difference in X for this move only to compute the length of the travel.
-                        dy = current_y
-                        while line_number + delta_line < len(lines) and self.getValue(lines[line_number + delta_line], "G") != 1:
-                            travel_move = lines[line_number + delta_line]
-                            if self.getValue(travel_move, "G") != 0:
-                                delta_line += 1
-                                continue
-                            travel_x = self.getValue(travel_move, "X", dx)
-                            travel_y = self.getValue(travel_move, "Y", dy)
-                            f = self.getValue(travel_move, "F", "no f")
-                            length = math.sqrt((travel_x - dx) * (travel_x - dx) + (travel_y - dy) * (travel_y - dy))  # Length of the travel move.
-                            new_e -= length * extra_retraction_speed  # New retraction is by ratio of this travel move.
-                            if f == "no f":
-                                new_travel_move = "G1 X{travel_x} Y{travel_y} E{new_e}".format(travel_x = travel_x, travel_y = travel_y, new_e = new_e)
-                            else:
-                                new_travel_move = "G1 F{f} X{travel_x} Y{travel_y} E{new_e}".format(f = f, travel_x = travel_x, travel_y = travel_y, new_e = new_e)
-                            lines[line_number + delta_line] = new_travel_move
-
+                    if not self.getValue(line, "E"):  # Either None or 0: Not a retraction then.
+                        continue
+                    new_e = self.getValue(line, "E")
+                    if new_e >= current_e:  # Not a retraction.
+                        continue
+                    # A retracted travel move may consist of multiple commands, due to combing.
+                    # This continues retracting over all of these moves and only unretracts at the end.
+                    delta_line = 1
+                    dx = current_x  # Track the difference in X for this move only to compute the length of the travel.
+                    dy = current_y
+                    while line_number + delta_line < len(lines) and self.getValue(lines[line_number + delta_line], "G") != 1:
+                        travel_move = lines[line_number + delta_line]
+                        if self.getValue(travel_move, "G") != 0:
                             delta_line += 1
-                            dx = travel_x
-                            dy = travel_y
+                            continue
+                        travel_x = self.getValue(travel_move, "X", dx)
+                        travel_y = self.getValue(travel_move, "Y", dy)
+                        f = self.getValue(travel_move, "F", "no f")
+                        length = math.sqrt((travel_x - dx) * (travel_x - dx) + (travel_y - dy) * (travel_y - dy))  # Length of the travel move.
+                        new_e -= length * extra_retraction_speed  # New retraction is by ratio of this travel move.
+                        if f == "no f":
+                            new_travel_move = "G1 X{travel_x} Y{travel_y} E{new_e}".format(travel_x = travel_x, travel_y = travel_y, new_e = new_e)
+                        else:
+                            new_travel_move = "G1 F{f} X{travel_x} Y{travel_y} E{new_e}".format(f = f, travel_x = travel_x, travel_y = travel_y, new_e = new_e)
+                        lines[line_number + delta_line] = new_travel_move
 
-                        current_e = new_e
+                        delta_line += 1
+                        dx = travel_x
+                        dy = travel_y
+
+                    current_e = new_e
 
             new_layer = "\n".join(lines)
             data[layer_number] = new_layer
