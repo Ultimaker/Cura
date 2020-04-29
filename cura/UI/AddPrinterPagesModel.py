d6f150a523296033fcbf298bf6f88845269c9138
Kostas Karmas <konskarm@gmail.com>
2020-04-29 11:38:36 +0200

    Rearrange the AddPrinterPagesModel and mark "Add Cloud Printer" page as final
    In order to avoid cases where the "Machine actions" page would lead to the "add cloud printer"
    page when pressing next, the AddPrinterPagesModel was rearranged and the "add cloud printer"
    page was marked as final.
    
    CURA-7022

--- cura/UI/AddPrinterPagesModel.py
+++ cura/UI/AddPrinterPagesModel.py
@@ -21,12 +21,14 @@ class AddPrinterPagesModel(WelcomePagesModel):
                             "page_url": self._getBuiltinWelcomePagePath("AddPrinterByIpContent.qml"),
                             "next_page_id": "machine_actions",
                             })
+        self._pages.append({"id": "add_cloud_printers",
+                            "page_url": self._getBuiltinWelcomePagePath("AddCloudPrintersView.qml"),
+                            "is_final_page": True,
+                            "next_page_button_text": self._catalog.i18nc("@action:button", "Finish"),
+                            })
         self._pages.append({"id": "machine_actions",
                             "page_url": self._getBuiltinWelcomePagePath("FirstStartMachineActionsContent.qml"),
                             "should_show_function": self.shouldShowMachineActions,
-                            })
-        self._pages.append({"id": "add_cloud_printers",
-                            "page_url": self._getBuiltinWelcomePagePath("AddCloudPrintersView.qml"),
                             })
         self.setItems(self._pages)
 
