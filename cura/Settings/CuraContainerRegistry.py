e0e0371de4aae62e932d382cdfb37c0b8dd41972
Ghostkeeper <rubend@tutanota.com>
2020-05-15 15:10:30 +0200

    Use extruderList rather than extruders
    Removes another warning from the log. And it makes the code simpler in this case.
    
    Done during Turbo Testing and Tooling to reduce the number of warnings in our log file.

--- cura/Settings/CuraContainerRegistry.py
+++ cura/Settings/CuraContainerRegistry.py
@@ -180,9 +180,7 @@ class CuraContainerRegistry(ContainerRegistry):
             return {"status": "error", "message": catalog.i18nc("@info:status Don't translate the XML tags <filename>!", "Can't import profile from <filename>{0}</filename> before a printer is added.", file_name)}
         container_tree = ContainerTree.getInstance()
 
-        machine_extruders = []
-        for position in sorted(global_stack.extruders):
-            machine_extruders.append(global_stack.extruders[position])
+        machine_extruders = global_stack.extruderList
 
         plugin_registry = PluginRegistry.getInstance()
         extension = file_name.split(".")[-1]
