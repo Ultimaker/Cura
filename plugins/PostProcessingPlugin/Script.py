1a079921cacb7d2b46b9efbf4508328be7c8dcea
Louis Wouters <l.wouters@blueriq.com>
2020-06-08 00:43:16 +0200

    Updated function "putValue" Fixed double ";" when used on an existing line with a comment. Optimized the construction of the new G-code line by using list.append and join Updated description (outdated)

--- plugins/PostProcessingPlugin/Script.py
+++ plugins/PostProcessingPlugin/Script.py
@@ -141,52 +141,46 @@ class Script:
         All other keyword parameters are put in the result in g-code's format.
         For instance, if you put ``G=1`` in the parameters, it will output
         ``G1``. If you put ``G=1, X=100`` in the parameters, it will output
-        ``G1 X100``. The parameters G and M will always be put first. The
-        parameters T and S will be put second (or first if there is no G or M).
-        The rest of the parameters will be put in arbitrary order.
+        ``G1 X100``. The parameters will be put in order: G, M, T, S, F, X, Y, Z, E.
+        Any other parameters will be put in arbitrary order at the end of the line
 
         :param line: The original g-code line that must be modified. If not
             provided, an entirely new g-code line will be produced.
         :return: A line of g-code with the desired parameters filled in.
         """
 
-        #Strip the comment.
-        comment = ""
-        if ";" in line:
-            comment = line[line.find(";"):]
-            line = line[:line.find(";")] #Strip the comment.
+        def putValue(self, line: str = "", **kwargs) -> str:
+            #Separate the code and comment
+            comment = ""
+            if ";" in line:
+                comment = line[line.find(";"):]
+                line = line[:line.find(";")]  #Strip the comment.
 
-        #Parse the original g-code line.
-        for part in line.split(" "):
-            if part == "":
-                continue
-            parameter = part[0]
-            if parameter in kwargs:
-                continue #Skip this one. The user-provided parameter overwrites the one in the line.
-            value = part[1:]
-            kwargs[parameter] = value
+            # Extract the parameters from the original g-code line and add them to kwargs.
+            for part in line.split(" "):
+                if part == "":
+                    continue
+                parameter = part[0]
+                if parameter not in kwargs: #Skip the parameters that are overwritten by the user.
+                    value = part[1:]
+                    kwargs[parameter] = value
 
-        #Write the new g-code line.
-        result = ""
-        priority_parameters = ["G", "M", "T", "S", "F", "X", "Y", "Z", "E"] #First some parameters that get priority. In order of priority!
-        for priority_key in priority_parameters:
-            if priority_key in kwargs:
-                if result != "":
-                    result += " "
-                result += priority_key + str(kwargs[priority_key])
-                del kwargs[priority_key]
-        for key, value in kwargs.items():
-            if result != "":
-                result += " "
-            result += key + str(value)
+            #Construct the new g-code line.
+            new_line = list()
+            #First add these parameters in order.
+            for parameter in ["G", "M", "T", "S", "F", "X", "Y", "Z", "E"]:
+                if parameter in kwargs:
+                    new_line.append(parameter + str(kwargs.pop(parameter)))
+            #Then add the rest of the parameters.
+            for parameter, value in kwargs.items():
+                new_line.append(parameter + str(value))
 
-        #Put the comment back in.
-        if comment != "":
-            if result != "":
-                result += " "
-            result += ";" + comment
+            #If there was a comment, put it back in.
+            if comment != "":
+                new_line.append(comment)
 
-        return result
+            #Construct the new line.
+            return " ".join(new_line)
 
     def execute(self, data: List[str]) -> List[str]:
         """This is called when the script is executed. 
