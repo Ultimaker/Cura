f7f582a901ce06dc9c1ad070ffa0c1761915ee70
Víctor Frisach Boj <victor.frisach@apser.es>
2021-04-08 11:35:17 +0200

    Al terminar el slicing se pasa directamente a PreviewStage

--- plugins/CuraEngineBackend/CuraEngineBackend.py
+++ plugins/CuraEngineBackend/CuraEngineBackend.py
@@ -240,6 +240,8 @@ class CuraEngineBackend(QObject, Backend):
 
         if self._error_message:
             self._error_message.hide()
+       
+
 
 
     @pyqtSlot()
@@ -758,6 +760,9 @@ class CuraEngineBackend(QObject, Backend):
         if self._build_plates_to_be_sliced:
             self.enableTimer()  # manually enable timer to be able to invoke slice, also when in manual slice mode
             self._invokeSlice()
+        else:
+            # Pasamos directamente a PreviewStage
+            CuraApplication.getInstance().getController().setActiveStage("PreviewStage")
 
     def _onGCodeLayerMessage(self, message: Arcus.PythonMessage) -> None:
         """Called when a g-code message is received from the engine.
