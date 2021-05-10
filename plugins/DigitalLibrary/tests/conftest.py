2d396a81c5ec12df43a0ca3263ac7ecfd7e83828
Jaime van Kessel <nallath@gmail.com>
2021-05-10 16:53:16 +0200

    Remove unneeded pycapsule imports

--- plugins/DigitalLibrary/tests/conftest.py
+++ plugins/DigitalLibrary/tests/conftest.py
@@ -1,8 +1,3 @@
-# Prevents error: "PyCapsule_GetPointer called with incorrect name" with conflicting SIP configurations between Arcus and PyQt: Import custom Sip bindings first!
-import Savitar  # Dont remove this line
-import Arcus  # No really. Don't. It needs to be there!
-import pynest2d  # Really!
-
 
 # Ensure that the importing for all tests work
 import sys
