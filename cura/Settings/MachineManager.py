fcdb8d9dd00a38e35a7b5e1508a462f82862b6fd
Alex Fanat <alex.fanat@smus.ca>
2021-06-28 14:02:57 -0700

    Handle Critical Variant and Material Validation Error
    Don't validate materials for extruder with unknown preferred variant, and use f-string indicator to translate `{variant_name}` into actual value in the string for the log statement for the case when there is no variant with the given name in `setVariantByName`.

--- cura/Settings/MachineManager.py
+++ cura/Settings/MachineManager.py
@@ -366,12 +366,14 @@ class MachineManager(QObject):
                 Logger.log("w", "An extruder has an unknown variant, switching it to the preferred variant")
                 self.setVariantByName(extruder.getMetaDataEntry("position"), machine_node.preferred_variant_name)
                 variant_node = machine_node.variants.get(machine_node.preferred_variant_name)
-
-            material_node = variant_node.materials.get(extruder.material.getMetaDataEntry("base_file"))
-            if material_node is None:
-                Logger.log("w", "An extruder has an unknown material, switching it to the preferred material")
-                if not self.setMaterialById(extruder.getMetaDataEntry("position"), machine_node.preferred_material):
-                    Logger.log("w", "Failed to switch to %s keeping old material instead", machine_node.preferred_material)
+            if variant_node is None:
+                Logger.log("e", "An extruder has an unknown preferred variant, cannot access variant node, cannot attempt material validation")
+            else
+                material_node = variant_node.materials.get(extruder.material.getMetaDataEntry("base_file"))
+                if material_node is None:
+                    Logger.log("w", "An extruder has an unknown material, switching it to the preferred material")
+                    if not self.setMaterialById(extruder.getMetaDataEntry("position"), machine_node.preferred_material):
+                        Logger.log("w", "Failed to switch to %s keeping old material instead", machine_node.preferred_material)
 
 
     @staticmethod
@@ -1533,7 +1535,7 @@ class MachineManager(QObject):
         machine_node = ContainerTree.getInstance().machines.get(machine_definition_id)
         variant_node = machine_node.variants.get(variant_name)
         if variant_node is None:
-            Logger.error("There is no variant with the name {variant_name}.")
+            Logger.error(f"There is no variant with the name {variant_name}.")
             return
         self.setVariant(position, variant_node)
 
