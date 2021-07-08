b4a8cb1b82cb87c8315d6ca91539a9339c180677
Jaime van Kessel <nallath@gmail.com>
2021-07-08 17:01:03 +0200

    Add monitor print button for cloud print
    The button redirects the user to the cloud page
    
    CURA-8018

--- plugins/UM3NetworkPrinting/src/Cloud/CloudOutputDevice.py
+++ plugins/UM3NetworkPrinting/src/Cloud/CloudOutputDevice.py
@@ -256,7 +256,16 @@ class CloudOutputDevice(UltimakerNetworkedPrinterOutputDevice):
         """
         self._uploaded_print_job = self._pre_upload_print_job
         self._progress.hide()
-        PrintJobUploadSuccessMessage().show()
+        message = PrintJobUploadSuccessMessage()
+        message.addAction("monitor print",
+                          name=I18N_CATALOG.i18nc("@action:button", "Monitor Print"),
+                          icon="",
+                          description="Remove the plugin",
+                          button_align=message.ActionButtonAlignment.ALIGN_RIGHT)
+        df_url = "https://digitalfactory.ultimaker.com/app/jobs/%s?utm_source=cura&utm_medium=software&utm_campaign=monitor-button" % self._cluster.cluster_id
+        message.pyQtActionTriggered.connect(lambda message, action: (QDesktopServices.openUrl(QUrl(df_url)), message.hide()))
+
+        message.show()
         self.writeFinished.emit()
 
     def _onPrintUploadSpecificError(self, reply: "QNetworkReply", _: "QNetworkReply.NetworkError"):
