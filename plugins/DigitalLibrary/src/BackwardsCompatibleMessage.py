ecf820f3dbeaa66002670163816421cb6ae31cf9
Remco Burema <r.burema@ultimaker.com>
2021-08-11 12:35:54 +0200

    Fix tests.
    CURA-8473

--- plugins/DigitalLibrary/src/BackwardsCompatibleMessage.py
+++ plugins/DigitalLibrary/src/BackwardsCompatibleMessage.py
@@ -1,12 +1,12 @@
 # Copyright (c) 2021 Ultimaker B.V.
 # Cura is released under the terms of the LGPLv3 or higher.
 
-from UM.Application import Application
+from cura.CuraApplication import CuraApplication
 from UM.Message import Message
 from UM.Version import Version
 
 def getBackwardsCompatibleMessage(text: str, title: str, lifetime: int, message_type_str: str) -> Message:
-    if Application.getInstance().getAPIVersion() < Version("7.7.0"):
+    if CuraApplication.getInstance().getAPIVersion() < Version("7.7.0"):
         return Message(text=text, title=title, lifetime=lifetime)
     else:
         message_type = Message.MessageType.NEUTRAL
