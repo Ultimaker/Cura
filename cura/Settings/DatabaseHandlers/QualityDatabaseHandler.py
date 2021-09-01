f80e1ec2790d7fca93ac5e6de0b5a0a1ad62df6d
jelle Spijker <j.spijker@ultimaker.com>
2021-09-01 18:32:40 +0200

    Updated documentation
    Contributes to CURA-6096

--- cura/Settings/DatabaseHandlers/QualityDatabaseHandler.py
+++ cura/Settings/DatabaseHandlers/QualityDatabaseHandler.py
@@ -24,6 +24,14 @@ class QualityDatabaseHandler(DatabaseMetadataContainerController):
         self.container_type = InstanceContainer
 
     def groomMetadata(self, metadata: metadata_type) -> metadata_type:
+        """
+        Ensures that the metadata is in the order of the field keys and has the right size.
+        if the metadata doesn't contains a key which is stored in the DB it will add it as
+        an empty string. Key, value pairs that are not stored in the DB are dropped.
+        If the `global_quality` isn't set it well default to 'False'
+
+        :param metadata: The container metadata
+        """
         if "global_quality" not in metadata:
             metadata["global_quality"] = "False"
         return super().groomMetadata(metadata)
