73ad2a4e08f66ae862cb52de1707880426cb2050
Ghostkeeper <rubend@tutanota.com>
2021-10-19 13:06:04 +0200

    Add Marketplace plug-in, starting a rewrite from the Toolbox
    This plug-in will be a complete re-write of the previous Toolbox plug-in. It's intended to solve some of the inherent architectural problems with the Toolbox.
    We're calling it Marketplace now as well.
    
    Contributes to issue CURA-8556.

---
+++ plugins/Marketplace/Marketplace.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2021 Ultimaker B.V.
+# Cura is released under the terms of the LGPLv3 or higher.
+
+from UM.PluginObject import PluginObject
+
+class Marketplace(PluginObject):
+    """
+    The main managing object for the Marketplace plug-in.
+    """
+    pass  # TODO
---
+++ plugins/Marketplace/__init__.py
@@ -0,0 +1,17 @@
+# Copyright (c) 2021 Ultimaker B.V.
+# Cura is released under the terms of the LGPLv3 or higher.
+
+from .Marketplace import Marketplace
+
+def getMetaData():
+    """
+    Extension-type plug-ins don't have any specific metadata being used by Cura.
+    """
+    return {}
+
+
+def register(app):
+    """
+    Register the plug-in object with Uranium.
+    """
+    return { "extension": Marketplace() }
