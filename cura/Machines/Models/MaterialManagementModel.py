1f6e1942d775d47dd48be4fd83c5346271827a5e
Jaime van Kessel <nallath@gmail.com>
2021-10-21 10:39:47 +0200

    Disable the material sync message again
    CURA-8639

--- cura/Machines/Models/MaterialManagementModel.py
+++ cura/Machines/Models/MaterialManagementModel.py
@@ -44,7 +44,8 @@ class MaterialManagementModel(QObject):
         for package_id, package_data in application.getPackageManager().getPackagesInstalledOnStartup().items():
             if package_data["package_info"]["package_type"] == "material":
                 # At least one new material was installed
-                self._showSyncNewMaterialsMessage()
+                # TODO: This should be enabled again once CURA-8609 is merged
+                #self._showSyncNewMaterialsMessage()
                 break
 
     def _showSyncNewMaterialsMessage(self) -> None:
