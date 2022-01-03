0615369cba94ad4671b7e8817cd168f464517f89
Jaime van Kessel <nallath@gmail.com>
2022-01-03 10:29:15 +0100

    Clean up import order
    CURA-8588

--- plugins/Marketplace/LocalPackageList.py
+++ plugins/Marketplace/LocalPackageList.py
@@ -1,17 +1,11 @@
-#  Copyright (c) 2021 Ultimaker B.V.
+#  Copyright (c) 2022 Ultimaker B.V.
 #  Cura is released under the terms of the LGPLv3 or higher.
 
 from typing import Any, Dict, List, Optional, TYPE_CHECKING
-from operator import attrgetter
 
 from PyQt5.QtCore import pyqtSlot, QObject
 
 from UM.Version import Version
-
-if TYPE_CHECKING:
-    from PyQt5.QtCore import QObject
-    from PyQt5.QtNetwork import QNetworkReply
-
 from UM.i18n import i18nCatalog
 from UM.TaskManagement.HttpRequestManager import HttpRequestManager
 from UM.Logger import Logger
@@ -19,6 +13,10 @@ from UM.Logger import Logger
 from .PackageList import PackageList
 from .PackageModel import PackageModel
 from .Constants import PACKAGE_UPDATES_URL
+
+if TYPE_CHECKING:
+    from PyQt5.QtCore import QObject
+    from PyQt5.QtNetwork import QNetworkReply
 
 catalog = i18nCatalog("cura")
 
