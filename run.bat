0e892940eb2faa2147b2bb7a8298b231cd4fea01
nadav tur <nadavt100@gmail.com>
2022-03-13 15:49:17 +0200

    Fixed script

--- run.bat
+++ run.bat
@@ -1,3 +1,2 @@
-set PYTHONPATH=%PYTHONPATH%;C:\dev\cura\Uranium
 copy /Y "..\CuraEngine\install_dir\bin\CuraEngine.exe" CuraEngine.exe
-python cura_app.py
+cmd /V /C "set PYTHONPATH=!PYTHONPATH!;C:\dev\cura\Uranium && python cura_app.py"
