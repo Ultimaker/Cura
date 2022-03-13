cf848aecab034043362a5f883b94f10e3ecbc728
nadav tur <nadavt100@gmail.com>
2022-03-13 16:48:29 +0200

    Added debug to cmd

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
 copy /Y "..\CuraEngine\install_dir\bin\CuraEngine.exe" CuraEngine.exe
-cmd /V /C "set PYTHONPATH=!PYTHONPATH!;C:\dev\cura\Uranium && python cura_app.py"
+cmd /V /C "set PYTHONPATH=!PYTHONPATH!;C:\dev\cura\Uranium && python cura_app.py --debug"
