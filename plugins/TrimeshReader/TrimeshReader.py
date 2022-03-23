34d6a2ba29e9396cb3b9f569cfca7850ea717339
j.spijker@ultimaker.com <jelle spijker>
2022-03-23 16:42:26 +0100

    Credits given where credits due
    The Update copyright feature of Pycharm automatically removed the
    mentioning of one of our main contributors in this license header.
    Added it manually again, unchecked the Update Copyright check box from
    the commit window (on the second attempt) and order is restored to the
    universe.
    
    Mea Culpa @fieldOfView, Mea Maxima Culpa

--- plugins/TrimeshReader/TrimeshReader.py
+++ plugins/TrimeshReader/TrimeshReader.py
@@ -1,4 +1,4 @@
-#  Copyright (c) 2019-2022 Ultimaker B.V.
+#  Copyright (c) 2019-2022 Ultimaker B.V., fieldOfView
 #  Cura is released under the terms of the LGPLv3 or higher.
 
 # The _toMeshData function is taken from the AMFReader class which was built by fieldOfView.
