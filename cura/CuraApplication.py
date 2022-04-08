fb22a04d4dbff7efbb1ed8e974ad2ca79433646a
Jelle Spijker <spijker.jelle@gmail.com>
2022-04-08 20:34:42 +0200

    Add search path for the pyinstaller locations
    Contributes to CURA-8640

--- cura/CuraApplication.py
+++ cura/CuraApplication.py
@@ -350,6 +350,7 @@ class CuraApplication(QtApplication):
 
         app_root = os.path.abspath(os.path.join(os.path.dirname(sys.executable)))
         Resources.addSearchPath(os.path.join(app_root, "share", "cura", "resources"))
+        Resources.addSearchPath(os.path.join(os.path.abspath(os.path.dirname(__file__)), "..", "share", "cura", "resources"))
 
         Resources.addSearchPath(os.path.join(self._app_install_dir, "share", "cura", "resources"))
         if not hasattr(sys, "frozen"):
