1a464051d45367e80f845b30d21ad51ba12fe112
j.delarago <joeydelarago@gmail.com>
2022-06-16 14:27:13 +0200

    On initial startup, when assigning your first printer the ActiveIntentQualitiesModel.py would not be updated. Causing an error unsupported configuration to show. Fixed by updating the model when the globalContainer is changed as well.
    CURA-8849

--- cura/Machines/Models/ActiveIntentQualitiesModel.py
+++ cura/Machines/Models/ActiveIntentQualitiesModel.py
@@ -36,6 +36,8 @@ class ActiveIntentQualitiesModel(ListModel):
         IntentManager.intentCategoryChangedSignal.connect(self._update)
         machine_manager = cura.CuraApplication.CuraApplication.getInstance().getMachineManager()
         machine_manager.activeQualityGroupChanged.connect(self._update)
+        machine_manager.globalContainerChanged.connect(self._updateDelayed)
+        machine_manager.extruderChanged.connect(self._updateDelayed)  # We also need to update if an extruder gets disabled
 
         self._update_timer = QTimer()
         self._update_timer.setInterval(100)
@@ -52,6 +54,7 @@ class ActiveIntentQualitiesModel(ListModel):
             self._updateDelayed()
 
     def _update(self):
+        print("Updating")
         active_extruder_stack = cura.CuraApplication.CuraApplication.getInstance().getMachineManager().activeStack
         if active_extruder_stack:
             self._intent_category = active_extruder_stack.intent.getMetaDataEntry("intent_category", "")
