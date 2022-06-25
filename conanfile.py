815160f45f0aadcda4f5547a8b8c184491678010
j.spijker@ultimaker.com <jelle spijker>
2022-06-25 18:56:13 +0200

    Build CPython shared
    Contributes to CURA-9365

--- conanfile.py
+++ conanfile.py
@@ -87,6 +87,7 @@ class CuraConan(ConanFile):
         self.options["arcus"].shared = True
         self.options["savitar"].shared = True
         self.options["pynest2d"].shared = True
+        self.options["cpython"].shared = True
 
     def validate(self):
         if self.version and tools.Version(self.version) <= tools.Version("4"):
