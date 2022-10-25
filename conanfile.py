f1936dc37ce144ac58ee4c11cf68394d0b545bc8
Jelle Spijker <spijker.jelle@gmail.com>
2022-10-25 02:41:35 +0200

    Ensure that gettext bin can be found

--- conanfile.py
+++ conanfile.py
@@ -296,7 +296,7 @@ class CuraConan(ConanFile):
         for po_file in self.source_path.joinpath("resources", "i18n").glob("**/*.po"):
             mo_file = self.build_path.joinpath(po_file.with_suffix('.mo').relative_to(self.source_path))
             mkdir(self, str(unix_path(self, mo_file.parent)))
-            self.run(f"msgfmt {po_file} -o {mo_file} -f", env="conanbuild")
+            self.run(f"msgfmt {po_file} -o {mo_file} -f", env="conanbuild", run_environment=True)
 
         # FIXME: once m4, autoconf, automake are Conan V2 ready self.win_bash = None
 
@@ -322,7 +322,7 @@ class CuraConan(ConanFile):
             for po_file in self.source_path.joinpath("resources", "i18n").glob("**/*.po"):
                 pot_file = self.source_path.joinpath("resources", "i18n", po_file.with_suffix('.pot').name)
                 mkdir(self, str(unix_path(self, pot_file.parent)))
-                self.run(f"msgmerge --no-wrap --no-fuzzy-matching -width=140 -o {po_file} {po_file} {pot_file}", env = "conanbuild")
+                self.run(f"msgmerge --no-wrap --no-fuzzy-matching -width=140 -o {po_file} {po_file} {pot_file}", env = "conanbuild", run_environment=True)
 
 
     def imports(self):
