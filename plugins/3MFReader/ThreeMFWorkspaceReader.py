28862c89e58b7a45f710b5931ab8ce283ba7dfe0
Joey de l'Arago <joeydelarago@gmail.com>
2022-11-15 15:03:23 +0100

    Show online badge and printer icon when initially loading machine selector CURA-9424

--- plugins/3MFReader/ThreeMFWorkspaceReader.py
+++ plugins/3MFReader/ThreeMFWorkspaceReader.py
@@ -600,6 +600,7 @@ class ThreeMFWorkspaceReader(WorkspaceReader):
         self._dialog.setActiveMode(active_mode)
         self._dialog.setUpdatableMachines(updatable_machines)
         self._dialog.setMachineName(machine_name)
+        self._dialog.setIsNetworkedMachine(existing_global_stack.hasNetworkedConnection())
         self._dialog.setMachineToOverride(global_stack_id)
         self._dialog.setMaterialLabels(material_labels)
         self._dialog.setMachineType(machine_type)
