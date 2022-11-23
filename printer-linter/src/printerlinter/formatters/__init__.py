a7800f824a7cebab14533e76f6d96bbe6b3540b6
Joey de l'Arago <joeydelarago@gmail.com>
2022-11-23 17:20:22 +0100

    Use formatters in terminal.py
    Do some general refactoring for readability

--- printer-linter/src/printerlinter/formatters/__init__.py
+++ printer-linter/src/printerlinter/formatters/__init__.py
@@ -1,4 +1,4 @@
-from def_json_formatter import DefJsonFormatter
-from inst_cfg_formatter import InstCfgFormatter
+from .def_json_formatter import DefJsonFormatter
+from .inst_cfg_formatter import InstCfgFormatter
 
 __all__ = ["DefJsonFormatter", "InstCfgFormatter"]
