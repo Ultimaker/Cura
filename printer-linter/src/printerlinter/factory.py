f8590be6da6498001be68ae179c98f0fb6db05b3
Joey de l'Arago <joeydelarago@gmail.com>
2022-11-25 14:11:06 +0100

    Remove elif after returns

--- printer-linter/src/printerlinter/factory.py
+++ printer-linter/src/printerlinter/factory.py
@@ -11,13 +11,16 @@ def getLinter(file: Path, settings: dict) -> Optional[Linter]:
     """ Returns a Linter depending on the file format """
     if not file.exists():
         return None
-    elif ".inst" in file.suffixes and ".cfg" in file.suffixes:
+
+    if ".inst" in file.suffixes and ".cfg" in file.suffixes:
         return Profile(file, settings)
-    elif ".def" in file.suffixes and ".json" in file.suffixes:
+
+    if ".def" in file.suffixes and ".json" in file.suffixes:
         if file.stem in ("fdmprinter.def", "fdmextruder.def"):
             return None
         return Definition(file, settings)
-    elif file.parent.stem == "meshes":
+
+    if file.parent.stem == "meshes":
         return Meshes(file, settings)
 
     return None
