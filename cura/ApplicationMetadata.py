0bb59acdf826e8f9b22d87be996ee6d19c46407d
jspijker <j.spijker@ultimaker.com>
2022-11-28 10:14:51 +0100

    Update company name to UltiMaker
    Contributes to CURA-9808

--- cura/ApplicationMetadata.py
+++ cura/ApplicationMetadata.py
@@ -5,7 +5,7 @@
 # General constants used in Cura
 # ---------
 DEFAULT_CURA_APP_NAME = "cura"
-DEFAULT_CURA_DISPLAY_NAME = "Ultimaker Cura"
+DEFAULT_CURA_DISPLAY_NAME = "UltiMaker Cura"
 DEFAULT_CURA_VERSION = "dev"
 DEFAULT_CURA_BUILD_TYPE = ""
 DEFAULT_CURA_DEBUG_MODE = False
