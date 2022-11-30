dbc0ac11802c3629834eb194ae26900136501b42
jspijker <j.spijker@ultimaker.com>
2022-11-30 08:04:09 +0100

    Only suggest a fix if it is on a single line

--- printer-linter/src/printerlinter/linters/defintion.py
+++ printer-linter/src/printerlinter/linters/defintion.py
@@ -45,17 +45,23 @@ class Definition(Linter):
                 if is_redefined:
                     redefined = re.compile(r'.*(\"' + key + r'\"[\s\:\S]*?)\{[\s\S]*?\},?')
                     found = redefined.search(self._content)
+                    # TODO: Figure out a way to support multiline fixes in the PR review GH Action, for now suggest no fix to ensure no ill-formed json are created
+                    if len(found.group().splitlines()) > 1:
+                        replacements = []
+                    else:
+                        replacements = [Replacement(
+                            file = self._file,
+                            offset = found.span(1)[0],
+                            length = len(found.group()),
+                            replacement_text = "")]
+
                     yield Diagnostic(
                         file = self._file,
                         diagnostic_name = "diagnostic-definition-redundant-override",
                         message = f"Overriding {key} with the same value ({value}) as defined in parent definition: {definition['inherits']}",
                         level = "Warning",
                         offset = found.span(0)[0],
-                        replacements = [Replacement(
-                            file = self._file,
-                            offset = found.span(1)[0],
-                            length = len(found.group()),
-                            replacement_text = "")]
+                        replacements = replacements
                     )
 
     def _loadDefinitionFiles(self, definition_file) -> None:
