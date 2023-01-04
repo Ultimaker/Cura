6def3dcb5e9c447a81870b54f9fa0b8b602c3c81
MariMakes <40423138+MariMakes@users.noreply.github.com>
2023-01-04 10:15:27 +0100

    Change Only_List_USB
    Apply Sovengers so more Macusers can print over USB as mentioned in https://community.ultimaker.com/topic/42417-fix-for-connecting-creality-ender-3-neo-to-mac-via-usb/

--- plugins/USBPrinting/USBPrinterOutputDeviceManager.py
+++ plugins/USBPrinting/USBPrinterOutputDeviceManager.py
@@ -83,7 +83,7 @@ class USBPrinterOutputDeviceManager(QObject, OutputDevicePlugin):
             if container_stack.getMetaDataEntry("supports_usb_connection"):
                 machine_file_formats = [file_type.strip() for file_type in container_stack.getMetaDataEntry("file_formats").split(";")]
                 if "text/x-gcode" in machine_file_formats:
-                    port_list = self.getSerialPortList(only_list_usb=True)
+                    port_list = self.getSerialPortList(only_list_usb=False)
             self._addRemovePorts(port_list)
             time.sleep(5)
 
