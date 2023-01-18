5186b234dc94a037d690506c0df61a7609ddba7b
Joey de l'Arago <joeydelarago@gmail.com>
2023-01-18 17:33:39 +0100

    Remove redundant code for merging po files
    CURA-9814

--- conanfile.py
+++ conanfile.py
@@ -326,17 +326,9 @@ class CuraConan(ConanFile):
             # Update the po files
             if self.settings.os != "Windows" or self.conf.get("tools.microsoft.bash:path", check_type = str):
                 # FIXME: once m4, autoconf, automake are Conan V2 ready use self.win_bash and add gettext as base tool_requirement
-                cpp_info = self.dependencies["gettext"].cpp_info
-
                 # Extract all the new strings and update the existing po files
                 extractTool = self.python_requires["translationextractor"].module.ExtractTranslations(self)
                 extractTool.extract(self.source_path, self.source_path.joinpath("resources", "i18n"), "cura.pot")
-
-                for po_file in self.source_path.joinpath("resources", "i18n").glob("**/*.po"):
-                    pot_file = self.source_path.joinpath("resources", "i18n", po_file.with_suffix('.pot').name)
-                    mkdir(self, str(unix_path(self, pot_file.parent)))
-                    self.run(f"{cpp_info.bindirs[0]}/msgmerge --no-wrap --no-fuzzy-matching -width=140 -o {po_file} {po_file} {pot_file}",
-                             env = "conanbuild", ignore_errors = True)
 
     def imports(self):
         self.copy("CuraEngine.exe", root_package = "curaengine", src = "@bindirs", dst = "", keep_path = False)
