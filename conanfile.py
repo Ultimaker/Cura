8a73fa893509b6c14a24660a44c728a9f66626ee
jspijker <j.spijker@ultimaker.com>
2023-03-07 11:55:04 +0100

    Pin 5.3.0 dependencies

--- conanfile.py
+++ conanfile.py
@@ -279,12 +279,12 @@ class CuraConan(ConanFile):
 
     def requirements(self):
         self.requires("pyarcus/5.2.2")
-        self.requires("curaengine/(latest)@ultimaker/stable")
+        self.requires("curaengine/5.3.0")
         self.requires("pysavitar/5.2.2")
         self.requires("pynest2d/5.2.2")
-        self.requires("uranium/(latest)@ultimaker/stable")
-        self.requires("fdm_materials/(latest)@ultimaker/stable")
-        self.requires("cura_binary_data/5.3.0-beta")
+        self.requires("uranium/5.3.0")
+        self.requires("fdm_materials/5.3.0")
+        self.requires("cura_binary_data/5.3.0")
         self.requires("cpython/3.10.4")
         if self.options.internal:
             self.requires("cura_private_data/(latest)@ultimaker/testing")
