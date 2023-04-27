7f6bb7aa420d602188cf1e36deeb1ae16ddb1bd0
Jelle Spijker <jelle.spijker@ultimaker.com>
2023-04-27 14:18:38 +0200

    use curaengine latest

--- conanfile.py
+++ conanfile.py
@@ -270,7 +270,7 @@ class CuraConan(ConanFile):
 
     def requirements(self):
         self.requires("pyarcus/5.2.2")
-        self.requires("curaengine/latest@ultimaker/cura_10415")
+        self.requires("curaengine/latest@ultimaker/testing")
         self.requires("pysavitar/5.2.2")
         self.requires("pynest2d/5.2.2")
         self.requires("uranium/(latest)@ultimaker/testing")
