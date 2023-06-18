b5e2018092ba8f342414f8386bdaa94dfcda9aa5
Jelle Spijker <jelle.spijker@ultimaker.com>
2023-06-18 13:18:54 +0200

    Use latest/stable for future developement of 5.4

--- conanfile.py
+++ conanfile.py
@@ -49,7 +49,7 @@ class CuraConan(ConanFile):
 
     def set_version(self):
         if not self.version:
-            self.version = "5.4.0-beta"
+            self.version = "5.4.0-beta.2"
 
     @property
     def _pycharm_targets(self):
@@ -270,17 +270,17 @@ class CuraConan(ConanFile):
 
     def requirements(self):
         self.requires("pyarcus/5.2.2")
-        self.requires("curaengine/5.4.0-beta.1")
+        self.requires("curaengine/(latest)@utlimaker/stable")
         self.requires("pysavitar/5.2.2")
         self.requires("pynest2d/5.2.2")
-        self.requires("uranium/5.4.0-beta.1")
-        self.requires("cura_binary_data/5.4.0-beta.1")
+        self.requires("uranium/(latest)@utlimaker/stable")
+        self.requires("cura_binary_data/(latest)@utlimaker/stable")
         self.requires("cpython/3.10.4")
         if self.options.internal:
             self.requires("cura_private_data/(latest)@ultimaker/testing")
             self.requires("fdm_materials/(latest)@internal/testing")
         else:
-            self.requires("fdm_materials/5.4.0-beta.1")
+            self.requires("fdm_materials/(latest)@utlimaker/stable")
 
     def build_requirements(self):
         if self.options.devtools:
