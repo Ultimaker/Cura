44c6cf105e516296babacd37d14b31fa5dd4bcdf
GregValiant <64202104+GregValiant@users.noreply.github.com>
2023-06-22 18:06:21 -0400

    Altered SearchAndReplace,  InsertAtLayerChange and TimeLapse
    All three files get new options:
    SearchAndReplace - Added range of layers, and options to skip the StartUp and Ending Gcode.
    InsertAtLayerChange - Added Insert Frequency and layer range.  Added multi-line input.
    TimeLapse - Added Insert Frequency

--- plugins/PostProcessingPlugin/scripts/SearchAndReplace.py
+++ plugins/PostProcessingPlugin/scripts/SearchAndReplace.py
@@ -1,16 +1,14 @@
 # Copyright (c) 2017 Ghostkeeper
 # The PostProcessingPlugin is released under the terms of the AGPLv3 or higher.
+# Altered by GregValiant (Greg Foresi) February, 2023.
+#    Added option for a layer search with a Start Layer and an End layer.
+#    Added 'Ignore StartUp G-code' and 'Ignore Ending G-code' options
 
-import re #To perform the search and replace.
-
+import re
 from ..Script import Script
 
-
 class SearchAndReplace(Script):
-    """Performs a search-and-replace on all g-code.
-
-    Due to technical limitations, the search can't cross the border between
-    layers.
+    """Performs a search-and-replace on the g-code.
     """
 
     def getSettingDataString(self):
@@ -23,37 +21,173 @@ class SearchAndReplace(Script):
             {
                 "search":
                 {
-                    "label": "Search",
-                    "description": "All occurrences of this text will get replaced by the replacement text.",
+                    "label": "Search for:",
+                    "description": "All occurrences of this text (within the search range) will be replaced by the 'Replace with' text.  The search string is CASE SPECIFIC so 'LAYER' is not the same as 'layer'.",
                     "type": "str",
                     "default_value": ""
                 },
                 "replace":
                 {
-                    "label": "Replace",
-                    "description": "The search text will get replaced by this text.",
+                    "label": "Replace with:",
+                    "description": "The 'Search For' text will get replaced by this text.  For Multi-Line insertions use the newline character 'backslash plus n' as the delimiter. Also for multi-line insertions the last character must be 'backslash plus n'",
                     "type": "str",
                     "default_value": ""
                 },
                 "is_regex":
                 {
                     "label": "Use Regular Expressions",
-                    "description": "When enabled, the search text will be interpreted as a regular expression.",
+                    "description": "When disabled the search string is treated as a simple text string.  When enabled, the search text will be re-compiled as a 'regular' python expression.",
                     "type": "bool",
                     "default_value": false
+                },
+                "enable_layer_search":
+                {
+                    "label": "Enable search within a Layer Range:",
+                    "description": "When enabled, You can choose a Start and End layer for the search.  When 'Layer Search' is enabled the StartUp and Ending g-codes are always ignored.",
+                    "type": "bool",
+                    "default_value": false,
+                    "enabled": true
+                },
+                "search_start":
+                {
+                    "label": "Start S&R at Layer:",
+                    "description": "Use the Cura Preview layer numbering.  The Start Layer will be included. Enter '1' to start with gcode ';LAYER:0'. Enter ''-6'' to start with the first layer of a raft",
+                    "type": "int",
+                    "default_value": 1,
+                    "minimum_value": -6,
+                    "enabled": "enable_layer_search"
+                },
+                "search_end":
+                {
+                    "label": "Stop S&R at end of Layer:",
+                    "description": "Use the Cura Preview layer numbering.  Enter ''end'' to replace to the end of the file.  Enter any other layer number and the replacements will conclude at the end of that layer.",
+                    "type": "int",
+                    "default_value": -1,
+                    "minimum_value": -1,
+                    "enabled": "enable_layer_search"
+                },
+                "first_instance_only":
+                {
+                    "label": "Replace first instance only:",
+                    "description": "When enabled only the first instance is replaced.",
+                    "type": "bool",
+                    "default_value": false,
+                    "enabled": true
+                },
+                "ignore_start":
+                {
+                    "label": "Ignore StartUp G-code:",
+                    "description": "When enabled the StartUp G-code is unaffected.  The StartUp G-code is everything from ';generated with Cura...' to ';LAYER_COUNT:' inclusive.",
+                    "type": "bool",
+                    "default_value": true,
+                    "enabled": "not enable_layer_search"
+                },
+                "ignore_end":
+                {
+                    "label": "Ignore Ending G-code:",
+                    "description": "When enabled the Ending G-code is unaffected.",
+                    "type": "bool",
+                    "default_value": true,
+                    "enabled": "not enable_layer_search"
                 }
             }
         }"""
 
     def execute(self, data):
         search_string = self.getSettingValueByKey("search")
-        if not self.getSettingValueByKey("is_regex"):
-            search_string = re.escape(search_string) #Need to search for the actual string, not as a regex.
-        search_regex = re.compile(search_string)
-
         replace_string = self.getSettingValueByKey("replace")
+        is_regex = self.getSettingValueByKey("is_regex")
+        enable_layer_search = self.getSettingValueByKey("enable_layer_search")
+        start_layer = self.getSettingValueByKey("search_start")
+        end_layer = self.getSettingValueByKey("search_end")
+        ignore_start = self.getSettingValueByKey("ignore_start")
+        ignore_end = self.getSettingValueByKey("ignore_end")
+        first_instance_only = bool(self.getSettingValueByKey("first_instance_only"))
 
-        for layer_number, layer in enumerate(data):
-            data[layer_number] = re.sub(search_regex, replace_string, layer) #Replace all.
+    #Find the raft and layer:0 indexes--------------------------------------------------------------------------
+        raft_start_index = 0
+        layer_0_index = 0
+        start_index = 1
+        end_index = len(data)
+        try:
+            for l_num in range(2,12,1):
+                layer = data[l_num]
+                if ";LAYER:-" in layer and raft_start_index == 0:
+                    raft_start_index = l_num
+                if ";LAYER:0" in layer:
+                    layer_0_index = l_num
+                    break
+            if raft_start_index == 0:
+                raft_start_index = layer_0_index
+                raft_layers = 0
+            elif raft_start_index < layer_0_index:
+                raft_layers = layer_0_index - raft_start_index
+            else:
+                raft_layers = 0
+        except:
+            all
+            
+    #Determine the actual start and end indexes of the data----------------------------------------------------
+        try:
+            if not enable_layer_search:
+                if ignore_start:
+                    start_index = 2
+                else:
+                    start_index = 1
+                if ignore_end:
+                    end_index = len(data) - 1
+                else:
+                    end_index = len(data)
+            elif enable_layer_search:
+                if start_layer < 1 and start_layer != -6:
+                    start_index = layer_0_index - raft_layers
+                elif start_layer == -6:
+                    start_index = 2
+                else:
+                    start_index = raft_start_index + start_layer-1
+                if end_layer == -1:
+                    end_index = len(data)-1
+                else:
+                    end_index = raft_start_index + int(end_layer)
+                if end_index > len(data)-1: end_index = len(data)-1 #For possible Input error
+                if int(end_layer) < int(start_layer): end_index = start_index #For possible Input error
+        except:
+            start_index = 2
+            end_index = len(data) -1
 
+    #if "first_instance_only" is enabled:
+        replaceone = False
+        new_string = search_string
+        if first_instance_only:
+            if not is_regex:
+                new_string = re.escape(search_string)
+            search_regex = re.compile(new_string)
+            for num in range(start_index, end_index,1):
+                layer = data[num]
+                lines = layer.split("\n")
+                for index, line in enumerate(lines):
+                    if re.match(search_regex, line) and replaceone == False:
+                        lines[index] = re.sub(search_regex, replace_string, line)
+                        data[num] = "\n".join(lines)
+                        replaceone = True
+                        break
+                if replaceone: break
+            data[0] += ";  Search and Replace (Search: " + str(self.getSettingValueByKey("search")) + " | Replace: " + str(self.getSettingValueByKey("replace")) + ")\n"
+            return data
+            
+    #Do all the replacements---------------------------------------------------------------------------------------
+        if not is_regex:
+            search_string = re.escape(search_string)
+        search_regex = re.compile(search_string)
+        if end_index > start_index:
+            for index in range(start_index,end_index,1):
+                layer = data[index]
+                data[index] = re.sub(search_regex, replace_string, layer)
+        elif end_index == start_index:
+            layer = data[start_index]
+            data[start_index] = re.sub(search_regex, replace_string, layer)
+        
+#Add the post-processor name and settings to the gcode----------------------------------------------------
+        data[0] += ";  Search and Replace (Search: " + str(self.getSettingValueByKey("search")) + " | Replace: " + str(self.getSettingValueByKey("replace")) + ")\n"        
         return data
+        
--- plugins/PostProcessingPlugin/scripts/TimeLapse.py
+++ plugins/PostProcessingPlugin/scripts/TimeLapse.py
@@ -1,9 +1,15 @@
 # Copyright (c) 2020 Ultimaker B.V.
 # Cura is released under the terms of the LGPLv3 or higher.
 # Created by Wayne Porter
+# Modified 5/15/2023 - Greg Valiant (Greg Foresi)
+#    Added insertion frequency
+#    Adjusted for use with Relative Extrusion
+#    Changed Retract to a boolean and when true use the regular Cura retract settings.
+#    Use the regular Cura settings for Travel Speed and Speed_Z instead of asking.
+#    Added code to check the E location to prevent retracts if the filament was already retracted.
 
 from ..Script import Script
-
+from UM.Application import Application
 
 class TimeLapse(Script):
     def __init__(self):
@@ -11,7 +17,7 @@ class TimeLapse(Script):
 
     def getSettingDataString(self):
         return """{
-            "name": "Time Lapse",
+            "name": "Time Lapse Camera",
             "key": "TimeLapse",
             "metadata": {},
             "version": 2,
@@ -19,10 +25,26 @@ class TimeLapse(Script):
             {
                 "trigger_command":
                 {
-                    "label": "Trigger camera command",
-                    "description": "G-code command used to trigger camera.",
+                    "label": "Camera Trigger Command",
+                    "description": "G-code command used to trigger the camera.",
                     "type": "str",
                     "default_value": "M240"
+                },
+                "insert_frequency":
+                {
+                    "label": "How often (layers)",
+                    "description": "Every so many layers (always starts at the first layer whether it's the model or a raft).",
+                    "type": "enum",
+                    "options": {
+                        "every_layer": "Every Layer",
+                        "every_2nd": "Every 2nd",
+                        "every_3rd": "Every 3rd",
+                        "every_5th": "Every 5th",
+                        "every_10th": "Every 10th",
+                        "every_25th": "Every 25th",
+                        "every_50th": "Every 50th",
+                        "every_100th": "Every 100th"},
+                    "default_value": "every_layer"
                 },
                 "pause_length":
                 {
@@ -36,7 +58,7 @@ class TimeLapse(Script):
                 "park_print_head":
                 {
                     "label": "Park Print Head",
-                    "description": "Park the print head out of the way. Assumes absolute positioning.",
+                    "description": "Park the print head out of the way.",
                     "type": "bool",
                     "default_value": true
                 },
@@ -55,90 +77,119 @@ class TimeLapse(Script):
                     "description": "What Y location does the head move to for photo.",
                     "unit": "mm",
                     "type": "float",
-                    "default_value": 190,
-                    "enabled": "park_print_head"
-                },
-                "park_feed_rate":
-                {
-                    "label": "Park Feed Rate",
-                    "description": "How fast does the head move to the park coordinates.",
-                    "unit": "mm/s",
-                    "type": "float",
-                    "default_value": 9000,
+                    "default_value": 0,
                     "enabled": "park_print_head"
                 },
                 "retract":
                 {
-                    "label": "Retraction Distance",
-                    "description": "Filament retraction distance for camera trigger.",
-                    "unit": "mm",
-                    "type": "int",
-                    "default_value": 0
+                    "label": "Retract when required",
+                    "description": "Retract if there isn't already a retraction.  If unchecked then there will be no retraction even if there is none in the gcode.",
+                    "type": "bool",
+                    "default_value": true
                 },
                 "zhop":
                 {
                     "label": "Z-Hop Height When Parking",
-                    "description": "Z-hop length before parking",
+                    "description": "The height to lift the nozzle off the print before parking.",
                     "unit": "mm",
                     "type": "float",
-                    "default_value": 0
+                    "default_value": 2.0,
+                    "minimum_value": 0.0
                 }
             }
         }"""
 
     def execute(self, data):
-        feed_rate = self.getSettingValueByKey("park_feed_rate")
+        mycura = Application.getInstance().getGlobalContainerStack()
+        relative_extrusion = bool(mycura.getProperty("relative_extrusion", "value"))
+        extruder = mycura.extruderList
+        retract_speed = int(extruder[0].getProperty("retraction_speed", "value"))*60
+        retract_dist = int(extruder[0].getProperty("retraction_amount", "value"))
+        speed_z = int(extruder[0].getProperty("speed_z_hop", "value"))*60
+        if relative_extrusion:
+            rel_cmd = 83
+        else:
+            rel_cmd = 82
+        trav_speed = int(extruder[0].getProperty("speed_travel", "value"))*60
         park_print_head = self.getSettingValueByKey("park_print_head")
         x_park = self.getSettingValueByKey("head_park_x")
         y_park = self.getSettingValueByKey("head_park_y")
         trigger_command = self.getSettingValueByKey("trigger_command")
         pause_length = self.getSettingValueByKey("pause_length")
-        retract = int(self.getSettingValueByKey("retract"))
+        retract = bool(self.getSettingValueByKey("retract"))
         zhop = self.getSettingValueByKey("zhop")
-        gcode_to_append = ";TimeLapse Begin\n"
+        when_to_insert = self.getSettingValueByKey("insert_frequency")
         last_x = 0
         last_y = 0
         last_z = 0
-
+        last_e = 0
+        prev_e = 0
+        is_retracted = False
+        gcode_to_append = ""
         if park_print_head:
-            gcode_to_append += self.putValue(G=1, F=feed_rate,
+            gcode_to_append += self.putValue(G=1, F=trav_speed,
                                              X=x_park, Y=y_park) + " ;Park print head\n"
-        gcode_to_append += self.putValue(M=400) + " ;Wait for moves to finish\n"
-        gcode_to_append += trigger_command + " ;Snap Photo\n"
-        gcode_to_append += self.putValue(G=4, P=pause_length) + " ;Wait for camera\n"
-
-        for idx, layer in enumerate(data):
-            for line in layer.split("\n"):
-                if self.getValue(line, "G") in {0, 1}:  # Track X,Y,Z location.
-                    last_x = self.getValue(line, "X", last_x)
-                    last_y = self.getValue(line, "Y", last_y)
-                    last_z = self.getValue(line, "Z", last_z)
-            # Check that a layer is being printed
-            lines = layer.split("\n")
-            for line in lines:
-                if ";LAYER:" in line:
-                    if retract != 0: # Retract the filament so no stringing happens
-                        layer += self.putValue(M=83) + " ;Extrude Relative\n"
-                        layer += self.putValue(G=1, E=-retract, F=3000) + " ;Retract filament\n"
-                        layer += self.putValue(M=82) + " ;Extrude Absolute\n"
-                        layer += self.putValue(M=400) + " ;Wait for moves to finish\n" # Wait to fully retract before hopping
-
-                    if zhop != 0:
-                        layer += self.putValue(G=1, Z=last_z+zhop, F=3000) + " ;Z-Hop\n"
-
-                    layer += gcode_to_append
-
-                    if zhop != 0:
-                        layer += self.putValue(G=0, X=last_x, Y=last_y, Z=last_z) + "; Restore position \n"
-                    else:
-                        layer += self.putValue(G=0, X=last_x, Y=last_y) + "; Restore position \n"
-
-                    if retract != 0:
-                        layer += self.putValue(M=400) + " ;Wait for moves to finish\n"
-                        layer += self.putValue(M=83) + " ;Extrude Relative\n"
-                        layer += self.putValue(G=1, E=retract, F=3000) + " ;Retract filament\n"
-                        layer += self.putValue(M=82) + " ;Extrude Absolute\n"
-
-                    data[idx] = layer
-                    break
+        gcode_to_append += self.putValue(M=400) + "         ;Wait for moves to finish\n"
+        gcode_to_append += trigger_command + "         ;Snap Photo\n"
+        gcode_to_append += self.putValue(G=4, P=pause_length) + "      ;Wait for camera\n"
+        if when_to_insert == "every_layer":
+            step_freq = 1
+        if when_to_insert == "every_2nd":
+            step_freq = 2
+        if when_to_insert == "every_3rd":
+            step_freq = 3
+        if when_to_insert == "every_5th":
+            step_freq = 5
+        if when_to_insert == "every_10th":
+            step_freq = 10
+        if when_to_insert == "every_25th":
+            step_freq = 25
+        if when_to_insert == "every_50th":
+            step_freq = 50
+        if when_to_insert == "every_100th":
+            step_freq = 100
+        # Use the step_freq to index through the layers----------------------------------------
+        for num in range(2,len(data)-1,step_freq):
+            layer = data[num]
+            try:                
+                # Track X,Y,Z location.--------------------------------------------------------
+                for line in layer.split("\n"):
+                    if self.getValue(line, "G") in {0, 1}:
+                        last_x = self.getValue(line, "X", last_x)
+                        last_y = self.getValue(line, "Y", last_y)
+                        last_z = self.getValue(line, "Z", last_z)
+                #Track the E location so that if there is already a retraction we don't double dip.        
+                        if " E" in line:
+                            last_e = line.split("E")[1]
+                            if float(last_e) < float(prev_e):
+                                is_retracted = True
+                            else:
+                                is_retracted = False
+                            prev_e = last_e
+                lines = layer.split("\n")
+                # Insert the code----------------------------------------------------
+                for line in lines:
+                    if ";LAYER:" in line:
+                        if retract and not is_retracted: # Retract unless already retracted
+                            layer += self.putValue(";  TimeLapse Begin\n")
+                            layer += self.putValue(M=83) + "          ;Extrude Relative\n"
+                            layer += self.putValue(G=1, E=-retract_dist, F=retract_speed) + " ;Retract filament\n"
+                        else:
+                            layer += self.putValue(";  TimeLapse Begin\n")
+                        if zhop != 0:
+                            layer += self.putValue(G=1, Z=last_z+zhop, F=speed_z) + " ;Z-Hop\n"
+                        layer += gcode_to_append
+                        layer += self.putValue(G=0, F=trav_speed, X=last_x, Y=last_y) + " ;Restore position \n"
+                        if zhop != 0:
+                            layer += self.putValue(G=0, F=speed_z, Z=last_z) + "  ;Restore Z position \n"
+                        if retract and not is_retracted:
+                            layer += self.putValue(G=1, E=retract_dist, F=retract_speed) + "  ;Un-Retract filament\n"
+                            layer += self.putValue(M=rel_cmd) + "          ;Extrude Mode\n"
+                        layer += self.putValue(";  TimeLapse End\n")
+                        data[num] = layer
+                        break
+            except:
+                all
+        # Add the post processor name to the gcode----------------------------------
+        data[0] += ";  Time Lapse (" + when_to_insert + ")\n"
         return data
