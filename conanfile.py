2a99e738c6d545bb3cd4e71c5f7add88527bc614
Jelle Spijker <jelle.spijker@ultimaker.com>
2023-07-04 13:40:47 +0200

    pin release version

--- conanfile.py
+++ conanfile.py
@@ -270,17 +270,17 @@ class CuraConan(ConanFile):
 
     def requirements(self):
         self.requires("pyarcus/5.2.2")
-        self.requires("curaengine/5.4.0@ultimaker/stable")
+        self.requires("curaengine/5.4.0")
         self.requires("pysavitar/5.2.2")
         self.requires("pynest2d/5.2.2")
-        self.requires("uranium/5.4.0@ultimaker/stable")
-        self.requires("cura_binary_data/5.4.0@ultimaker/stable")
+        self.requires("uranium/5.4.0")
+        self.requires("cura_binary_data/5.4.0")
         self.requires("cpython/3.10.4")
         if self.options.internal:
             self.requires("cura_private_data/(latest)@ultimaker/testing")
             self.requires("fdm_materials/(latest)@internal/testing")
         else:
-            self.requires("fdm_materials/5.4.0@ultimaker/stable")
+            self.requires("fdm_materials/5.4.0")
 
     def build_requirements(self):
         if self.options.devtools:
