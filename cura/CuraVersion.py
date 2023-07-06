9b9aced7cb1a12f89749b779f044b2790941e959
alfonsopl <alfonsoplgu@gmail.com>
2023-07-06 12:48:42 +0200

    Update Stratos from base source

--- cura/CuraVersion.py
+++ cura/CuraVersion.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2022 UltiMaker
 # Cura is released under the terms of the LGPLv3 or higher.
 
-CuraAppName = "cura"
+CuraAppName = "BCN3D Stratos"
 CuraAppDisplayName = "BCN3D Stratos"
 CuraVersion = "2.0.0"
 CuraBuildType = ""
