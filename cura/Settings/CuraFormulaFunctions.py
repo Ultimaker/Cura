9129a526ba25414570039a12efca3b699c706d43
saumya.jain <saumya.jain@ultimaker.com>
2023-08-21 10:06:51 +0200

    improve number of iterations
    CURA-10685

--- cura/Settings/CuraFormulaFunctions.py
+++ cura/Settings/CuraFormulaFunctions.py
@@ -101,10 +101,10 @@ class CuraFormulaFunctions:
     def getAnyExtruderPositionWithOrDefault(self, filter_key: str,
                                context: Optional["PropertyEvaluationContext"] = None) -> str:
         for extruder in self._getActiveExtruders(context):
-            value = extruder.getRawProperty(filter_key, "value", context=context)
-            if value is None or not value:
-                continue
-            return str(extruder.position)
+            material_container = extruder.material
+            value = material_container.getProperty(filter_key, "value", context)
+            if value is not None:
+                return extruder.position
         return self.getDefaultExtruderPosition()
 
     # Get the resolve value or value for a given key.
