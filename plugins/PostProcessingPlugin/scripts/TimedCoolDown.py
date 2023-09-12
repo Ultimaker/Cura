f6d36cc66eb097004ff50e9e45ca1f3429ff7eee
GregValiant <64202104+GregValiant@users.noreply.github.com>
2023-09-12 19:49:15 -0400

    Update TimedCoolDown.py
    Split the Park Head function so the Z is separate in case the user wants to put a cover over the print while it anneals.

--- plugins/PostProcessingPlugin/scripts/TimedCoolDown.py
+++ plugins/PostProcessingPlugin/scripts/TimedCoolDown.py
@@ -10,8 +10,6 @@ from UM.Message import Message
 class TimedCoolDown(Script):
     """Performs a search-and-replace on the g-code.
     """
-    def __init__(self):
-        super().__init__()
     
     def getSettingDataString(self):
         return """{
@@ -60,8 +58,16 @@ class TimedCoolDown(Script):
                 },
                 "park_head":
                 {
-                    "label": "Park at MaxY and MaxZ",
-                    "description": "Move to Max Y and Max Z.",
+                    "label": "Park at MaxY and MaxX",
+                    "description": "Enable this setting to move the nozzle to the Max X and Max Y to allow access to the print.",
+                    "type": "bool",
+                    "default_value": false,
+                    "enabled": true
+                },
+                "park_max_z":
+                {
+                    "label": "Move to MaxZ",
+                    "description": "Enable this setting to move the nozzle to the Max Z to allow the print to be covered.",
                     "type": "bool",
                     "default_value": false,
                     "enabled": true
@@ -89,14 +95,14 @@ class TimedCoolDown(Script):
     def execute(self, data):
         # Exit if there is no heated bed.
         if not bool(Application.getInstance().getGlobalContainerStack().getProperty("machine_heated_bed", "value")):
-            Message(text = "Timed Cool Down:" + "\n" + "  Did not run because Heated Bed is disabled in Machine Settings.").show()
+            Message(title = "Timed Cool Down", text = "The script did not run because Heated Bed is disabled in Machine Settings.").show()
             return data
         lowest_temp = int(self.getSettingValueByKey("lowest_temp"))
         
         #If the shutoff temp is under 30° then exit as a safety precaution so the bed doesn't stay on.
         if lowest_temp < 30:
             data[0] += ";  Timed Cool Down did not run.  Shutoff Temp < 30\n"
-            Message(text = "Timed Cool Down did not run because the Shutoff Temp is less than 30°.").show()     
+            Message(title = "Timed Cool Down", text = "The script did not run because the Shutoff Temp is less than 30°.").show()     
             return data
         time_span = int(float(self.getSettingValueByKey("time_span")) * 3600)
         bed_temperature = Application.getInstance().getGlobalContainerStack().getProperty("material_bed_temperature", "value")
@@ -110,15 +116,21 @@ class TimedCoolDown(Script):
             anneal_type = "bed_only"
             chamber_temp = "0"
         
-        # If Park Head is selected---------------------------------------------------------
-        if bool(self.getSettingValueByKey("park_head")):
-            max_Y = str(Application.getInstance().getGlobalContainerStack().getProperty("machine_depth", "value"))
-            max_Z = str(Application.getInstance().getGlobalContainerStack().getProperty("machine_height", "value"))
-            extruder = Application.getInstance().getGlobalContainerStack().extruderList
-            travel_speed = str(extruder[0].getProperty("speed_travel", "value")*60)
-            park_string = "G0 F" + str(travel_speed) + " Y" + str(max_Y) + " Z" + str(max_Z) + "\n" + "M18 X Y E ;disable steppers except Z\n"
-        else:
-            park_string = ""
+        # Park Head---------------------------------------------------------
+        max_y = str(Application.getInstance().getGlobalContainerStack().getProperty("machine_depth", "value"))
+        max_x = str(Application.getInstance().getGlobalContainerStack().getProperty("machine_width", "value"))
+        max_z = str(Application.getInstance().getGlobalContainerStack().getProperty("machine_height", "value"))
+        extruder = Application.getInstance().getGlobalContainerStack().extruderList
+        travel_speed = str(extruder[0].getProperty("speed_travel", "value")*60)        
+        park_xy = bool(self.getSettingValueByKey("park_head"))
+        park_z = bool(self.getSettingValueByKey("park_max_z"))
+        park_string = ""
+        if park_xy and not park_z:
+            park_string = f"G0 F{travel_speed} X{max_x} Y{max_y} ;Park XY\nM18 X Y E ;disable steppers except Z\n"
+        elif park_xy and park_z:
+            park_string = f"G0 F{travel_speed} X{max_x} Y{max_y} ;Park XY\nG0 Z{max_z} ;Raise Z to max\nM18 X Y E ;disable steppers except Z\n"
+        elif not park_xy and park_z:
+            park_string = f"G0 F{travel_speed} Z{max_z} ;Raise Z to max\nM18 X Y E ;disable steppers except Z\n"
             
         #Calculate the temperature differential
         hysteresis = bed_temperature - lowest_temp
@@ -126,7 +138,7 @@ class TimedCoolDown(Script):
         #if the bed temp is below the shutoff temp then exit
         if hysteresis <= 0:
             data[0] += ";  Timed Cool Down did not run.  Bed Temp < Shutoff Temp\n"
-            Message(text = "Timed Cool Down: {}".format("Did not run because the Bed Temp < Shutoff Temp.")).show()
+            Message(title = "Timed Cool Down", text = "Did not run because the Bed Temp < Shutoff Temp.").show()
             return data
         
         # Drop the bed temperature in 3° increments.  We only want integers.
@@ -141,20 +153,25 @@ class TimedCoolDown(Script):
         anneal_string = ";TYPE:CUSTOM:Timed Cool Down\nM117 Cool Down for " + str(round((wait_time + time_span)/3600,2)) + "hr\n" + park_string
         if wait_time > 0:
             if anneal_type == "bed_only":
-                anneal_string += "M140 S" + str(bed_temperature) + "\n"
+                anneal_string += f"M140 S{bed_temperature}\n"
             if anneal_type == "bed_chamber":
-                anneal_string += "M140 S" + str(bed_temperature) + "\n" + "M141 S" + str(chamber_temp) + "\n"
-            anneal_string += "G4 S" + str(wait_time) + "\n"            
-        anneal_string += "M140 S" + str(step_down) + "\n" + "G4 S" + str(time_per_step) + "\n"
+                anneal_string += f"M140 S{bed_temperature}\nM141 S{chamber_temp}\n"
+            anneal_string += f"G4 S{wait_time}\n"            
+        anneal_string += f"M140 S{step_down}\nG4 S{time_per_step}\n"
         step_down -= deg_per_step
         
         # Step the bed/chamber temps down and add to the anneal string--------------------------
         for num in range(bed_temperature,lowest_temp, -3):
-            anneal_string += "M140 S" + str(step_down) + "\n"
+            anneal_string += f"M140 S{step_down}\n"
             if anneal_type == "bed_chamber" and int(step_down) < int(chamber_temp):
-                anneal_string += "M141 S" + str(step_down) + "\n"
-            anneal_string += "G4 S" + str(time_per_step) + "\n"
-            anneal_string += "M117 CoolDown for " + str(round((time_span-(step_index*time_per_step))/3600,2)) + "hr\n"
+                anneal_string += f"M141 S{step_down}\n"
+            anneal_string += f"G4 S{time_per_step}\n"
+            time_remaining = round((time_span-(step_index*time_per_step))/3600,2)
+            if time_remaining >= 1.00:
+                anneal_string += "M117 CoolDown for " + str(round(time_remaining,1)) + "hr\n"
+            else:
+                time_seconds = round(time_remaining * 60)
+                anneal_string += f"M117 CoolDown for {time_seconds}min\n"
             step_down -= deg_per_step
             step_index += 1
             if step_down < lowest_temp:
@@ -169,7 +186,7 @@ class TimedCoolDown(Script):
         anneal_string += "M140 S0 ;Shut off the bed heater" + "\n"
         if anneal_type == "bed_chamber":
             anneal_string += "M141 S0 ;Shut off the chamber heater\n"
-        anneal_string += beep_string + "M117 CoolDown Complete\n;  End of Cool Down\n"
+        anneal_string += beep_string + "M117 CoolDown Complete\n;TYPE:CUSTOM End of Cool Down\n"
         layer = data[len(data)-1]
         lines = layer.split("\n")
         
@@ -188,7 +205,7 @@ class TimedCoolDown(Script):
                     
         # If park head is enabled then dont let the steppers disable until the head is parked--------------
         disable_string = ""
-        if bool(self.getSettingValueByKey("park_head")):
+        if bool(self.getSettingValueByKey("park_head")) or bool(self.getSettingValueByKey("park_max_z")):
             for num in range(0,len(lines)-1,1):
                 if lines[num].startswith("M84") or lines[num].startswith("M18"):
                     disable_string = lines[num] + "\n"
