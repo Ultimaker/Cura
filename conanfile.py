8a21a0a5cbfa041751fb6e09a9f039bae6bb92a9
Jelle Spijker <jelle.spijker@ultimaker.com>
2023-09-13 07:55:26 +0200

    Use gradual_flow from CURA-11031 branch
    Contributes to CURA-11031

--- conanfile.py
+++ conanfile.py
@@ -311,7 +311,7 @@ class CuraConan(ConanFile):
         self.requires("curaengine/(latest)@ultimaker/testing")
         self.requires("pysavitar/5.3.0")
         self.requires("pynest2d/5.3.0")
-        self.requires("curaengine_plugin_gradual_flow/(latest)@ultimaker/testing")
+        self.requires("curaengine_plugin_gradual_flow/(latest)@ultimaker/cura_11031")  # FIXME: Use `curaengine_plugin_gradual_flow/(latest)@ultimaker/testing` once merged to main
         self.requires("uranium/(latest)@ultimaker/testing")
         self.requires("cura_binary_data/(latest)@ultimaker/testing")
         self.requires("cpython/3.10.4")
