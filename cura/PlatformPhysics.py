e4a6b8dd911ddfac576749d07a9d0a5e158818e7
Erwan MATHIEU <erwan.mathieu@ultimaker.com>
2023-09-18 14:57:55 +0200

    Revert "Remove convex hull computation when not required"
    This reverts commit ab22611ecfb4f74146afc49f78ee1065373447c9.

--- cura/PlatformPhysics.py
+++ cura/PlatformPhysics.py
@@ -84,6 +84,10 @@ class PlatformPhysics:
                 z_offset = node.callDecoration("getZOffset") if node.getDecorator(ZOffsetDecorator.ZOffsetDecorator) else 0
                 move_vector = move_vector.set(y = -bbox.bottom + z_offset)
 
+            # If there is no convex hull for the node, start calculating it and continue.
+            if not node.getDecorator(ConvexHullDecorator) and not node.callDecoration("isNonPrintingMesh"):
+                node.addDecorator(ConvexHullDecorator())
+
             # only push away objects if this node is a printing mesh
             if not node.callDecoration("isNonPrintingMesh") and app_automatic_push_free:
                 # Do not move locked nodes
