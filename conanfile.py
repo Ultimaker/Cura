7633ebf70414f1e0be1535ad87f67f68f37efaab
Remco Burema <r.burema@ultimaker.com>
2023-09-20 14:01:36 +0200

    Start 5.5.x release branch.

--- conanfile.py
+++ conanfile.py
@@ -48,7 +48,7 @@ class CuraConan(ConanFile):
 
     def set_version(self):
         if not self.version:
-            self.version = "5.5.0-alpha"
+            self.version = "5.5.0-beta.1"
 
     @property
     def _pycharm_targets(self):
@@ -305,21 +305,21 @@ class CuraConan(ConanFile):
 
     def requirements(self):
         self.requires("boost/1.82.0")
-        self.requires("curaengine_grpc_definitions/latest@ultimaker/testing")
+        self.requires("curaengine_grpc_definitions/(latest)@ultimaker/stable")
         self.requires("zlib/1.2.13")
-        self.requires("pyarcus/5.3.0")
-        self.requires("curaengine/(latest)@ultimaker/testing")
-        self.requires("pysavitar/5.3.0")
-        self.requires("pynest2d/5.3.0")
-        self.requires("curaengine_plugin_gradual_flow/(latest)@ultimaker/testing")
-        self.requires("uranium/(latest)@ultimaker/testing")
-        self.requires("cura_binary_data/(latest)@ultimaker/testing")
+        self.requires("pyarcus/(latest)@ultimaker/stable")
+        self.requires("curaengine/(latest)@ultimaker/stable")
+        self.requires("pysavitar/(latest)@ultimaker/stable")
+        self.requires("pynest2d/(latest)@ultimaker/stable")
+        self.requires("curaengine_plugin_gradual_flow/(latest)@ultimaker/stable")
+        self.requires("uranium/(latest)@ultimaker/stable")
+        self.requires("cura_binary_data/(latest)@ultimaker/stable")
         self.requires("cpython/3.10.4")
         if self.options.internal:
             self.requires("cura_private_data/(latest)@ultimaker/testing")
-            self.requires("fdm_materials/(latest)@internal/testing")
+            self.requires("fdm_materials/(latest)@internal/stable")
         else:
-            self.requires("fdm_materials/(latest)@ultimaker/testing")
+            self.requires("fdm_materials/(latest)@ultimaker/stable")
 
     def build_requirements(self):
         if self.options.devtools:
