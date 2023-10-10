0f770aca15a9c401591ed5424da11e813cba5c63
Jelle Spijker <jelle.spijker@ultimaker.com>
2023-10-10 11:00:49 +0200

    set version to 5.6.0-alpha

--- conanfile.py
+++ conanfile.py
@@ -48,7 +48,7 @@ class CuraConan(ConanFile):
 
     def set_version(self):
         if not self.version:
-            self.version = "5.5.0-beta.1"
+            self.version = "5.6.0-alpha"
 
     @property
     def _pycharm_targets(self):
