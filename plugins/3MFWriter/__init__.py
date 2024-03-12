5824cf5414346d1c069fcdeb63a0b0466d095473
Saumya Jain <saumya.jain@ultimaker.com>
2024-03-12 16:02:10 +0100

    Both Project and UCP has same text
    CURA-11715

--- plugins/3MFWriter/__init__.py
+++ plugins/3MFWriter/__init__.py
@@ -39,7 +39,7 @@ def getMetaData():
             "output": [
                 {
                     "extension": workspace_extension,
-                    "description": i18n_catalog.i18nc("@item:inlistbox", "Universal Cura Project (UCP) file"),
+                    "description": i18n_catalog.i18nc("@item:inlistbox", "3MF Project file"),
                     "mime_type": "application/vnd.ms-package.3dmanufacturing-3dmodel+xml",
                     "mode": ThreeMFWorkspaceWriter.ThreeMFWorkspaceWriter.OutputMode.BinaryMode
                 }
