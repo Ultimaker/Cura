421ca02e7e604afa6abb17d864b52eed09566327
Erwan MATHIEU <erwan.mathieu@ultimaker.com>
2024-03-22 15:30:51 +0100

    Translate "Universal Cura Project" again

--- plugins/3MFWriter/__init__.py
+++ plugins/3MFWriter/__init__.py
@@ -45,7 +45,7 @@ def getMetaData():
                 },
                 {
                     "extension": "3mf",
-                    "description": "Universal Cura Project",
+                    "description": i18n_catalog.i18nc("@item:inlistbox", "Universal Cura Project"),
                     "mime_type": "application/x-ucp",
                     "mode": ThreeMFWorkspaceWriter.ThreeMFWorkspaceWriter.OutputMode.BinaryMode
                 }
