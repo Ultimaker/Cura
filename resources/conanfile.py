26c4354ac28d2ed8cb77665895eabebcd2c65657
Jelle Spijker <jelle.spijker@ultimaker.com>
2024-05-06 08:23:38 +0200

    Reuse conandata.yml from Cura
    The changes mainly include improvements to the code layout and maintainability in conanfile.py script. With additional importations and enhancing the file copy operations, the process is made clearer and reusable. Meanwhile, conandata.yml has been removed for redundancy in favor of on-the-fly version data updates.
    
    Contribute to NP-186

--- resources/conanfile.py
+++ resources/conanfile.py
@@ -1,4 +1,6 @@
 import os
+from pathlib import Path
+from jinja2 import Template
 
 from conan import ConanFile
 from conan.tools.files import copy, update_conandata
@@ -24,26 +26,37 @@ class CuraResource(ConanFile):
             self.version = self.conan_data["version"]
 
     def export(self):
+        copy(self, pattern="conandata.yml", src=os.path.join(self.recipe_folder, ".."), dst=self.export_folder,
+             keep_path=False)
         update_conandata(self, {"version": self.version})
 
     def export_sources(self):
-        copy(self, "*", os.path.join(self.recipe_folder, "definitions"), os.path.join(self.export_sources_folder, "resources", "definitions"))
-        copy(self, "*", os.path.join(self.recipe_folder, "extruders"), os.path.join(self.export_sources_folder, "resources", "extruders"))
-        copy(self, "*", os.path.join(self.recipe_folder, "intent"), os.path.join(self.export_sources_folder, "resources", "intent"))
-        copy(self, "*", os.path.join(self.recipe_folder, "meshes"), os.path.join(self.export_sources_folder, "resources", "meshes"))
-        copy(self, "*", os.path.join(self.recipe_folder, "quality"), os.path.join(self.export_sources_folder, "resources", "quality"))
-        copy(self, "*", os.path.join(self.recipe_folder, "variants"), os.path.join(self.export_sources_folder, "resources", "variants"))
+        copy(self, pattern="*", src=os.path.join(self.recipe_folder, "definitions"),
+             dst=os.path.join(self.export_sources_folder, "definitions"))
+        copy(self, pattern="*", src=os.path.join(self.recipe_folder, "extruders"),
+             dst=os.path.join(self.export_sources_folder, "extruders"))
+        copy(self, pattern="*", src=os.path.join(self.recipe_folder, "intent"),
+             dst=os.path.join(self.export_sources_folder, "intent"))
+        copy(self, pattern="*", src=os.path.join(self.recipe_folder, "meshes"),
+             dst=os.path.join(self.export_sources_folder, "meshes"))
+        copy(self, pattern="*", src=os.path.join(self.recipe_folder, "quality"),
+             dst=os.path.join(self.export_sources_folder, "quality"))
+        copy(self, pattern="*", src=os.path.join(self.recipe_folder, "variants"),
+             dst=os.path.join(self.export_sources_folder, "variants"))
 
     def validate(self):
         if Version(self.version) <= Version("4"):
             raise ConanInvalidConfiguration("Only versions 5+ are support")
 
+    def layout(self):
+        self.cpp.source.resdirs = ["definitions", "extruders", "intent", "meshes", "quality", "variants"]
+
     def package(self):
-        copy(self, "*", os.path.join(self.export_sources_folder, "resources"), os.path.join(self.package_folder, "res", "resources"))
+        copy(self, "*", os.path.join(self.export_sources_folder),
+             os.path.join(self.package_folder, "res"))
 
     def package_info(self):
         self.cpp_info.includedirs = []
-        self.cpp_info.resdirs = ["res"]
 
     def package_id(self):
         self.info.clear()
