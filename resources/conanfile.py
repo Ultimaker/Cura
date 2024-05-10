9e1d6b15373dc28d1e69ef0baa3fbc9162211aa0
Jelle Spijker <jelle.spijker@ultimaker.com>
2024-05-10 11:33:55 +0200

    Addressed code-review comments
    Contribute to NP-186

--- resources/conanfile.py
+++ resources/conanfile.py
@@ -12,10 +12,9 @@ class CuraResource(ConanFile):
     name = "cura_resources"
     license = ""
     author = "UltiMaker"
-    url = "https://github.com/Ultimaker/cura-private-data"
+    url = "https://github.com/Ultimaker/cura"
     description = "Cura Resources"
     topics = ("conan", "cura")
-    exports = "LICENSE*"
     settings = "os", "compiler", "build_type", "arch"
     no_copy_source = True
 
@@ -26,31 +25,22 @@ class CuraResource(ConanFile):
     def export(self):
         copy(self, pattern="conandata.yml", src=os.path.join(self.recipe_folder, ".."), dst=self.export_folder,
              keep_path=False)
+        copy(self, pattern="LICENSE*", src=os.path.join(self.recipe_folder, ".."), dst=self.export_folder,
+             keep_path=False)
         update_conandata(self, {"version": self.version})
 
     def export_sources(self):
-        copy(self, pattern="*", src=os.path.join(self.recipe_folder, "definitions"),
-             dst=os.path.join(self.export_sources_folder, "definitions"))
-        copy(self, pattern="*", src=os.path.join(self.recipe_folder, "extruders"),
-             dst=os.path.join(self.export_sources_folder, "extruders"))
-        copy(self, pattern="*", src=os.path.join(self.recipe_folder, "images"),
-             dst=os.path.join(self.export_sources_folder, "images"))
-        copy(self, pattern="*", src=os.path.join(self.recipe_folder, "intent"),
-             dst=os.path.join(self.export_sources_folder, "intent"))
-        copy(self, pattern="*", src=os.path.join(self.recipe_folder, "meshes"),
-             dst=os.path.join(self.export_sources_folder, "meshes"))
-        copy(self, pattern="*", src=os.path.join(self.recipe_folder, "quality"),
-             dst=os.path.join(self.export_sources_folder, "quality"))
-        copy(self, pattern="*", src=os.path.join(self.recipe_folder, "variants"),
-             dst=os.path.join(self.export_sources_folder, "variants"))
+        for shared_resources in self.conan_data["shared_resources"]:
+            copy(self, pattern="*", src=os.path.join(self.recipe_folder, shared_resources),
+                 dst=os.path.join(self.export_sources_folder, shared_resources))
 
     def validate(self):
         if Version(self.version) <= Version("4"):
             raise ConanInvalidConfiguration("Only versions 5+ are support")
 
     def layout(self):
-        self.cpp.source.resdirs = ["definitions", "extruders", "images", "intent", "meshes", "quality", "variants"]
-        self.cpp.package.resdirs = ["res/definitions", "res/extruders", "res/images", "res/intent", "res/meshes", "res/quality", "res/variants"]
+        self.cpp.source.resdirs = self.conan_data["shared_resources"]
+        self.cpp.package.resdirs = [f"res/{res}" for res in self.conan_data["shared_resources"]]
 
     def package(self):
         copy(self, "*", os.path.join(self.export_sources_folder),
