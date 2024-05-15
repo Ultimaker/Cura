3fbc91112376d51498d93440c3a0df4d12fb4817
Erwan MATHIEU <erwan.mathieu@ultimaker.com>
2024-05-15 09:09:29 +0200

    Remove useless variable declaration
    The node_matrix is defined in the following if whatever the condition, so there is no need to define it first.
    
    CURA-11902

--- plugins/3MFWriter/ThreeMFWriter.py
+++ plugins/3MFWriter/ThreeMFWriter.py
@@ -112,7 +112,6 @@ class ThreeMFWriter(MeshWriter):
         savitar_node = Savitar.SceneNode()
         savitar_node.setName(um_node.getName())
 
-        node_matrix = um_node.getLocalTransformation()
         mesh_data = um_node.getMeshData()
 
         if center_mesh:
