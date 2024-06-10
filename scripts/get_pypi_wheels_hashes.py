8210c712652de9b1bd4d56304684b78f3b869605
Erwan MATHIEU <erwan.mathieu@ultimaker.com>
2024-06-10 15:14:30 +0200

    Bump numpy and scipy versions

--- scripts/get_pypi_wheels_hashes.py
+++ scripts/get_pypi_wheels_hashes.py
@@ -13,17 +13,6 @@ def get_wheel_file_hashes(package_name, package_version, python_version):
 
     return hashes
 
-    #wheel_files = [file_info['url'] for file_info in data['releases'].values() for file_info in file_info if file_info['filename'].endswith('.whl')]
-    
-    #hashes = {}
-    #for wheel_url in wheel_files:
-        #file_name = wheel_url.split('/')[-1]
-        #download_file(wheel_url, file_name)
-        #file_hash = compute_hash(file_name, hash_algorithm)
-        #hashes[file_name] = file_hash
-    
-    #return hashes
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description = 'Displays the hashes files to be inserted in a requirements.txt')
     parser.add_argument('package', type = str, help = 'Package name')
