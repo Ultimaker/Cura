b79b95c68e326b01c8884f60df4dd390c20ffa3d
Remco Burema <r.burema@ultimaker.com>
2025-06-18 09:16:16 +0200

    empty-out CuraVersion

--- cura/CuraVersion.py
+++ cura/CuraVersion.py
@@ -1,9 +1,8 @@
-# Copyright (c) 2015 Ultimaker B.V.
-# Cura is released under the terms of the LGPLv3 or higher.
+CuraVersion = "5.11"
+CuraVersionFull = "5.11.0-alpha"
 
-CuraVersion = "3.6.2"
-CuraBuildType = "BlackBelt"
-CuraDebugMode = False
-CuraSDKVersion = "5"
-CuraCloudAPIRoot = "https://api.ultimaker.com"
-CuraCloudAPIVersion = "1"
+ConanInstalls = []
+
+PythonInstalls = []
+
+DependenciesDescriptions = {}
