bc190da418ee3291c9bb8683be04f5c9f13335a9
GregValiant <64202104+GregValiant@users.noreply.github.com>
2025-12-05 10:23:20 -0500

    Update PurgeLinesAndUnload.py
    An extremely minor bug fix.
    The GcodeReader did not like ";TYPE:CUSTOM" and was adding a "WARNNG" to the log file.  This change addresses that.

--- plugins/PostProcessingPlugin/scripts/PurgeLinesAndUnload.py
+++ plugins/PostProcessingPlugin/scripts/PurgeLinesAndUnload.py
@@ -497,10 +497,10 @@ class PurgeLinesAndUnload(Script):
                 "G92 E0 ; Reset extruder\n"
             ]
             return "\n".join(gcode_lines)
-
+        
         purge_location = self.getSettingValueByKey("purge_line_location")
         purge_extrusion_full = True if self.getSettingValueByKey("purge_line_length") == "purge_full" else False
-        purge_str = ";TYPE:CUSTOM----------[Purge Lines]\nG0 F600 Z2 ; Move up\nG92 E0 ; Reset extruder\n"
+        purge_str = ";---------------------[Purge Lines]\nG0 F600 Z2 ; Move up\nG92 E0 ; Reset extruder\n"
         purge_str += self._get_blob_code()
         # Normal cartesian printer with origin at the left front corner
         if self.bed_shape == "rectangular" and not self.origin_at_center:
